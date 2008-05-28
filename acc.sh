79df517c3c5ad4a30edfc790001223cac1b69f4f
SQL <sxwiki@gmail.com>
2008-05-28 19:02:20 +0000

    add dev list to the rest
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@49 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- acc.sh
+++ acc.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+/************************************************
+** English Wikipedia Account Request Interface **
+** All code is released into the public domain **
+**             Developers:                     **
+**  SQL ( http://en.wikipedia.org/User:SQL )   **
+**  Cobi ( http://en.wikipedia.org/User:Cobi ) **
+**                                             **
+************************************************/
+
 if [ "X`ps auxfwww|grep 'sql'|grep 'php'|grep 'accounts2.php'|grep -v grep`" = "X" ] ; then cd /home/sql/public_html/acc ; ( 
 nohup php accounts2.php &>/dev/null & ) ; done
