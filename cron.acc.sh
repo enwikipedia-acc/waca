fba311f2796999215a3449bc6dd95bfa15e90e10
SQL <sxwiki@gmail.com>
2008-05-28 19:03:37 +0000

    rename cron script
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@50 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- cron.acc.sh
+++ cron.acc.sh
@@ -10,3 +10,6 @@
 
 if [ "X`ps auxfwww|grep 'sql'|grep 'php'|grep 'accounts2.php'|grep -v grep`" = "X" ] ; then cd /home/sql/public_html/acc ; ( 
 nohup php accounts2.php &>/dev/null & ) ; done
+
+
+
