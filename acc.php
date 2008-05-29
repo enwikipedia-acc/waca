5b85e8afd42ac92c1f97978823105bd4f4738290
SQL <sxwiki@gmail.com>
2008-05-29 19:21:56 +0000

    rm some logging of reason on suspend
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@83 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- acc.php
+++ acc.php
@@ -818,7 +818,7 @@ if ($_GET['action'] == "usermgmt") {
 		$uname = $row[user_name];
 		$uoname = $row[user_onwikiname];
 		$userid = $row[user_id];
-		$out = "<li><small>[ $uname / <a href=\"http://en.wikipedia.org/wiki/User:$uoname\">$uoname</a> ] <a href=\"acc.php?action=usermgmt&approve=$userid\">Unsuspend!</a> (Suspended by $row[log_user] because $row[log_cmt])</small></li>";
+		$out = "<li><small>[ $uname / <a href=\"http://en.wikipedia.org/wiki/User:$uoname\">$uoname</a> ] <a href=\"acc.php?action=usermgmt&approve=$userid\">Unsuspend!</a> (Suspended by $row[log_user]/* FREAKING PIECE OF CRAP ISN'T WORKING RIGHT NOW because $row[log_cmt]*/)</small></li>";
 		echo "$out\n";
 	}
 	?>
