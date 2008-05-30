db4099c994b8d569e8b567c4f0ce7db3d26d45ab
SQL <sxwiki@gmail.com>
2008-05-30 16:31:33 +0000

    remove suspended
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@93 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- users.php
+++ users.php
@@ -16,12 +16,15 @@ function sanitize($what) {
 require_once('../../database.inc');
 mysql_connect("sql",$toolserver_username,$toolserver_password);
 @mysql_select_db("u_sql") or print mysql_error();
-$query = "SELECT * FROM acc_user";
+$query = "SELECT * FROM acc_user ORDER BY user_level";
 $result = mysql_query($query);
 if(!$result) Die("ERROR: No result returned.");
 echo "<h2>User List</h2>\n<ul>\n";
 while ($row = mysql_fetch_assoc($result)) {
-	echo "<li>$row[user_name] - $row[user_level]</li>\n";
+	if($row[user_level] == "Suspended") { $row[user_name] = ""; }
+	if($row[user_name] != "") {
+		echo "<li>$row[user_name] - $row[user_level]</li>\n";
+	}
 }
 echo "<ul>\n";
 ?>
