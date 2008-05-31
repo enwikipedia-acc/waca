bb98091f7f19aba41a258712465f75035d8ef3d5
SQL <sxwiki@gmail.com>
2008-05-31 05:54:40 +0000

    I broked it
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@108 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- cron.acc.sh
+++ cron.acc.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-/*****************************************************
-** English Wikipedia Account Request Interface      **
-** Wikipedia Account Request Graphic Design by      **
-** Charles Melbye is licensed under a Creative      **
-** Commons Attribution-Noncommercial-Share Alike    **
-** 3.0 United States License. All other code        **
-** released under Public Domain by the ACC          **
-** Development Team.                                **
-**             Developers:                          **
-**  SQL ( http://en.wikipedia.org/User:SQL )        **
-**  Cobi ( http://en.wikipedia.org/User:Cobi )      **
-** Cmelbye ( http://en.wikipedia.org/User:cmelbye ) **
-**                                                  **
-*****************************************************/
+######################################################
+## English Wikipedia Account Request Interface      ##
+## Wikipedia Account Request Graphic Design by      ##
+## Charles Melbye is licensed under a Creative      ##
+## Commons Attribution-Noncommercial-Share Alike    ##
+## 3.0 United States License. All other code        ##
+## released under Public Domain by the ACC          ##
+## Development Team.                                ##
+##             Developers:                          ##
+##  SQL ( http://en.wikipedia.org/User:SQL )        ##
+##  Cobi ( http://en.wikipedia.org/User:Cobi )      ##
+## Cmelbye ( http://en.wikipedia.org/User:cmelbye ) ##
+##                                                  ##
+######################################################
 
 if [ "X`ps auxfwww|grep 'sql'|grep 'php'|grep 'accounts2.php'|grep -v grep`" = "X" ] ; then cd /home/sql/public_html/acc ; ( 
 nohup php accounts2.php &>/dev/null & ) ; done
