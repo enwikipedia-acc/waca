c1ebcc975843eea4fba0d003caa31b06ab08249c
SQL <sxwiki@gmail.com>
2008-06-02 21:16:07 +0000

    add bad words
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@214 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- blacklist.php
+++ blacklist.php
@@ -19,4 +19,5 @@
 $nameblacklist[grawp1] = '/.*(KALOM(O|)RA|Kal(o|)m(o|)ira).*/i';
 $nameblacklist[grawp2] = '/.*GRAWP.*/i';
 $nameblacklist[grawp3] = '/.*(HAGG(E|A)R|HERM(E|)Y).*/i';
+$nameblacklist[badwords1] = '/.*(fuck|shit|damn|ass|bitch|cock|hell|loser|wanker|bollocks|tosser|admin|wikipedia|bot).*/i';
 ?>
