059f08e3273dc442d5d43ca08e6049b010dc0968
SQL <sxwiki@gmail.com>
2008-06-03 21:05:34 +0000

    add moar, and, start ip blacklist
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@274 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- blacklist.php
+++ blacklist.php
@@ -20,5 +20,7 @@ $nameblacklist[grawp1] = '/.*k*l[o0]?m[o0]?[i1]r*.*/i';
 $nameblacklist[grawp2] = '/.*gr*(w|vv)p.*/i';
 $nameblacklist[grawp3] = '/.*(hagg[ea]r|herme?y).*/i';
 $nameblacklist[grawp4] = '/.*secret.*combination.*/i';
+$nameblacklist[grawp5] = '/[-_\s]fuck[-_\s]/i';
 $nameblacklist[upolicy1] = '/.*([4a]dm[1i]n|w[i1]k[1i]p[3e]d[1i][4a]|b[0o]t|st[3e]w[4a]rd|j[1i]mb[0o]).*/i';
+$ipblacklist[example1] = '/127\.0\.0\..*/i';
 ?>
