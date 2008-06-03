402d4d04b8de83a4fe3321561507b939c05016cf
SQL <sxwiki@gmail.com>
2008-06-03 09:55:17 +0000

    well, if it doesn't work, it's safe, right?
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@260 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- accbot.php
+++ accbot.php
@@ -113,7 +113,7 @@ while (!feof($fp)) {
 		sleep(.75); 
 		$cmatch = preg_match("/\:.* PRIVMSG #wikipedia-en-accounts :!count (.*)/", $line, $matches);
 		if($cmatch > 0) {
-			$matches[1] = ltrim(rtrim(sanitize($matches[1])));
+			$matches[1] = sanitize(ltrim(rtrim($matches[1])));
 			$query = "SELECT COUNT(*) FROM acc_log WHERE log_action = 'Closed 1' AND log_user = '$matches[1]';";
 			$result = myq($query);
 			if(!$result) Die("ERROR: No result returned.");
