d8cb9e65f48d4fd0f1a9bff80a45ebe9d033633d
SQL <sxwiki@gmail.com>
2008-06-09 20:14:22 +0000

    mayber via SH
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@455 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- update.php
+++ update.php
@@ -98,7 +98,7 @@ if(isset($_GET[sandup])) {
 	sendtobot("[WEB]: $_SESSION[user] synchronizing ACC sandbox");
 }
 if(isset($_GET[startbot])) {
-	exec('/usr/bin/php /home/sql/public_html/acc/accbot.php > /home/sql/public_html/accbot.log');
+	exec('sh ./startbot.sh');
 	echo "Bot started!<br />\n";
 }
 if(isset($_GET[stopbot])) {
