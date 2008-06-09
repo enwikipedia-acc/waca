4a77916c879c417e709fe18fca4b2009c7c31815
Yetanotherx <yetanotherx@gmail.com>
2008-06-09 01:57:17 +0000

    Devlist
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@431 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

---
+++ devlist.php
@@ -0,0 +1,23 @@
+<?php
+/**************************************************************
+** English Wikipedia Account Request Interface               **
+** Wikipedia Account Request Graphic Design by               **
+** Charles Melbye is licensed under a Creative               **
+** Commons Attribution-Noncommercial-Share Alike             **
+** 3.0 United States License. All other code                 **
+** released under Public Domain by the ACC                   **
+** Development Team.                                         **
+**             Developers:                                   **
+**  SQL ( http://en.wikipedia.org/User:SQL )                 **
+**  Cobi ( http://en.wikipedia.org/User:Cobi )               **
+** Cmelbye ( http://en.wikipedia.org/User:cmelbye )          **
+**FastLizard4 ( http://en.wikipedia.org/User:FastLizard4 )   **
+**Stwalkerster ( http://en.wikipedia.org/User:Stwalkerster ) **
+**Soxred93 ( http://en.wikipedia.org/User:Soxred93)          **
+**                                                           **
+**************************************************************/
+
+$regdevlist = array('SQL', 'Cobi', 'Charlie', 'FastLizard4', 'Stwalkerster', 'Soxred93', 'Alexfusco5');
+$ircdevlist = array('SQLDb', 'Cobi', 'Cobi-Laptop', 'chuck', 'FastLizard4', 'stwalkerster', 'Soxred93', 'Alexfusco5');
+
+?>
