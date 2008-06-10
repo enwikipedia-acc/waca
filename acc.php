53d3dd371ede1eb1963e650c1d579b13212395da
SQL <sxwiki@gmail.com>
2008-06-10 22:37:13 +0000

    undo fix, unneeded now
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@504 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- acc.php
+++ acc.php
@@ -1084,7 +1084,7 @@ if ($_GET['action'] == "usermgmt") {
     die();
 }
 
-if ($_GET['action'] == "defer" && $_GET['id'] != "") {
+if ($_GET['action'] == "defer" && $_GET['id'] != "" && $_GET['sum'] != "") {
     if ($_GET['target'] == "admin" || $_GET['target'] == "user") {
         if ($_GET['target'] == "admin") {
             $target = "Admin";
