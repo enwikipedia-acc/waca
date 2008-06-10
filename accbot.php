8a9ffafcb347f0139d3357a7eb0bc85146ab03e6
Cobi <cobi@jacobicarter.com>
2008-06-10 23:24:02 +0000

    Fix access.
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@506 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- accbot.php
+++ accbot.php
@@ -83,7 +83,8 @@
 		'Cobi-Laptop!*@2002:1828:8399:4000:21f:3bff:fe10:4ae3'		=> 'root',
 		'|Cobi|!*@2002:1828:8399:4000:21f:3bff:fe10:4ae3'		=> 'root',
 		'SQLDb!*@wikipedia/SQL'						=> 'root',
-		'Stwalkerster*!*@wikipedia/Stwalkerster'				=> 'developer',
+		'Stwalkerster*!*@wikipedia/Stwalkerster'			=> 'developer',
+		'Alexfusco5!*@wikimedia/Alexfusco5'				=> 'developer',
 		'*!*@*'								=> '*'
 		);
 
