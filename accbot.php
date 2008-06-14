e5e6f64c7efea3fb766b2823ff7b33531d94e8e0
SQL <sxwiki@gmail.com>
2008-06-14 18:56:16 +0000

    add deugging
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@544 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- accbot.php
+++ accbot.php
@@ -21,9 +21,9 @@
 
 	// Declares
 	declare( ticks=1 );
-	$lout = rand(1,30892); //seed
-	$lout2 = rand(1,30892);
-	$lout3 = rand(1,30892);
+	$lout = 1; //seed
+	$lout2 = 2;
+	$lout3 = 3;
 	// Defines
 
 	// Includes
@@ -152,13 +152,14 @@
 		global $lout; // Last output. If all three of these are the same, the bot won't speak.
 		global $lout2;
 		global $lout3;
-		if ( $lout != $lout2 && $lout != $lout3 && $lout2 != $lout3 && $data != $lout && $data != $lout2 && $data != $lout3 ) { //If ALL those are matching, we're probably being flooded, ignore it.
+		if ( $lout != $lout2 && $lout != $lout3 && $lout2 != $lout3 && $data != $lout ) { //If ALL those are matching, we're probably being flooded, ignore it.
 			fwrite( $fp, $data . "\r\n" ); 
 			$flooded = 0; //Future use, for 'Last message repeated X times
 		} else {
 			$flooded = 1;
 			$rptcnt++;
 		}
+		echo "lout - $lout, 2 - $lout2, 3 - $lout3, data - $data\n";
 		$lout3 = $lout2; //Move the data on down thru the variables.
 		$lout2 = $lout;
 		$lout = $data;
