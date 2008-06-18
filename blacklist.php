dcf5b87d7ae1ac43512b9b3c87468a638ada6771
Yetanotherx <yetanotherx@gmail.com>
2008-06-18 02:32:52 +0000

    Hrm.
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@585 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- blacklist.php
+++ blacklist.php
@@ -102,7 +102,7 @@ $emailblacklist[grawp8] = '/@rawks\.com/i';
 $emailblacklist[grawp9] = '/(fuck|sex|damn|shit|pedo)/i';
 $emailblacklist[webring] = '/webring@.*/i';
 
-$uablacklist[grawp1] = '/Mozilla\/4\.0 \(compatible; MSIE 7\.0; Windows NT 6\.0; SLCC1; \.NET CLR 2\.0\.50727; \.NET CLR 3\.0\.04506; InfoPath\.2; \.NET CLR 3\.5\.21022\)/'
+$uablacklist[grawp1] = '/Mozilla\/4\.0 \(compatible; MSIE 7\.0; Windows NT 6\.0; SLCC1; \.NET CLR 2\.0\.50727; \.NET CLR 3\.0\.04506; InfoPath\.2; \.NET CLR 3\.5\.21022\)/';
 
 //DNSBLS
 $dnsbls = array(
