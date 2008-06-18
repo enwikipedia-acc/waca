cec3422c5ebb92ebb5c277062e3bed651740b0f1
SQL <sxwiki@gmail.com>
2008-06-18 20:04:14 +0000

    there too
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@633 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-sfHover = function() {
-	var sfEls = document.getElementById("nav").getElementsByTagName("LI");
-	for (var i=0; i<sfEls.length; i++) {
-		sfEls[i].onmouseover=function() {
-			this.className+=" sfhover";
-		}
-		sfEls[i].onmouseout=function() {
-			this.className=this.className.replace(new RegExp(" sfhover\\b"), "");
-		}
-	}
-}
-if (window.attachEvent) window.attachEvent("onload", sfHover);
-
 function showhide(listid) {
 	list = document.getElementById(listid);
 	link = document.getElementById(listid + "-link");
