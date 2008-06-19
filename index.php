ea72bfe2cd123898b9982c4afb49df103990903b
SQL <sxwiki@gmail.com>
2008-06-19 18:10:45 +0000

    moar fix
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@659 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- index.php
+++ index.php
@@ -24,11 +24,14 @@ function sanitize($what) {
 	return($what);
 }
 function sendtobot($message) {
-	global $whichami;
-	sleep(3);
-	$fp = fsockopen("udp://nightshade.toolserver.org", 9001, $erno, $errstr, 30);
-	fwrite($fp, "[$whichami]: $message\r\n");
-	fclose($fp);
+        global $whichami;
+        sleep(3);
+        $fp = fsockopen("udp://91.198.174.201", 9001, $erno, $errstr, 30);
+        if (!$fp) {
+          echo "SOCKET ERROR: $errstr ($errno)<br />\n";
+        }
+        fwrite($fp, "[$whichami]: $message\r\n");
+        fclose($fp);
 }
 function checktor ($addr) {
 	$flags = array();
