efb4e3303443a49e24ac743d43b743eb1f6fb9ce
SQL <sxwiki@gmail.com>
2008-06-19 18:00:36 +0000

    debug trap
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@656 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- accbot.php
+++ accbot.php
@@ -507,7 +507,9 @@
 
 	if( ( $udpReader = pcntl_fork() ) == 0 ) {
 		$fpt = stream_socket_server( 'udp://0.0.0.0:9001', $errNo, $errStr, STREAM_SERVER_BIND );
-		
+		if (!fpt) {
+ 		 echo "SOCKET ERROR: $errstr ($errno)\n";
+		}		
 		while( !feof( $fp ) ) {
 			$data = fread( $fpt, 4096 );
 			if( $data != '' ) {
