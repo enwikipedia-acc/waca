62bdfd528bfc3b2be463b3bf183e34d2983dd807
SQL <sxwiki@gmail.com>
2008-06-22 10:22:00 +0000

    tired
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@714 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- users.php
+++ users.php
@@ -158,7 +158,7 @@ elseif ($_GET['list'] != "devs" && $_GET['list'] != "") {
 	if ($_GET['list'] == "admins") {
 		while ($row = mysql_fetch_a'ssoc($result)) {
 			if($row[u'ser_level] == "Admin") { 
-				echo "<h3>$row".['user_level']."</h3>\n";
+				echo "<h3>".$row['user_level']."</h3>\n";
 			
 			if($row['user_name'] != "") {
 				echo "<li><a href=\"users.php?viewuser=".$row['user_id']."\">".$row['user_name']."</a></li>\n";
