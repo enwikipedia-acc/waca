600fa60b1c5f7146111b59c2cf7e82594da7cc5a
OverlordQ <overlordq@gmail.com>
2008-07-01 23:59:03 +0000

    Fix request detail link.
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@781 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- users.php
+++ users.php
@@ -1,4 +1,5 @@
 <?php
+
 /**************************************************************
 ** English Wikipedia Account Request Interface               **
 ** Wikipedia Account Request Graphic Design by               **
@@ -18,223 +19,245 @@
 **OverlordQ ( http://en.wikipedia.org/wiki/User:OverlordQ )  **
 **                                                           **
 **************************************************************/
-require_once('config.inc.php');
-include('devlist.php');
+require_once ('config.inc.php');
+include ('devlist.php');
 function displayheader() {
-        global $toolserver_username;
-        global $toolserver_password;
-    global $toolserver_host;
-    global $toolserver_database;
-        mysql_connect($toolserver_host,$toolserver_username,$toolserver_password);
-        @mysql_select_db($toolserver_database) or print mysql_error();
-        $query = "SELECT * FROM acc_emails WHERE mail_id = '8';";
-        $result = mysql_query($query);
-        if(!$result) Die("ERROR: No result returned.");
-        $row = mysql_fetch_assoc($result);
-        echo $row['mail_text'];
+	global $toolserver_username;
+	global $toolserver_password;
+	global $toolserver_host;
+	global $toolserver_database;
+	mysql_connect($toolserver_host, $toolserver_username, $toolserver_password);
+	@ mysql_select_db($toolserver_database) or print mysql_error();
+	$query = "SELECT * FROM acc_emails WHERE mail_id = '8';";
+	$result = mysql_query($query);
+	if (!$result)
+		Die("ERROR: No result returned.");
+	$row = mysql_fetch_assoc($result);
+	echo $row['mail_text'];
 }
 function displayfooter() {
-        global $toolserver_username;
-        global $toolserver_password;
-    global $toolserver_host;
-    global $toolserver_database;
-        mysql_connect($toolserver_host,$toolserver_username,$toolserver_password);
-        @mysql_select_db($toolserver_database) or print mysql_error();
-        $query = "SELECT * FROM acc_emails WHERE mail_id = '7';";
-        $result = mysql_query($query);
-        if(!$result) Die("ERROR: No result returned.");
-        $row = mysql_fetch_assoc($result);
-    echo "</div>";
-        echo $row['mail_text'];
+	global $toolserver_username;
+	global $toolserver_password;
+	global $toolserver_host;
+	global $toolserver_database;
+	mysql_connect($toolserver_host, $toolserver_username, $toolserver_password);
+	@ mysql_select_db($toolserver_database) or print mysql_error();
+	$query = "SELECT * FROM acc_emails WHERE mail_id = '7';";
+	$result = mysql_query($query);
+	if (!$result)
+		Die("ERROR: No result returned.");
+	$row = mysql_fetch_assoc($result);
+	echo "</div>";
+	echo $row['mail_text'];
 }
 function showfooter() {
-    echo "</body></html>\n";
+	echo "</body></html>\n";
 }
 function sanitize($what) {
-        $what = mysql_real_escape_string($what);
-        return($what);
+	$what = mysql_real_escape_string($what);
+	return ($what);
 }
 
-mysql_connect($toolserver_host,$toolserver_username,$toolserver_password);
-@mysql_select_db($toolserver_database) or print mysql_error();
+mysql_connect($toolserver_host, $toolserver_username, $toolserver_password);
+@ mysql_select_db($toolserver_database) or print mysql_error();
 if ($_GET['viewuser'] != "") {
-    displayheader();
-    $gid = sanitize($_GET['viewuser']);
-    $query = "SELECT * FROM acc_user WHERE user_id = $gid AND user_level != 'Declined' AND user_level != 'New' ;";
-    $result = mysql_query($query);
-    if(!$result) Die("ERROR: No result returned.");
-    $row = mysql_fetch_assoc($result);
-    if($row['user_id'] == "") {
-        echo "Invalid user!<br />\n";
-        showfooter();
-        die();
-    }
-    echo "<h2>Detail report for user: ".$row['user_name']."</h2>\n";
-    echo "<ol>\n";
-    echo "<li>User ID: ".$row['user_id']."</li>\n";
-    echo "<li>User Level: ".$row['user_level']."</li>\n";
-    echo "<li>User On-wiki name: <a href=\"http://en.wikipedia.org/wiki/User:".$row['user_onwikiname']."\">".$row['user_onwikiname']."</a>  |  <a href=\"http://en.wikipedia.org/wiki/User talk:".$row['user_onwikiname']."\">talk page</a> </li>\n";
-    $query = 'SELECT `user_lastactive` AS `time` FROM `acc_user` WHERE `user_id` = \''.mysql_real_escape_string($_GET['viewuser']).'\' LIMIT 1;';
-    $result = mysql_query($query);
-    if(!$result) Die("ERROR: No result returned.");
-    $lastactive = mysql_fetch_assoc($result);
-    $lastactive = $lastactive['time'];
-    if($lastactive == "0000-00-00 00:00:00") {
-        echo "<li>User has never used the interface</li>\n";
-    } else {
-        echo "<li>User last active: $lastactive</li>\n";
-    }
-    $query = 'SELECT * FROM `acc_user` WHERE `user_id` = \''.mysql_real_escape_string($_GET['viewuser']).'\' LIMIT 1;';
-    $result = mysql_query($query);
-    if(!$result) Die("ERROR: No result returned.");
-    $welcomee = mysql_fetch_assoc($result);
-    $welcomee = $welcomee['user_welcome'];
-    if ($welcomee == "1") {
-        $welcomee = "Yes";
-    }
-    else {
-        $welcomee = "No";
-    }
-    echo "<li>User has <a href=\"acc.php?action=welcomeperf\"><span style=\"color: red;\" title=\"Login required to continue\">automatic welcoming</span></a> enabled: $welcomee.</li>\n";
-    echo "</ol>\n";
-    echo "<h2>Users created</h2>\n";
-        $query = "SELECT * FROM acc_log JOIN acc_user ON user_name = log_user JOIN acc_pend ON pend_id = log_pend WHERE user_id = '$gid' AND log_action = 'Closed 1';";
-        $result = mysql_query($query);
-        if(!$result) Die("ERROR: No result returned.");
-    echo "<ol>\n";
-        while($row = mysql_fetch_assoc($result)) {
-        if($row['log_time'] == "0000-00-00 00:00:00") { $row['log_time'] = "Date unknown"; }
-	$pn = $row['pend_name'];
-        echo "<li> <a href=\"http://en.wikipedia.org/wiki/User:$pn\">$pn</a> (<a href=\"http://en.wikipedia.org/wiki/User_talk:$pn\">talk</a> - <a href=\"http://en.wikipedia.org/wiki/Special:Contributions/$pn\">contribs</a> - <a href=\"$tsurl/acc.php?action=zoom&id=".$row['pend_id']."\"><span style = \"color: red;\" title=\"Login required to view request\">zoom</span></a>) at $row[log_time]</li>\n";
-        // Not every row $noc = count($row[pend_name]); //Define total number of users created
-    // Not every row echo "<b>Number of users created: $noc</b>\n"; //Display total number of users created
-    }
-    echo "</ol>\n";
-    echo "<h2>Users not created</h2>\n";
-        $query = "SELECT * FROM acc_log JOIN acc_user ON user_name = log_user JOIN acc_pend ON pend_id = log_pend WHERE user_id = '$gid' AND log_action != 'Closed 1';";
-        $result = mysql_query($query);
-        if(!$result) Die("ERROR: No result returned.");
-    echo "<ol>\n";
-        while($row = mysql_fetch_assoc($result)) {
-        if($row['log_time'] == "0000-00-00 00:00:00") { $row['log_time'] = "Date unknown"; }
-	$pn = $row['pend_name'];
-        echo "<li> <a href=\"http://en.wikipedia.org/wiki/User:$pn\">$pn</a> (<a href=\"http://en.wikipedia.org/wiki/User_talk:$pn\">talk</a> - <a href=\"http://en.wikipedia.org/wiki/Special:Contributions/$pn\">contribs</a> - <a href=\"$tsurl/acc.php?action=zoom&id=$pn\"><span style = \"color: red;\" title=\"Login required to view request\">zoom</span></a>) at ".$row['log_time']."</li>\n";
-    }
-    echo "<br /><a href=\"users.php\">User list</a><br /><a href=\"acc.php\"><span style=\"color: red;\" title=\"Login required to continue\">Return to request management interface</span></a>\n";
-    echo "</ol>\n";
-    displayfooter();
-    die();
+	displayheader();
+	$gid = sanitize($_GET['viewuser']);
+	$query = "SELECT * FROM acc_user WHERE user_id = $gid AND user_level != 'Declined' AND user_level != 'New' ;";
+	$result = mysql_query($query);
+	if (!$result)
+		Die("ERROR: No result returned.");
+	$row = mysql_fetch_assoc($result);
+	if ($row['user_id'] == "") {
+		echo "Invalid user!<br />\n";
+		showfooter();
+		die();
+	}
+	echo "<h2>Detail report for user: " . $row['user_name'] . "</h2>\n";
+	echo "<ol>\n";
+	echo "<li>User ID: " . $row['user_id'] . "</li>\n";
+	echo "<li>User Level: " . $row['user_level'] . "</li>\n";
+	echo "<li>User On-wiki name: <a href=\"http://en.wikipedia.org/wiki/User:" . $row['user_onwikiname'] . "\">" . $row['user_onwikiname'] . "</a>  |  <a href=\"http://en.wikipedia.org/wiki/User talk:" . $row['user_onwikiname'] . "\">talk page</a> </li>\n";
+	$query = 'SELECT `user_lastactive` AS `time` FROM `acc_user` WHERE `user_id` = \'' . mysql_real_escape_string($_GET['viewuser']) . '\' LIMIT 1;';
+	$result = mysql_query($query);
+	if (!$result)
+		Die("ERROR: No result returned.");
+	$lastactive = mysql_fetch_assoc($result);
+	$lastactive = $lastactive['time'];
+	if ($lastactive == "0000-00-00 00:00:00") {
+		echo "<li>User has never used the interface</li>\n";
+	} else {
+		echo "<li>User last active: $lastactive</li>\n";
+	}
+	$query = 'SELECT * FROM `acc_user` WHERE `user_id` = \'' . mysql_real_escape_string($_GET['viewuser']) . '\' LIMIT 1;';
+	$result = mysql_query($query);
+	if (!$result)
+		Die("ERROR: No result returned.");
+	$welcomee = mysql_fetch_assoc($result);
+	$welcomee = $welcomee['user_welcome'];
+	if ($welcomee == "1") {
+		$welcomee = "Yes";
+	} else {
+		$welcomee = "No";
+	}
+	echo "<li>User has <a href=\"acc.php?action=welcomeperf\"><span style=\"color: red;\" title=\"Login required to continue\">automatic welcoming</span></a> enabled: $welcomee.</li>\n";
+	echo "</ol>\n";
+	echo "<h2>Users created</h2>\n";
+	$query = "SELECT * FROM acc_log JOIN acc_user ON user_name = log_user JOIN acc_pend ON pend_id = log_pend WHERE user_id = '$gid' AND log_action = 'Closed 1';";
+	$result = mysql_query($query);
+	if (!$result)
+		Die("ERROR: No result returned.");
+	echo "<ol>\n";
+	while ($row = mysql_fetch_assoc($result)) {
+		if ($row['log_time'] == "0000-00-00 00:00:00") {
+			$row['log_time'] = "Date unknown";
+		}
+		$pn = $row['pend_name'];
+		echo "<li> <a href=\"http://en.wikipedia.org/wiki/User:$pn\">$pn</a> (<a href=\"http://en.wikipedia.org/wiki/User_talk:$pn\">talk</a> - <a href=\"http://en.wikipedia.org/wiki/Special:Contributions/$pn\">contribs</a> - <a href=\"$tsurl/acc.php?action=zoom&id=" . $row['pend_id'] . "\"><span style = \"color: red;\" title=\"Login required to view request\">zoom</span></a>) at $row[log_time]</li>\n";
+		// Not every row $noc = count($row[pend_name]); //Define total number of users created
+		// Not every row echo "<b>Number of users created: $noc</b>\n"; //Display total number of users created
+	}
+	echo "</ol>\n";
+	echo "<h2>Users not created</h2>\n";
+	$query = "SELECT * FROM acc_log JOIN acc_user ON user_name = log_user JOIN acc_pend ON pend_id = log_pend WHERE user_id = '$gid' AND log_action != 'Closed 1';";
+	$result = mysql_query($query);
+	if (!$result)
+		Die("ERROR: No result returned.");
+	echo "<ol>\n";
+	while ($row = mysql_fetch_assoc($result)) {
+		if ($row['log_time'] == "0000-00-00 00:00:00") {
+			$row['log_time'] = "Date unknown";
+		}
+		$pn = $row['pend_name'];
+		echo "<li> <a href=\"http://en.wikipedia.org/wiki/User:$pn\">$pn</a> (<a href=\"http://en.wikipedia.org/wiki/User_talk:$pn\">talk</a> - <a href=\"http://en.wikipedia.org/wiki/Special:Contributions/$pn\">contribs</a> - <a href=\"$tsurl/acc.php?action=zoom&id=" . $row['pend_id'] . "\"><span style = \"color: red;\" title=\"Login required to view request\">zoom</span></a>) at " . $row['log_time'] . "</li>\n";
+	}
+	echo "<br /><a href=\"users.php\">User list</a><br /><a href=\"acc.php\"><span style=\"color: red;\" title=\"Login required to continue\">Return to request management interface</span></a>\n";
+	echo "</ol>\n";
+	displayfooter();
+	die();
 }
 if ($_GET['list'] == "devs") {
-    displayheader();
-    echo "<h2>User List</h2>\n";
-    echo "<h3>Developers</h3>\n<ul>\n";
-	foreach ($regdevlist as $dev) {
-		echo "<li><a href=\"users.php?viewuser=".$dev['2']."\">".$dev['0']."</a></li>\n";
-	}
-	echo "<ul>\n";
-	echo "<br /><a href=\"users.php\">User list</a><br /><a href=\"acc.php\"><span style=\"color: red;\" title=\"Login required to continue\">Return to request management interface</span></a>\n";
-    displayfooter();
-    die();
-}
-elseif ($_GET['list'] != "devs" && $_GET['list'] != "") {
-    displayheader();
-	$query = "SELECT * FROM acc_user ORDER BY user_level";
-	$result = mysql_query($query);
-	if(!$result) Die("ERROR: No result returned.");
+	displayheader();
 	echo "<h2>User List</h2>\n";
-	if ($_GET['list'] == "users") {
-		while ($row = mysql_fetch_assoc($result)) {
-			if($row['user_level'] == "User") { 
-				echo "<h3>".$row['user_level']."</h3>\n";
-			
-			if($row['user_name'] != "") {
-				echo "<li><a href=\"users.php?viewuser=".$row['user_id']."\">".$row['user_name']."</a></li>\n";
-			}
-			}
-			$lastlevel = $row['user_level'];
-		} 
-	}
-	if ($_GET['list'] == "admins") {
-		while ($row = mysql_fetch_assoc($result)) {
-			if($row['user_level'] == "Admin") { 
-				echo "<h3>".$row['user_level']."</h3>\n";
-			
-			if($row['user_name'] != "") {
-				echo "<li><a href=\"users.php?viewuser=".$row['user_id']."\">".$row['user_name']."</a></li>\n";
-			}
-			}
-			$lastlevel = $row['user_level'];
-		} 
-	}
-	if ($_GET['list'] == "suspended") {
-		while ($row = mysql_fetch_assoc($result)) {
-			if($row['user_level'] == "Suspended") { 
-				echo "<h3>".$row['user_level']."</h3>\n";
-			
-			if($row['user_name'] != "") {
-				echo "<li><a href=\"users.php?viewuser=".$row['user_id']."\">".$row['user_name']."</a></li>\n";
-			}
-			}
-			$lastlevel = $row['user_level'];
-		} 
-	}
-	if ($_GET['list'] == "declined") {
-		while ($row = mysql_fetch_assoc($result)) {
-			if($row['user_level'] == "Declined") { 
-				echo "<h3>".$row['user_level']."</h3>\n";
-			
-			if($row['user_name'] != "") {
-				echo "<li><a href=\"users.php?viewuser=".$row['user_id']."\">".$row['user_name']."</a></li>\n";
-			}
-			}
-			$lastlevel = $row['user_level'];
-		} 
-	}
-	if ($_GET['list'] == "new") {
-		while ($row = mysql_fetch_assoc($result)) {
-			if($row['user_level'] == "New") { 
-				echo "<h3>".$row['user_level']."</h3>\n";
-			
-			if($row['user_name'] != "") {
-				echo "<li><a href=\"users.php?viewuser=".$row['user_id']."\">".$row['user_name']."</a></li>\n";
-			}
-			}
-			$lastlevel = $row['user_level'];
-		} 
+	echo "<h3>Developers</h3>\n<ul>\n";
+	foreach ($regdevlist as $dev) {
+		echo "<li><a href=\"users.php?viewuser=" . $dev['2'] . "\">" . $dev['0'] . "</a></li>\n";
 	}
 	echo "<ul>\n";
 	echo "<br /><a href=\"users.php\">User list</a><br /><a href=\"acc.php\"><span style=\"color: red;\" title=\"Login required to continue\">Return to request management interface</span></a>\n";
 	displayfooter();
 	die();
 }
-else {
-displayheader();
-$query = "SELECT * FROM acc_user ORDER BY user_level";
-$result = mysql_query($query);
-if(!$result) Die("ERROR: No result returned.");
-echo "<h2>User List</h2>\n";
-echo "<i>Developers are bolded</i>\n<ul>\n";
-while ($row = mysql_fetch_assoc($result)) {
-    if($row['user_level'] != $lastlevel && $row['user_level'] != "Suspended" && $row['user_level'] != "Declined") { echo "<h3>".$row['user_level']."</h3>\n"; }
-    if($row['user_level'] == "Suspended") { $row['user_name'] = ""; }
-    if($row['user_level'] == "Declined") { $row['user_name'] = ""; }
-    if($row['user_level'] == "New") { $row['user_name'] = ""; }
-    if($row['user_name'] != "") {
-        echo "<li><a href=\"users.php?viewuser=".$row['user_id']."\">";
-        $uid = array($row['user_name'], $row['user_onwikiname'], $row['user_id']);
-        if (in_array($uid, $regdevlist)) {
-        	echo "<b>".$row['user_name']."</b>";
-        }
-        else {
-        	echo $row['user_name'];
-        }
-        echo "</a></li>\n";
-    }
-    $lastlevel = $row['user_level'];
-}
-echo "<ul>\n";
-echo "<br /><a href=\"users.php\">User list</a><br /><a href=\"acc.php\"><span style=\"color: red;\" title=\"Login required to continue\">Return to request management interface</span></a>\n";
-displayfooter();
+elseif ($_GET['list'] != "devs" && $_GET['list'] != "") {
+	displayheader();
+	$query = "SELECT * FROM acc_user ORDER BY user_level";
+	$result = mysql_query($query);
+	if (!$result)
+		Die("ERROR: No result returned.");
+	echo "<h2>User List</h2>\n";
+	if ($_GET['list'] == "users") {
+		while ($row = mysql_fetch_assoc($result)) {
+			if ($row['user_level'] == "User") {
+				echo "<h3>" . $row['user_level'] . "</h3>\n";
+
+				if ($row['user_name'] != "") {
+					echo "<li><a href=\"users.php?viewuser=" . $row['user_id'] . "\">" . $row['user_name'] . "</a></li>\n";
+				}
+			}
+			$lastlevel = $row['user_level'];
+		}
+	}
+	if ($_GET['list'] == "admins") {
+		while ($row = mysql_fetch_assoc($result)) {
+			if ($row['user_level'] == "Admin") {
+				echo "<h3>" . $row['user_level'] . "</h3>\n";
+
+				if ($row['user_name'] != "") {
+					echo "<li><a href=\"users.php?viewuser=" . $row['user_id'] . "\">" . $row['user_name'] . "</a></li>\n";
+				}
+			}
+			$lastlevel = $row['user_level'];
+		}
+	}
+	if ($_GET['list'] == "suspended") {
+		while ($row = mysql_fetch_assoc($result)) {
+			if ($row['user_level'] == "Suspended") {
+				echo "<h3>" . $row['user_level'] . "</h3>\n";
+
+				if ($row['user_name'] != "") {
+					echo "<li><a href=\"users.php?viewuser=" . $row['user_id'] . "\">" . $row['user_name'] . "</a></li>\n";
+				}
+			}
+			$lastlevel = $row['user_level'];
+		}
+	}
+	if ($_GET['list'] == "declined") {
+		while ($row = mysql_fetch_assoc($result)) {
+			if ($row['user_level'] == "Declined") {
+				echo "<h3>" . $row['user_level'] . "</h3>\n";
+
+				if ($row['user_name'] != "") {
+					echo "<li><a href=\"users.php?viewuser=" . $row['user_id'] . "\">" . $row['user_name'] . "</a></li>\n";
+				}
+			}
+			$lastlevel = $row['user_level'];
+		}
+	}
+	if ($_GET['list'] == "new") {
+		while ($row = mysql_fetch_assoc($result)) {
+			if ($row['user_level'] == "New") {
+				echo "<h3>" . $row['user_level'] . "</h3>\n";
+
+				if ($row['user_name'] != "") {
+					echo "<li><a href=\"users.php?viewuser=" . $row['user_id'] . "\">" . $row['user_name'] . "</a></li>\n";
+				}
+			}
+			$lastlevel = $row['user_level'];
+		}
+	}
+	echo "<ul>\n";
+	echo "<br /><a href=\"users.php\">User list</a><br /><a href=\"acc.php\"><span style=\"color: red;\" title=\"Login required to continue\">Return to request management interface</span></a>\n";
+	displayfooter();
+	die();
+} else {
+	displayheader();
+	$query = "SELECT * FROM acc_user ORDER BY user_level";
+	$result = mysql_query($query);
+	if (!$result)
+		Die("ERROR: No result returned.");
+	echo "<h2>User List</h2>\n";
+	echo "<i>Developers are bolded</i>\n<ul>\n";
+	while ($row = mysql_fetch_assoc($result)) {
+		if ($row['user_level'] != $lastlevel && $row['user_level'] != "Suspended" && $row['user_level'] != "Declined") {
+			echo "<h3>" . $row['user_level'] . "</h3>\n";
+		}
+		if ($row['user_level'] == "Suspended") {
+			$row['user_name'] = "";
+		}
+		if ($row['user_level'] == "Declined") {
+			$row['user_name'] = "";
+		}
+		if ($row['user_level'] == "New") {
+			$row['user_name'] = "";
+		}
+		if ($row['user_name'] != "") {
+			echo "<li><a href=\"users.php?viewuser=" . $row['user_id'] . "\">";
+			$uid = array (
+				$row['user_name'],
+				$row['user_onwikiname'],
+				$row['user_id']
+			);
+			if (in_array($uid, $regdevlist)) {
+				echo "<b>" . $row['user_name'] . "</b>";
+			} else {
+				echo $row['user_name'];
+			}
+			echo "</a></li>\n";
+		}
+		$lastlevel = $row['user_level'];
+	}
+	echo "<ul>\n";
+	echo "<br /><a href=\"users.php\">User list</a><br /><a href=\"acc.php\"><span style=\"color: red;\" title=\"Login required to continue\">Return to request management interface</span></a>\n";
+	displayfooter();
 }
 ?>
