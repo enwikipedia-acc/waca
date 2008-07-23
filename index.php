895e732f42b504f5c8389be422ff208397f102bd
SQL <sxwiki@gmail.com>
2008-07-23 15:44:43 +0000

    what Atomican said
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@1012 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- index.php
+++ index.php
@@ -482,7 +482,8 @@ if (isset ($_POST['name']) && isset ($_POST['email'])) {
 	$row = mysql_fetch_assoc($result);
 	$pid = $row['pend_id'];
 	$pem = $row['pend_email'];
-	sendtobot("[[acc:$pid]] N $tsurl/acc.php?action=zoom&id=$pid /* " . $_POST['name'] . " */ " . substr(str_replace(array (
+	if( $uLevel == "Open" ) { $what = "User"; } else { $what = "Admin"; }
+	sendtobot("[[acc:$pid]] N $tsurl/acc.php?action=zoom&id=$pid /* " . $_POST['name'] . " */ $what NEEDED " . substr(str_replace(array (
 		"\n",
 		"\r"
 	), array (
