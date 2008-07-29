bb5f90444307713a68185ce5e6eca80bbb0dc0f9
Prodego <Prodego@gmail.com>
2008-07-29 00:06:15 +0000

    move sitenotice script to script.js
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@1047 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- script.js
+++ script.js
@@ -11,3 +11,31 @@ function showhide(listid) {
 	}
 	
 }
+
+var cookieName = "dismissSiteNotice=";
+var cookiePos = document.cookie.indexOf(cookieName);
+var cookieValue = "";
+var msgClose = "Dismiss";
+
+if (cookiePos > -1) {
+	cookiePos = cookiePos + cookieName.length;
+	var endPos = document.cookie.indexOf(";", cookiePos);
+	if (endPos > -1) {
+		cookieValue = document.cookie.substring(cookiePos, endPos);
+	} else {
+		cookieValue = document.cookie.substring(cookiePos);
+	}
+}
+if (cookieValue != siteNoticeID) {
+	function dismissNotice() {
+		var date = new Date();
+		date.setTime(date.getTime() + 7*24*60*60*1000);
+		document.cookie = cookieName + siteNoticeID + "; expires="+date.toGMTString() + "; path=/";
+		var element = document.getElementById('sitenotice');
+		element.style.display = "none";
+	}
+	document.writeln('	<div id="sitenotice">');
+	document.writeln('<span title="Hide sitenotice for one week" id="dismiss"><a href="javascript:dismissNotice();">['+msgClose+']</a></span>');
+	document.writeln(siteNoticeValue);
+	document.writeln('</div>');
+}
