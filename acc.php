4cdf2d0daf27d55689d53ad6e641579831aeed35
SQL <sxwiki@gmail.com>
2008-07-30 08:13:09 +0000

    try this.
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@1112 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- acc.php
+++ acc.php
@@ -49,7 +49,7 @@ if ( !isset ( $_SESSION['user'] ) && !isset ( $_GET['nocheck'] ) ) {
 		die( );
 	}
 }
-elseif ( !isset ( $_GET['action'] ) || $_GET['action'] != "login" || $_GET['action'] != "register" || $_GET['action'] != "sreg" ) {
+elseif ( !isset ( $_GET['nocheck'] ) && $_GET['action'] == 'login' ) {
 	echo makehead( $_SESSION['user'] );
 	checksecurity( $_SESSION['user'] );
 	$out = showmessage( '20' );
