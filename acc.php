ce974455b74cb03dd3d27c9763e185d869aae0e9
SQL <sxwiki@gmail.com>
2008-07-30 08:09:00 +0000

    moar fix
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@1111 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- acc.php
+++ acc.php
@@ -49,7 +49,7 @@ if ( !isset ( $_SESSION['user'] ) && !isset ( $_GET['nocheck'] ) ) {
 		die( );
 	}
 }
-else {
+elseif ( !isset ( $_GET['action'] ) || $_GET['action'] != "login" || $_GET['action'] != "register" || $_GET['action'] != "sreg" ) {
 	echo makehead( $_SESSION['user'] );
 	checksecurity( $_SESSION['user'] );
 	$out = showmessage( '20' );
