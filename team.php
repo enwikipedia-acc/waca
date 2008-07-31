7b441cc3f8077ec998c303d4f2d94098bb5defb3
SQL <sxwiki@gmail.com>
2008-07-31 02:44:21 +0000

    add team
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@1139 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

---
+++ team.php
@@ -0,0 +1,148 @@
+<?php
+require_once ( 'config.inc.php' );
+require_once ( 'devlist.php' );
+require_once ( 'functions.php' );
+
+//FIXME: displayheader() Stolen from users.php... Maybe these need to be in functions.php?
+function displayheader() {
+	global $toolserver_username;
+	global $toolserver_password;
+	global $toolserver_host;
+	global $toolserver_database;
+	mysql_connect($toolserver_host, $toolserver_username, $toolserver_password);
+	@ mysql_select_db($toolserver_database) or print mysql_error();
+	$query = "SELECT * FROM acc_emails WHERE mail_id = '8';";
+	$result = mysql_query($query);
+	if (!$result)
+		Die("ERROR: No result returned.");
+	$row = mysql_fetch_assoc($result);
+	echo $row['mail_text'];
+}
+//FIXME: displayfooter() Stolen from users.php... Maybe these need to be in functions.php?
+function displayfooter() {
+	global $toolserver_username;
+	global $toolserver_password;
+	global $toolserver_host;
+	global $toolserver_database;
+	mysql_connect($toolserver_host, $toolserver_username, $toolserver_password);
+	@ mysql_select_db($toolserver_database) or print mysql_error();
+	$query = "SELECT * FROM acc_emails WHERE mail_id = '7';";
+	$result = mysql_query($query);
+	if (!$result)
+		Die("ERROR: No result returned.");
+	$row = mysql_fetch_assoc($result);
+	echo "</div>";
+	echo $row['mail_text'];
+}
+
+$developer = //Developer info / list.
+	array(
+		"SQL" =>
+			array(						//Set any of these to NULL to keep them from being displayed.
+				"IRC" => "SQLDb", 			//IRC Name
+				"EMail" => "sxwiki@gmail.com", 		//Public E-mail address
+				"wiki" => "SQL", 			//Enwiki Username
+				"WWW" => "http://toolserver.org/~sql", 	//Your website
+				"Name" => NULL,				//Real name
+				"Role" => "Project Lead",		//Project Role(s)
+				"Access" => "Shell, SQL, Commit",	//Project Access levels
+				"Other" => NULL,			//Anything else, comments, etc.
+			),
+		"Cobi" =>
+			array(
+				"IRC" => NULL,
+				"EMail" => NULL,
+				"wiki" => NULL,
+				"WWW" => NULL,
+				"Name" => NULL,
+				"Role" => NULL,
+				"Access" => NULL,
+				"Other" => NULL,
+			),
+		"charlie" =>
+			array(
+				"IRC" => NULL,
+				"EMail" => NULL,
+				"wiki" => NULL,
+				"WWW" => NULL,
+				"Name" => NULL,
+				"Role" => NULL,
+				"Access" => NULL,
+				"Other" => NULL,
+			),
+		"FastLizard4" =>
+			array(
+				"IRC" => NULL,
+				"EMail" => NULL,
+				"wiki" => NULL,
+				"WWW" => NULL,
+				"Name" => NULL,
+				"Role" => NULL,
+				"Access" => NULL,
+				"Other" => NULL,
+			),
+		"Stwalkerster" =>
+			array(
+				"IRC" => NULL,
+				"EMail" => NULL,
+				"wiki" => NULL,
+				"WWW" => NULL,
+				"Name" => NULL,
+				"Role" => NULL,
+				"Access" => NULL,
+				"Other" => NULL,
+			),
+		"Soxred93" =>
+			array(
+				"IRC" => NULL,
+				"EMail" => NULL,
+				"wiki" => NULL,
+				"WWW" => NULL,
+				"Name" => NULL,
+				"Role" => NULL,
+				"Access" => NULL,
+				"Other" => NULL,
+			),
+		"Alexfusco5" =>
+			array(
+				"IRC" => NULL,
+				"EMail" => NULL,
+				"wiki" => NULL,
+				"WWW" => NULL,
+				"Name" => NULL,
+				"Role" => NULL,
+				"Access" => NULL,
+				"Other" => NULL,
+			),
+		"OverlordQ" =>
+			array(
+				"IRC" => NULL,
+				"EMail" => NULL,
+				"wiki" => NULL,
+				"WWW" => NULL,
+				"Name" => NULL,
+				"Role" => NULL,
+				"Access" => NULL,
+				"Other" => NULL,
+			),
+		"Prodego" =>
+			array(
+				"IRC" => NULL,
+				"EMail" => NULL,
+				"wiki" => NULL,
+				"WWW" => NULL,
+				"Name" => NULL,
+				"Role" => NULL,
+				"Access" => NULL,
+				"Other" => NULL,
+			),
+	);
+
+
+displayheader();
+echo "<h2>ACC Development Team</h2>\n";
+echo "<pre>\n";
+print_r($developer);
+echo "</pre>\n";
+displayfooter();
+?>
