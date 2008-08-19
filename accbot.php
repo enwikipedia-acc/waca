4e4c09816617a7fbc949c72677b05b1ef763cd73
Prodego <Prodego@gmail.com>
2008-08-19 06:13:55 +0000

    restore restart to where it belongs (priv esc)
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@1534 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- accbot.php
+++ accbot.php
@@ -111,12 +111,13 @@
 
 	$privgroups[ 'developer' ]                  = $privgroups['*']; // 'developer' inherits '*'.
 	$privgroups[ 'developer' ][ 'sand-svnup'  ] = 1;
-	$privgroups[ 'developer' ][ 'restart'     ] = 1;
 
 	$privgroups[ 'root'      ]                  = $privgroups['developer']; // 'root' inherits 'developer'.
 	$privgroups[ 'root'      ][ 'svnup'       ] = 1;
 	$privgroups[ 'root'      ][ 'recreatesvn' ] = 1;
 	$privgroups[ 'root'      ][ 'svn'         ] = 1;
+	$privgroups[ 'developer' ][ 'restart'     ] = 1;
+
 
 	// Functions
 	function sanitize( $data ) {
