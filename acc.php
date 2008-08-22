ab03652af8ffb354c788170a956bc00ec87c3654
FunPika <funpikawiki@gmail.com>
2008-08-22 20:27:54 +0000

    um...why is ending the contents of if unexpected? O_O
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@1591 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- acc.php
+++ acc.php
@@ -1304,12 +1304,13 @@ elseif ($action == "done" && $_GET['id'] != "") {
 		if (!$result)
 			Die("Query failed: $query ERROR: " . mysql_error());
 	}
+
 	if ($row2['pend_status'] = Admin) {
         http_redirect(acc.php#admin);
-        }
-        else {
+        } else {
         http_redirect(acc.php#open);
         }
+
 	$query = "UPDATE acc_pend SET pend_status = 'Closed' WHERE pend_id = '$gid';";
 	$result = mysql_query($query);
 	if (!$result)
