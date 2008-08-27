0c81012cbfa6da752027db7e30f02213c3bfe90e
Simon Walker <simon@stwalkerster.co.uk>
2008-08-27 22:20:17 +0000

    add search possibility
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@1622 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

---
+++ search.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once( 'config.inc.php' );
+require_once ( 'functions.php' );
+
+$link = mysql_connect( $toolserver_host, $toolserver_username, $toolserver_password );
+if ( !$link ) {
+	die( 'Could not connect: ' . mysql_error( ) );
+}
+@ mysql_select_db( $toolserver_database ) or print mysql_error( );
+
+echo makehead($_SESSION['user']);
+echo '<div id="content">';
+echo "<h3>Searching for: $_GET[email] ...</h3>";
+	$query = "SELECT pend_id, pend_name, pend_email FROM acc_pend WHERE pend_email LIKE '".$_GET['email']."';";
+	$result = mysql_query($query);
+	if (!$result)
+		Die("Query failed: $query ERROR: " . mysql_error());
+	$html .= "<table cellspacing=\"0\">\n";
+	$currentrow = 0;
+	while ( list( $pend_id, $pend_name, $pend_email ) = mysql_fetch_row( $result ) ) {
+		$currentrow += 1;
+		$out = '<tr';
+		if ($currentrow % 2 == 0) {
+			$out .= ' class="even">';
+		} else {
+			$out .= ' class="odd">';
+		}
+		$out .= "<td><b>$currentrow.</b></td><td><small><a style=\"color:blue\" href=\"acc.php?action=zoom&amp;id=" . $pend_id . "\">Request " . $pend_id . "</a></small></tr>";
+		$html .= $out;
+	}
+	$html .= "</table>\n";
+	$html .= "<b>Results found: </b> $currentrow.";
+echo $html;
+echo "</div>";
+echo showfooter();
+?>
