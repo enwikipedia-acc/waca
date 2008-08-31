35969bc586cf734bfbab15362f575624cd2cc779
FunPika <funpikawiki@gmail.com>
2008-08-31 00:07:35 +0000

    Eol style native and header on search.php.
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@1627 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- search.php
+++ search.php
@@ -1,5 +1,26 @@
 <?php
 
+/**************************************************************
+** English Wikipedia Account Request Interface               **
+** Wikipedia Account Request Graphic Design by               **
+** Charles Melbye is licensed under a Creative               **
+** Commons Attribution-Noncommercial-Share Alike             **
+** 3.0 United States License. All other code                 **
+** released under Public Domain by the ACC                   **
+** Development Team.                                         **
+**             Developers:                                   **
+**  SQL ( http://en.wikipedia.org/User:SQL )                 **
+**  Cobi ( http://en.wikipedia.org/User:Cobi )               **
+** Cmelbye ( http://en.wikipedia.org/User:cmelbye )          **
+**FastLizard4 ( http://en.wikipedia.org/User:FastLizard4 )   **
+**Stwalkerster ( http://en.wikipedia.org/User:Stwalkerster ) **
+**Soxred93 ( http://en.wikipedia.org/User:Soxred93)          **
+**Alexfusco5 ( http://en.wikipedia.org/User:Alexfusco5)      **
+**OverlordQ ( http://en.wikipedia.org/wiki/User:OverlordQ )  **
+**Prodego    ( http://en.wikipedia.org/wiki/User:Prodego )   **
+**FunPika    ( http://en.wikipedia.org/wiki/User:FunPika )   **
+**************************************************************/
+
 require_once( 'config.inc.php' );
 require_once ( 'functions.php' );
 
