1d446115532fa6b796a37f8ecbb1997c4be77e20
Prodego <Prodego@gmail.com>
2008-09-20 20:01:37 +0000

    keep it in <tr>
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@1710 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- functions.php
+++ functions.php
@@ -483,7 +483,7 @@ function listrequests($type, $hideip) {
 			}
 			// Username U:
 			$duname = _utf8_decode($pend_name);
-			$out .= '</small></td> ] <td><small><a class="request-req" href="http://en.wikipedia.org/wiki/User:' . $uname . '" target="_blank"><strong>' . $duname . '</strong></a> ';
+			$out .= '</small> ] </td><td><small><a class="request-req" href="http://en.wikipedia.org/wiki/User:' . $uname . '" target="_blank"><strong>' . $duname . '</strong></a> ';
 
 			// Creation log
 			$out .= '</small></td><td><small>(<a class="request-req" href="http://en.wikipedia.org/w/index.php?title=Special:Log&amp;type=newusers&amp;user=&amp;page=User:';
