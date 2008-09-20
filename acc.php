2e5c123d6f089886111c6393af570bffb84c5cc8
Prodego <Prodego@gmail.com>
2008-09-20 18:53:22 +0000

    fix /another/ undefined var
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@1698 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- acc.php
+++ acc.php
@@ -685,7 +685,7 @@ elseif ($action == "usermgmt") {
 	if (isset ($_GET['demote'])) {
 		$did = sanitize($_GET['demote']);
 		$siuser = sanitize($_SESSION['user']);
-		if ($_POST['demotereason'] == "") {
+		if (!isset($_POST['demotereason'])) {
 			echo "<h2>Demote Reason</h2><strong>The reason you enter here will be shown in the log. Please keep this in mind.</strong><br />\n<form action=\"acc.php?action=usermgmt&amp;demote=$did\" method=\"post\"><br />\n";
 			echo "<textarea name=\"demotereason\" rows=\"20\" cols=\"60\"></textarea><br />\n";
 			echo "<input type=\"submit\"><input type=\"reset\"/><br />\n";
