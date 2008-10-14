dfea133c2f9f2a20bdd0b880422401b8089a5e94
FunPika <funpikawiki@gmail.com>
2008-10-14 21:51:07 +0000

    Making 2 fields null because otherwise it is impossible to register users in a local install (says no value for those 2 fields on attempt).
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@1823 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- sql/acc.sql
+++ sql/acc.sql
@@ -38,8 +38,8 @@
 `user_welcome` INT( 11 ) NOT NULL DEFAULT 0 ,
 `user_welcome_sig` VARCHAR( 4096 ) NOT NULL ,
 `user_welcome_template` VARCHAR( 1024 ) NOT NULL ,
-`user_lastactive` DATETIME NOT NULL ,
-`user_forcelogout` INT( 3 ) NOT NULL ,
+`user_lastactive` DATETIME NULL DEFAULT NULL ,
+`user_forcelogout` INT( 3 ) NULL DEFAULT NULL ,
 `user_secure` INT( 11 ) NOT NULL DEFAULT 0
 ) ENGINE = InnoDB; 
 
---
+++ sql/patch-2usertablenull.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `acc_user` CHANGE `user_forcelogout` `user_forcelogout` INT( 3 ) NULL DEFAULT NULL ;
+ALTER TABLE `acc_user` CHANGE `user_lastactive` `user_lastactive` DATETIME NULL DEFAULT NULL ;
