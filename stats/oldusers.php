acf28730a31adbed1c46094b6b4a7571230003d1
Simon Walker <simon@stwalkerster.co.uk>
2008-12-08 12:41:30 +0000

    modify my info add header, footer, and description to oldusers script
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@1868 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- stats/oldusers.php
+++ stats/oldusers.php
@@ -6,6 +6,7 @@
 */
 
 require_once ('../config.inc.php');
+require_once ( '../functions.php' );
 
 mysql_connect($toolserver_host, $toolserver_username, $toolserver_password);
 @ mysql_select_db($toolserver_database) or print mysql_error();
@@ -29,8 +30,12 @@ ORDER BY user_lastactive ASC
 $result = mysql_query($query);
 if (!$result)
 	Die("ERROR: No result returned.");
+displayheader();
+echo '<h2>Old user accounts</h2>This list contains the usernames of all accounts that have not logged in in the past 45 days.';
+
 echo "<table><tr><th>User ID</th><th>Tool Username</th><th>User access level</th><th>enwiki username</th><th>Last activity</th></tr>";
 while ($r = mysql_fetch_assoc($result)) {
 	echo "<tr><th>".$r['tooluserid']."</th><td>".$r[tooluser]."</td><td>".$r['toolaccesslevel']."</td><td>".$r['enwikiuser']."</td><td>".$r['lasttoollogon']."</td></tr>";
 }
 echo "</table>";
+displayfooter();
