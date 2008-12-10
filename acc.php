32bf551b94e29444174855b58b063bfbc0459793
Prodego <Prodego@gmail.com>
2008-12-10 00:56:11 +0000

    Fix ban types, htmlencode the ban reason and type
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@1896 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- acc.php
+++ acc.php
@@ -558,9 +558,11 @@ elseif ($action == "sban" && $_GET['user'] != "") {
 		$duration = $duration +time();
 	}
 	$siuser = sanitize($_SESSION['user']);
-	$reason = sanitize($_POST['banreason']);
-	$target = sanitize($_GET['target']);
+	$reason = sanitize(htmlentities(($_POST['banreason']));
+	$target = sanitize(htmlentities(($_GET['target']));
 	$type = sanitize($_GET['type']);
+	if($type != "IP" || $type != "name" || $type != "EMail")
+		die(Invalid ban reason!);
 	$now = date("Y-m-d H-i-s");
 	upcsum($target);
 	$query = "INSERT INTO acc_log (log_pend, log_user, log_action, log_time) VALUES ('$target', '$siuser', 'Banned', '$now');";
