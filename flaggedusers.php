2be5b8276aa9ad22badff9c6e3350aeaf23d82e9
Simon Walker <simon@stwalkerster.co.uk>
2008-12-13 23:09:04 +0000

    add two stats files which do nothing at the moment, but I'm intending to let them do something. Descriptions are within each file.
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@1918 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

---
+++ flaggedusers.php
@@ -0,0 +1,5 @@
+<?php 
+
+/* Which users on enwiki are flagged with accountcreator, compared to users on the tool. */
+
+?>
---
+++ nonexistantrequests.php
@@ -0,0 +1,23 @@
+<?php
+// taken, or created requests which are not existant
+
+// for taken,
+// log_action == "Closed 3"
+// for created,
+// log_action == "Closed 1"
+
+/*
+
+SELECT acc_pend.pend_id, acc_pend.pend_name, acc_log.log_action
+FROM acc_pend
+INNER JOIN acc_log ON acc_pend.pend_id = acc_log.log_pend
+WHERE acc_pend.pend_status = "Closed"
+AND ( acc_log.log_action = "Closed 1"
+      OR acc_log.log_action = "Closed 3" )
+;
+
+
+api.php?action=query&list=users&ususers=Stwalkerster|Stwalkersock2&usprop=groups|editcount&format=php
+
+*/
+?>
