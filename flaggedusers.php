2f4985780e35409fbd6ed428ac3cdfab175d2aa1
Simon Walker <simon@stwalkerster.co.uk>
2008-12-19 16:35:37 +0000

    put some code behind script, check this works with two mysql server links.
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@1922 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- flaggedusers.php
+++ flaggedusers.php
@@ -2,4 +2,30 @@
 
 /* Which users on enwiki are flagged with accountcreator, compared to users on the tool. */
 
+/*
+mysql -h sql-s1
+use enwiki_p;
+select g.ug_user, n.user_name from user_groups g inner join user_ids n on g.ug_user=n.user_id where ug_group = "accountcreator";
+*/
+
+require_once('config.inc.php');
+
+$wikilink = mysql_connect($antispoof_host, $toolserver_username, $toolserver_password, true);
+$acclink = mysql_connect($toolserver_host,$toolserver_username, $toolserver_password, true);
+@mysql_select_db($antispoof_db, $wikilink);
+@mysql_select_db($toolserver_database, $acclink);
+
+$query = 'select g.ug_user, n.user_name from user_groups g inner join user_ids n on g.ug_user=n.user_id where ug_group = "accountcreator";';
+$results = mysql_query($query,$wikilink) or die();
+
+echo "<h2>List of users on enwiki with accountcreator flag</h2><table>";
+echo "<tr><th>en.wiki User ID</th><th>en.wiki Username</th></tr>";
+while($row = mysql_fetch_assoc($results))
+{
+echo "<tr><th>".$row['g.ug_user']."</th><td>".$row['n.user_name']."</td></tr>";
+}
+
+
+
+echo "</table>";
 ?>
