c99765bac4d2d8ee2531ccb13f0adff5e939eec3
Simon Walker <simon@stwalkerster.co.uk>
2008-12-22 01:25:36 +0000

    change header/footer, add access rights check (User | Admin)
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@1937 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- topcreators.php
+++ topcreators.php
@@ -1,10 +1,42 @@
 <?php
-
+/**************************************************************
+** English Wikipedia Account Request Interface               **
+** Wikipedia Account Request Graphic Design by               **
+** Charles Melbye is licensed under a Creative               **
+** Commons Attribution-Noncommercial-Share Alike             **
+** 3.0 United States License. All other code                 **
+** released under Public Domain by the ACC                   **
+** Development Team.                                         **
+**             Developers:                                   **
+**  SQL ( http://en.wikipedia.org/User:SQL )                 **
+**  Cobi ( http://en.wikipedia.org/User:Cobi )               **
+** Cmelbye ( http://en.wikipedia.org/User:cmelbye )          **
+**FastLizard4 ( http://en.wikipedia.org/User:FastLizard4 )   **
+**Stwalkerster ( http://en.wikipedia.org/User:Stwalkerster ) **
+**Soxred93 ( http://en.wikipedia.org/User:Soxred93)          **
+**Alexfusco5 ( http://en.wikipedia.org/User:Alexfusco5)      **
+**OverlordQ ( http://en.wikipedia.org/wiki/User:OverlordQ )  **
+**Prodego    ( http://en.wikipedia.org/wiki/User:Prodego )   **
+**FunPika    ( http://en.wikipedia.org/wiki/User:FunPika )   **
+**************************************************************/
 require_once ('config.inc.php');
 require_once('functions.php');
 
+session_start();
+
 mysql_connect($toolserver_host, $toolserver_username, $toolserver_password);
 @ mysql_select_db($toolserver_database) or print mysql_error();
+
+if( isset( $_SESSION['user'] ) ) {
+	$sessionuser = $_SESSION['user'];
+} else {
+	$sessionuser = "";
+}
+
+if( !(hasright($sessionuser, "Admin") || hasright($sessionuser, "User")))
+	die("You are not authorized to use this feature");
+
+
 
 $now = date("Y-m-d", mktime(0, 0, 0, date("m"), date("d")));
 
@@ -48,9 +80,10 @@ foreach ($top5 as $top1) {
 }
 $top5out .= "</table>";
 
-displayheader();
+echo makehead( $sessionuser );
+echo '<div id="content">';
 echo $top5aout;
 echo $top5out;
-echo "<br />";
-displayfooter();
+echo "</div>";
+echo showfooter();
 ?>
