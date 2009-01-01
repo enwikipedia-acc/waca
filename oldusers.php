c80fdf740864c0195df91da772418aebf0bdb18a
Simon Walker <simon@stwalkerster.co.uk>
2009-01-01 21:38:15 +0000

    add approval column to table, and add zebra effect
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@1965 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- oldusers.php
+++ oldusers.php
@@ -53,9 +53,30 @@ if (!$result)
 displayheader();
 echo '<h2>Old user accounts</h2>This list contains the usernames of all accounts that have not logged in in the past 45 days.';
 
-echo "<table><tr><th>User ID</th><th>Tool Username</th><th>User access level</th><th>enwiki username</th><th>Last activity</th></tr>";
+echo "<table><tr><th>User ID</th><th>Tool Username</th><th>User access level</th><th>enwiki username</th><th>Last activity</th><th>Approval</th></th></tr>";
+$currentrow = 0;
 while ($r = mysql_fetch_assoc($result)) {
-	echo "<tr><th>".$r['tooluserid']."</th><td>".$r['tooluser']."</td><td>".$r['toolaccesslevel']."</td><td>".$r['enwikiuser']."</td><td>".$r['lasttoollogon']."</td></tr>";
+	$userid = $r['tooluserid'];
+	$q2 = 'select log_time from acc_log where log_pend = '.$userid.' and log_action = "Approved" order by log_id desc limit 1;';
+	$res2 = mysql_query($q2);
+	if (!$res2)
+		die("ERROR: No result returned.");
+	$row2 = mysql_fetch_assoc($res2);
+	$approved = $row2['log_time'];
+	
+	$appr_array = date_parse($approved);
+	$appr_ts = mktime($appr_array['hour'], $appr_array['minute'], $appr_array['second'], $appr_array['month'], $appr_array['day'], $appr_array['year'] );
+	
+	if( $appr_ts < mktime($date->format("H"), $date->format("i"), $date->format("s"), $date->format("m"), $date->format("d"), $date->format("Y") )) {
+		$currentrow +=1;
+		echo "<tr";		
+		if ($currentrow % 2 == 0) {
+			echo ' class="even">';
+		} else {
+			echo ' class="odd">';
+		}	
+		echo "<th>$userid</th><td>".$r['tooluser']."</td><td>".$r['toolaccesslevel']."</td><td>".$r['enwikiuser']."</td><td>".$r['lasttoollogon']."</td><td>".$approved."</td></tr>";
+	}
 }
 echo "</table>";
 displayfooter();
