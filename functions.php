305e8bdc97a06d992749e970947503fa985f78e3
OverlordQ <overlordq@gmail.com>
2009-01-06 01:04:52 +0000

    oops, yay for starting lines of code
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@2019 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- functions.php
+++ functions.php
@@ -709,7 +709,6 @@ HTML;
 HTML;
 	$html .= listrequests("Admin", TRUE);
 	$html .= "<h2>Last 5 Closed requests</h2><a name='closed'></a><span id=\"closed\"/>\n";
-	@mysql_select_db
 	$query = "SELECT pend_id, pend_name, pend_checksum FROM acc_pend JOIN acc_log ON pend_id = log_pend WHERE log_action LIKE 'Closed%' ORDER BY log_time DESC LIMIT 5;";
 	$result = mysql_query($query, $tsSQLlink);
 	if (!$result)
