4839551f77b4a21826a47c1360f7a6d9cef0e5df
Simon Walker <simon@stwalkerster.co.uk>
2009-01-19 21:29:10 +0000

    add reservedrequests.php
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@2044 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

---
+++ reservedrequests.php
@@ -0,0 +1,65 @@
+<?php
+/**************************************************************
+** English Wikipedia Account Request Interface               **
+** Wikipedia Account Request Graphic Design by               **
+** Charles Melbye is licensed under a Creative               **
+** Commons Attribution-Noncommercial-Share Alike             **
+** 3.0 United States License. All other code                 **
+** released under Public Domain by the ACC                   **
+** Development Team.                                         **
+**             Developers:                                   **
+**  SQL ( http://en.wikipedia.org/User:SQL )                 **
+**  Cobi ( http://en.wikipedia.org/User:Cobi )               **
+** Cmelbye ( http://en.wikipedia.org/User:cmelbye )          **
+**FastLizard4 ( http://en.wikipedia.org/User:FastLizard4 )   **
+**Stwalkerster ( http://en.wikipedia.org/User:Stwalkerster ) **
+**Soxred93 ( http://en.wikipedia.org/User:Soxred93)          **
+**Alexfusco5 ( http://en.wikipedia.org/User:Alexfusco5)      **
+**OverlordQ ( http://en.wikipedia.org/wiki/User:OverlordQ )  **
+**Prodego    ( http://en.wikipedia.org/wiki/User:Prodego )   **
+**FunPika    ( http://en.wikipedia.org/wiki/User:FunPika )   **
+**************************************************************/
+require_once ('config.inc.php');
+require_once('functions.php');
+
+// check to see if the database is unavailable
+readOnlyMessage();
+
+session_start();
+
+// retrieve database connections
+global $tsSQLlink, $asSQLlink, $toolserver_database;
+list($tsSQLlink, $asSQLlink) = getDBconnections();
+@ mysql_select_db($toolserver_database, $tsSQLlink);
+if( isset( $_SESSION['user'] ) ) {
+	$sessionuser = $_SESSION['user'];
+} else {
+	$sessionuser = "";
+}
+
+if( !(hasright($sessionuser, "Admin") || hasright($sessionuser, "User")))
+	die("You are not authorized to use this feature. Only logged in users may use this statistics page.");
+
+echo makehead( $sessionuser );
+
+$query = "select pend_id, pend_name, pend_status, user_name from acc_pend inner join acc_user on user_id = pend_reserved where pend_reserved != 0;";
+$result = mysql_query($query, $tsSQLlink);
+if(!$result) die();
+
+echo "<table cellspacing=\"0\"><tr><th>#</th><th>Requested name</th><th>Status of request</th><th>Reserved by</th></tr>";
+$currentreq=0;
+while($row = $mysql_fetch_assoc($result)) {
+	$currentreq +=1;
+
+	echo "<tr";
+	if ($currentreq % 2 == 0) {
+		echo ' class="alternate">';
+	} else {
+		echo '>';
+	}
+	echo "<th>".$row['pend_id']."</th><td>".$row['pend_name']."</td><td>".$row['pend_status']."</td><td>".$row['user_name']."</td></tr>";
+}
+echo "</table>"; 
+
+
+?>
