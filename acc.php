4b688679ffb1ddd47dc5de6e9c0797448452e869
OverlordQ <overlordq@gmail.com>
2009-03-11 07:42:40 +0000

    Change to unordered list, elements are already mis numbered due to additions
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@2107 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- acc.php
+++ acc.php
@@ -502,10 +502,10 @@ elseif ($action == "messagemgmt") {
 	if (!$result)
 		Die("Query failed: $query ERROR: " . mysql_error());
 	echo "<h2>Mail messages</h2>\n";
-	echo "<ol>\n";
+	echo "<ul>\n";
 	while ( list( $mail_id, $mail_count, $mail_desc ) = mysql_fetch_row( $result ) ) {
-		$out = "<li><small>[ $mail_desc ] <a href=\"acc.php?action=messagemgmt&amp;edit=$mail_id\">Edit!</a> - <a href=\"acc.php?action=messagemgmt&amp;view=$mail_id\">View!</a></small></li>";
-		$out2 = "<li><small>[ $mail_desc ] <a href=\"acc.php?action=messagemgmt&amp;view=$mail_id\">View!</a></small></li>";
+		$out = "<li><small> $mail_id ) [ $mail_desc ] <a href=\"acc.php?action=messagemgmt&amp;edit=$mail_id\">Edit!</a> - <a href=\"acc.php?action=messagemgmt&amp;view=$mail_id\">View!</a></small></li>";
+		$out2 = "<li><small> $mail_id )[ $mail_desc ] <a href=\"acc.php?action=messagemgmt&amp;view=$mail_id\">View!</a></small></li>";
 		if(hasright($_SESSION['user'], 'Admin')){
 		echo "$out\n";
 		}
@@ -513,19 +513,19 @@ elseif ($action == "messagemgmt") {
 		echo "$out2\n";
 		}
 	}
-	echo "</ol><br />\n";
+	echo "</ul><br />\n";
 	$query = "SELECT * FROM acc_emails WHERE mail_type = 'Interface';";
 	$result = mysql_query($query, $tsSQLlink);
 	if (!$result)
 		Die("Query failed: $query ERROR: " . mysql_error());
 	echo "<h2>Public Interface messages</h2>\n";
-	echo "<ol start=\"6\">\n";
+	echo "<ul>\n";
 	while ($row = mysql_fetch_assoc($result)) {
 		$mailn = $row['mail_id'];
 		$mailc = $row['mail_count'];
 		$maild = $row['mail_desc'];
-		$out = "<li><small>[ $maild ] <a href=\"acc.php?action=messagemgmt&amp;edit=$mailn\">Edit!</a> - <a href=\"acc.php?action=messagemgmt&amp;view=$mailn\">View!</a></small></li>";
-		$out2 = "<li><small>[ $maild ] <a href=\"acc.php?action=messagemgmt&amp;view=$mailn\">View!</a></small></li>";
+		$out = "<li><small>$mailn ) [ $maild ] <a href=\"acc.php?action=messagemgmt&amp;edit=$mailn\">Edit!</a> - <a href=\"acc.php?action=messagemgmt&amp;view=$mailn\">View!</a></small></li>";
+		$out2 = "<li><small>$mailn ) [ $maild ] <a href=\"acc.php?action=messagemgmt&amp;view=$mailn\">View!</a></small></li>";
 		if(hasright($_SESSION['user'], 'Admin')){
 		echo "$out\n";
 		}
@@ -533,20 +533,20 @@ elseif ($action == "messagemgmt") {
 		echo "$out2\n";
 		}
 	}
-	echo "</ol><br />\n";
+	echo "</ul><br />\n";
 	$query = "SELECT * FROM acc_emails WHERE mail_type = 'Internal';";
 	$result = mysql_query($query, $tsSQLlink);
 	if (!$result)
 		Die("Query failed: $query ERROR: " . mysql_error());
 
 	echo "<h2>Internal Interface messages</h2>\n";
-	echo "<ol start=\"21\">\n";
+	echo "<ul>\n";
 	while ($row = mysql_fetch_assoc($result)) {
 		$mailn = $row['mail_id'];
 		$mailc = $row['mail_count'];
 		$maild = $row['mail_desc'];
-		$out = "<li><small>[ $maild ] <a href=\"acc.php?action=messagemgmt&amp;edit=$mailn\">Edit!</a> - <a href=\"acc.php?action=messagemgmt&amp;view=$mailn\">View!</a></small></li>";
-		$out2 = "<li><small>[ $maild ] <a href=\"acc.php?action=messagemgmt&amp;view=$mailn\">View!</a></small></li>";
+		$out = "<li><small>$mailn ) [ $maild ] <a href=\"acc.php?action=messagemgmt&amp;edit=$mailn\">Edit!</a> - <a href=\"acc.php?action=messagemgmt&amp;view=$mailn\">View!</a></small></li>";
+		$out2 = "<li><small>$mailn ) [ $maild ] <a href=\"acc.php?action=messagemgmt&amp;view=$mailn\">View!</a></small></li>";
 		if(hasright($_SESSION['user'], 'Admin')){
 		echo "$out\n";
 		}
