c52bcaff39832f56eaed20da8b6798d04e3a88e2
FunPika <funpikawiki@gmail.com>
2009-04-11 20:09:34 +0000

    Add session_start function to oldusers.php.
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@2144 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- oldusers.php
+++ oldusers.php
@@ -29,6 +29,8 @@ require_once ( 'devlist.php' );
 // check to see if the database is unavailable
 readOnlyMessage();
 
+session_start();
+
 // retrieve database connections
 global $tsSQLlink, $asSQLlink, $toolserver_database;
 list($tsSQLlink, $asSQLlink) = getDBconnections();
@@ -57,7 +59,7 @@ echo '<h2>Old user accounts</h2>This list contains the usernames of all accounts
 
 echo "<table><tr><th>User ID</th><th>Tool Username</th><th>User access level</th><th>enwiki username</th><th>Last activity</th><th>Approval</th>";
 if(hasright($_SESSION['user'], "Admin")) {
-echo "<th>Suspend</th>";
+	echo "<th>Suspend</th>";
 }
 echo "</tr>";
 $currentrow = 0;
@@ -88,8 +90,8 @@ while ($r = mysql_fetch_assoc($result)) {
 			}	
 			echo "<th>$userid</th><td>$tooluser</td><td>".$r['toolaccesslevel']."</td><td>".$r['enwikiuser']."</td><td>".$r['lasttoollogon']."</td><td>".$approved."</td>";
 			if(hasright($_SESSION['user'], "Admin")) {
-			$inactivesuspend = "Inactive for 45+ days. Please contact a tool admin if you wish to come back.";
-			echo "<td><a class=\"request-req\" href=\"acc.php?action=usermgmt&amp;suspend=$userid&amp;preload=$inactivesuspend\"></td>";
+				$inactivesuspend = "Inactive for 45+ days. Please contact a tool admin if you wish to come back.";
+				echo "<td><a class=\"request-req\" href=\"acc.php?action=usermgmt&amp;suspend=$userid&amp;preload=$inactivesuspend\"></td>";
 			}
 			echo "</tr>";
 		}
