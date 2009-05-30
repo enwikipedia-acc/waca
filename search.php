1714ab2c3a0466fbccf487e228ac12c6e380a1c6
Brett Hillebrand <bretthillebrand@internode.on.net>
2009-05-30 03:11:12 +0000

    Enhancing search result for IPs, No point echoing the IP being searched as it is already known. Perhaps user name would be better?
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@2254 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- search.php
+++ search.php
@@ -98,13 +98,13 @@ if( isset($_GET['term'])) {
 		if( !hasright($sessionuser, "Admin"))
 			die("You are not authorized to use this feature");
 		
-		$query = "SELECT pend_id,pend_ip FROM acc_pend WHERE pend_ip LIKE '%$term%';";
+		$query = "SELECT pend_id,pend_ip,pend_name FROM acc_pend WHERE pend_ip LIKE '%$term%';";
 		$result = mysql_query($query, $tsSQLlink);
 		if (!$result)
 			Die("Query failed: $query ERROR: " . mysql_error());
 		$html = "<table cellspacing=\"0\">\n";
 		$currentrow = 0;
-		while ( list( $pend_id,$pend_ip ) = mysql_fetch_row( $result ) ) {
+		while ( list( $pend_id,$pend_ip,$pend_name ) = mysql_fetch_row( $result ) ) {
 			$currentrow += 1;
 			$out = '<tr';
 			if ($currentrow % 2 == 0) {
@@ -112,7 +112,7 @@ if( isset($_GET['term'])) {
 			} else {
 				$out .= ' class="odd">';
 			}
-			$out .= "<td><b>$currentrow.</b></td><td><small><a style=\"color:blue\" href=\"acc.php?action=zoom&amp;id=" . $pend_id . "\"> $pend_ip </a></small></tr>";
+			$out .= "<td><b>$currentrow.</b></td><td><small><a style=\"color:blue\" href=\"acc.php?action=zoom&amp;id=" . $pend_id . "\"> $pend_name </a> ($pend_ip) </small></tr>";
 			$html .= $out;
 		}
 		$html .= "</table>\n";
