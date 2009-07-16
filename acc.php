87b1caf71435e7026a49ba3f8ba611f002ae7bd8
OverlordQ <overlordq@gmail.com>
2009-07-16 00:00:23 +0000

    reverting r2390
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@2440 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- acc.php
+++ acc.php
@@ -25,7 +25,6 @@
 
 
 require_once ( 'config.inc.php' );
-$useCaptcha = true; // TODO: This needs to be moved to the config file - i'd do it myself but I don't have shell - Chris
 require_once ( 'devlist.php' );
 require_once ( 'functions.php' );
 $version = "0.9.7";
