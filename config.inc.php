2f25d6a559304e269b55a8523fac0234e91cee42
Simon Walker <simon@stwalkerster.co.uk>
2009-08-02 00:16:32 +0000

    Remove config.inc.php from svn:ignore Rename configex.inc.php to config.inc.php Rehash config.inc.php to force a local config file, and provide sane defaults for all config options
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@2481 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

---
+++ config.inc.php
@@ -0,0 +1,86 @@
+<?php
+ 
+///////////////////////////////////////////////////////////////
+// English Wikipedia Account Request Interface               //
+// Wikipedia Account Request Graphic Design by               //
+// Charles Melbye is licensed under a Creative               //
+// Commons Attribution-Noncommercial-Share Alike             //
+// 3.0 United States License. All other code                 //
+// released under Public Domain by the ACC                   //
+// Development Team.                                         //
+//             Developers:                                   //
+//  SQL ( http://en.wikipedia.org/User:SQL )                 //
+//  Cobi ( http://en.wikipedia.org/User:Cobi )               //
+// Cmelbye ( http://en.wikipedia.org/User:cmelbye )          //
+//FastLizard4 ( http://en.wikipedia.org/User:FastLizard4 )   //
+//Stwalkerster ( http://en.wikipedia.org/User:Stwalkerster ) //
+//Soxred93 ( http://en.wikipedia.org/User:Soxred93)          //
+//Alexfusco5 ( http://en.wikipedia.org/User:Alexfusco5)      //
+//OverlordQ ( http://en.wikipedia.org/wiki/User:OverlordQ )  //
+//                                                           //
+///////////////////////////////////////////////////////////////
+
+# YOU *MUST* OVERRIDE THE CONFIGURATION IN THIS FILE IN A FILE CALLED config.local.inc.php 
+
+//name for the tool	
+$whichami = 'Live';
+
+//main database location and access details
+$toolserver_mycnf = parse_ini_file("/projects/acc/.my.cnf"); //location of  a .my.cnf file with connection data in, if one exists
+$toolserver_username = $toolserver_mycnf['user'];
+$toolserver_password = $toolserver_mycnf['password'];
+$toolserver_host = "sql";
+$toolserver_database = "p_acc";
+unset ($toolserver_mycnf);
+
+$wikiurl = "en.wikipedia.org"; //Does nothing yet, intended for further localization
+$tsurl = "http://stable.toolserver.org/acc"; 
+
+//set up cookies and session information
+$cookiepath = '/acc/';
+$sessionname = 'ACC';
+
+//a few options
+$enableRenames = 1; // enable account renaming
+$enableEmailConfirm = 1; // enable request email confirmation
+$enableReserving = 1; // enable request reservations
+$enableSQLError = 0; // enable display of SQL errors
+$enableDnsblChecks = 1; // enable DNS blacklist checks
+$showGraphs = 1; // show graphs on statistics pages
+
+$dontUseDb = 0; // disable the tool completely
+$dontUseWikiDb = 0; // disable the wiki database
+$dontUseDbReason = ""; // if disabling the tool, please enter a reason here to be displayed internally.
+$dontUseDbCulprit = ""; // "     "      "    "     "      "   your name, or the person who broke the tool's name.
+	
+//antispoof configuration
+$antispoof_equivset = "equivset.php";
+$antispoof_host = "sql-s1";
+$antispoof_db = "enwiki_p";
+$antispoof_table = "spoofuser";
+
+// double reserving checks.
+// possible values:
+//    ignore: ignores the fact that some users have reserved two requests.
+//    inform: reserves the second request, and alerts the user that they have several requests reserved
+//    warn: asks the user if they are sure they wish to reserve the second request
+//    deny: prevents the user from reserving a second request.
+$allowDoubleReserving = "inform";
+
+$useCaptcha = false;
+
+$ircBotNickServPassword = ""; // password for ACCBot's nickserv account
+	
+// By default, reserve to a specific user. Adapted from livehack by st - use the userid, zero for unreserved.
+$defaultReserver = 0;
+
+////// Don't add any new config options below this line, as they will not be changable by the local config file.
+require_once('config.local.inc.php');
+	
+	
+$ACC = 1; //Keep included files from being executed
+
+require_once ('blacklist.php');
+
+ini_set( 'session.cookie_path', $cookiepath );
+ini_set( 'session.name', $sessionname );
