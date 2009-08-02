ce9cfdbbddfa92c313a177d172a35b61dc0beefe
Simon Walker <simon@stwalkerster.co.uk>
2009-08-02 00:04:59 +0000

    Fix bot messaging script, needs testing properly though
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@2480 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- root/send-bot-message.php
+++ root/send-bot-message.php
@@ -6,7 +6,11 @@ if (isset($_SERVER['REQUEST_METHOD'])) {
 
 require_once('../config.inc.php');
 require_once('../functions.php');
+require_once('../includes/accbotSend.php');
 
 $message = $argv[1];
 // $formatted = formatforbot($message);
-sendtobot($message);
+# sendtobot($message);
+
+$botSend = new accbotSend();
+$botSend->send($message);
