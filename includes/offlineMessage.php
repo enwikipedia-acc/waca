d8650910a87f846519f49a4439cc096b144c55c0
Lourie Pieterse <louriepieterse@yahoo.com>
2009-10-09 18:56:47 +0000

    Corrected the variables in offlineMessage with those in config.
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@2603 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- includes/offlineMessage.php
+++ includes/offlineMessage.php
@@ -87,13 +87,13 @@ HTML;
 	}
 	
 	private function showInternalOfflineMessage() {
-		global $offlineProblem, $offlineCulprit;
+		global $dontUseDbReason, $dontUseDbCulprit;
 		echo <<<HTML
 		<h2>Whoops!</h2>
 		<p>After much experimentation, someone finally managed to kill ACC. So, the tool is currently offline while our resident developers pound their skulls against the furniture.</p> 
 		<p>Apparently, this is supposed to fix it.</p>
-		<p>Once the nature of the problem is known, we will insert it here: <b>$offlineProblem</b></p>
-		<p>Once the identity of the culprit(s) is known, trout should be applied here: <b>$offlineCulprit</b></p>
+		<p>Once the nature of the problem is known, we will insert it here: <b>$dontUseDbReason</b></p>
+		<p>Once the identity of the culprit(s) is known, trout should be applied here: <b>$dontUseDbCulprit</b></p>
 		<p>Although the tool is dead and the Bot is sleeping, email still works fine. So, we expect a swarm of irate potential editors to bury us in requests shortly. Please keep an eye on the mailing list. Remember to 'cc' or 'bcc' accounts-enwiki when you reply to let others know you have replied.</p> 
 		<p>For more information, <a href="irc://irc.freenode.net/#wikipedia-en-accounts">join IRC</a>, check the mailing list (<a href="https://lists.wikimedia.org/mailman/listinfo/accounts-enwiki-l">sign up if you need to</a>) or just light candles – they may help too.</p>
 HTML;
