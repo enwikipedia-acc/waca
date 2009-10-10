25353135392e0fe6efb1fb4d2906b497c7617125
Lourie Pieterse <louriepieterse@yahoo.com>
2009-10-10 10:19:40 +0000

    Corrected both the background and the spacing.
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@2618 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- includes/imagegen.php
+++ includes/imagegen.php
@@ -27,53 +27,51 @@ if ($ACC != "1") {
 } //Re-route, if you're a web client.
 
 class imagegen {
-	public function create( $text) {
+	public function create($text) {
 	
-		// get the md5 hash of the provided text.
-		// md5 will always return the exact same value, for the exact same string.
+		// Get the md5 hash of the provided text.
+		// Md5 will always return the exact same value, for the exact same string.
 		$id = md5($text);
 		
-		// calculate the directory the image should be in, by taking the first letter of the hash
+		// Calculate the directory the image should be in, by taking the first letter of the hash
 		$imageDir = './images/' . substr($id,0,1) . '/';
 		
-		// if the directory doesn't exist...
+		// If the directory doesn't exist...
 		if(! file_exists($imageDir) )
 		{
-			// ... make it
+			// ... make it!
 			mkdir($imageDir);
 		}
 		
-		// if there's already a file with tha name, why create it again?
+		// If there's already a file with tha name, why create it again?
 		if( file_exists($imageDir.$id.'.png'))
 		{
-			// return the id, that's enough to tell us where the image is stored
+			// Return the id, that's enough to tell us where the image is stored.
 			return $id;	
 		}
-		
 		
 		// Font size of text.
 		$font  = 2;
 		
 		// The size of the image.
-		$width  = ImageFontWidth($font) * strlen($text) + 4;
-		$height = ImageFontHeight($font) + 4;
+		$width  = ImageFontWidth($font) * strlen($text);
+		$height = ImageFontHeight($font);
 		
 		// Create a blank image with the above dimessions.
 		$im = imagecreate ($width, $height);
 		
 		// Generate the colours to be used in the image.
-		$background_color = imagecolorallocate ($im, 229, 229, 229);
+		// Uses the backgroud colour of the ACC as image background.
+		$background_color = imagecolorallocate ($im, 248, 252, 255);
 		$text_color = imagecolorallocate ($im, 0, 0, 0);
 		
 		// Put it all together in the image.
-		imagestring ($im, $font, 2, 2,  $text, $text_color);
-		
-
-		
+		imagestring ($im, $font, 0, 0,  $text, $text_color);
+				
 		// Writes the image to the system.
 		imagepng($im, $imageDir . $id . '.png');
 		
-		// return the id, so we can figure out where it's stored
+		// Return the id, so we can figure out where it's stored.
 		return $id;
 	}
 }
