666c66279bee546fa64b7da64132dfe7f2a5a178
Simon Walker <simon@stwalkerster.co.uk>
2009-10-11 12:49:56 +0000

    more graphs
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@2656 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- includes/statistics/StatsMonthlyStats.php
+++ includes/statistics/StatsMonthlyStats.php
@@ -16,9 +16,49 @@ class StatsMonthlyStats extends StatisticsPage
 			require_once($filepath . 'graph/pChart/pChart.class');
 			require_once($filepath . 'graph/pChart/pData.class');
 			
-			$this->createGraph();
+			$queries = array(
+				array(
+					'query' => "SELECT COUNT(DISTINCT log_id) AS 'y', CONCAT( YEAR(log_time), '/' , MONTHNAME(log_time)) AS 'x' FROM acc_log WHERE log_action LIKE 'Closed%' AND YEAR(log_time) != 0 GROUP BY EXTRACT(YEAR_MONTH FROM log_time) ORDER BY YEAR(log_time), MONTH(log_time) ASC;",
+					'series' => "All closed requests by month"
+				),
+				array(
+					'query' => "SELECT COUNT(DISTINCT log_id) AS 'y', CONCAT( YEAR(log_time), '/' , MONTHNAME(log_time)) AS 'x' FROM acc_log WHERE log_action LIKE 'Closed 0' AND YEAR(log_time) != 0 GROUP BY EXTRACT(YEAR_MONTH FROM log_time) ORDER BY YEAR(log_time), MONTH(log_time) ASC;",
+					'series' => "Dropped requests by month"
+				),
+				/*array(
+					'query' => "SELECT COUNT(DISTINCT log_id) AS 'y', CONCAT( YEAR(log_time), '/' , MONTHNAME(log_time)) AS 'x' FROM acc_log WHERE log_action LIKE 'Closed 1' AND YEAR(log_time) != 0 GROUP BY EXTRACT(YEAR_MONTH FROM log_time) ORDER BY YEAR(log_time), MONTH(log_time) ASC;",
+					'series' => "Created"
+				),*/
+				array(
+					'query' => "SELECT COUNT(DISTINCT log_id) AS 'y', CONCAT( YEAR(log_time), '/' , MONTHNAME(log_time)) AS 'x' FROM acc_log WHERE log_action LIKE 'Closed 2' AND YEAR(log_time) != 0 GROUP BY EXTRACT(YEAR_MONTH FROM log_time) ORDER BY YEAR(log_time), MONTH(log_time) ASC;",
+					'series' => "Similar requests by month"
+				),
+				array(
+					'query' => "SELECT COUNT(DISTINCT log_id) AS 'y', CONCAT( YEAR(log_time), '/' , MONTHNAME(log_time)) AS 'x' FROM acc_log WHERE log_action LIKE 'Closed 3' AND YEAR(log_time) != 0 GROUP BY EXTRACT(YEAR_MONTH FROM log_time) ORDER BY YEAR(log_time), MONTH(log_time) ASC;",
+					'series' => "Taken requests by month"
+				),
+				array(
+					'query' => "SELECT COUNT(DISTINCT log_id) AS 'y', CONCAT( YEAR(log_time), '/' , MONTHNAME(log_time)) AS 'x' FROM acc_log WHERE log_action LIKE 'Closed 4' AND YEAR(log_time) != 0 GROUP BY EXTRACT(YEAR_MONTH FROM log_time) ORDER BY YEAR(log_time), MONTH(log_time) ASC;",
+					'series' => "UVio requests by month"
+				),
+				array(
+					'query' => "SELECT COUNT(DISTINCT log_id) AS 'y', CONCAT( YEAR(log_time), '/' , MONTHNAME(log_time)) AS 'x' FROM acc_log WHERE log_action LIKE 'Closed 5' AND YEAR(log_time) != 0 GROUP BY EXTRACT(YEAR_MONTH FROM log_time) ORDER BY YEAR(log_time), MONTH(log_time) ASC;",
+					'series' => "Technical requests by month"
+				),
+				array(
+					'query' => "SELECT COUNT(DISTINCT log_id) AS 'y', CONCAT( YEAR(log_time), '/' , MONTHNAME(log_time)) AS 'x' FROM acc_log WHERE log_action LIKE 'Closed 26' AND YEAR(log_time) != 0 GROUP BY EXTRACT(YEAR_MONTH FROM log_time) ORDER BY YEAR(log_time), MONTH(log_time) ASC;",
+					'series' => "SUL Taken requests by month"
+				),
+				array(
+					'query' => "SELECT COUNT(DISTINCT log_id) AS 'y', CONCAT( YEAR(log_time), '/' , MONTHNAME(log_time)) AS 'x' FROM acc_log WHERE log_action LIKE 'Closed custom' AND YEAR(log_time) != 0 GROUP BY EXTRACT(YEAR_MONTH FROM log_time) ORDER BY YEAR(log_time), MONTH(log_time) ASC;",
+					'series' => "Custom requests by month"
+				)
+			);
 
-			$out.= '<img src="render/'.$_SERVER['REQUEST_TIME'].'" />';
+			foreach ($this->createClosuresGraph($queries) as $i) {
+				$out.= '<img src="render/' . $i . '" />';
+			}
+
 		}
 		else
 		{
@@ -39,49 +79,65 @@ class StatsMonthlyStats extends StatisticsPage
 	{
 		return true;
 	}
-	
-	function createGraph()
-	{
-		$qb = new QueryBrowser();
-		
-		$gquery = "SELECT COUNT(DISTINCT log_id) AS 'y', CONCAT( YEAR(log_time), '/' , MONTHNAME(log_time)) AS 'x' FROM acc_log WHERE log_action LIKE 'Closed%' GROUP BY EXTRACT(YEAR_MONTH FROM log_time) ORDER BY YEAR(log_time), MONTH(log_time) ASC;";
-	
-		$DataSet = new pData;
-		foreach($qb->executeQueryToArray($gquery) as $row)
-		{
-			$DataSet->AddPoint($row['y'], "Serie1", $row['x']);
-		}
-		$DataSet->AddAllSeries();
-		$DataSet->SetAbsciseLabelSerie();
-		$DataSet->SetSerieName("Closed requests","Serie1");
-	
-		$Test = new pChart(700,280);
-		$Test->setFontProperties("graph/Fonts/tahoma.ttf",8);
-		$Test->setGraphArea(50,30,585,200);
-		$Test->drawFilledRoundedRectangle(7,7,693,273,5,240,240,240);
-		$Test->drawRoundedRectangle(5,5,695,275,5,230,230,230);
-		$Test->drawGraphArea(255,255,255,TRUE);
-		$Test->drawScale($DataSet->GetData(),$DataSet->GetDataDescription(),SCALE_NORMAL,150,150,150,TRUE,45,2);
-		$Test->drawGrid(4,TRUE,230,230,230,50);
-		
-		// Draw the 0 line
-		$Test->setFontProperties("graph/Fonts/tahoma.ttf",6);
-		$Test->drawTreshold(0,143,55,72,TRUE,TRUE);
-		
-		// Draw the cubic curve graph
-		$Test->drawFilledCubicCurve($DataSet->GetData(),$DataSet->GetDataDescription(),.1,50);
-		
-		// Finish the graph
-		$Test->setFontProperties("graph/Fonts/tahoma.ttf",8);
-		$Test->drawLegend(600,30,$DataSet->GetDataDescription(),255,255,255);
-		$Test->setFontProperties("graph/Fonts/tahoma.ttf",10);
-		$Test->drawTitle(50,22,"Monthly Statistics",50,50,50,585);
-		$Test->Render("render/".$_SERVER['REQUEST_TIME']);   	
-	}
-	
 	function requiresWikiDatabase()
 	{
 		return false;	
+	}
+
+	function createClosuresGraph($queries)
+	{
+		$qb = new QueryBrowser();
+		
+		$imagehashes = array();		
+		
+		foreach ($queries as $q) {
+			$DataSet = new pData();
+			$qResult = $qb->executeQueryToArray($q['query']);
+			
+			if(sizeof($qResult) > 0)
+			{
+			
+				foreach($qResult as $row)
+				{
+					$DataSet->AddPoint($row['y'], $q['series'], $row['x']);
+				}
+	
+				
+				
+				$DataSet->AddAllSeries();
+				$DataSet->SetAbsciseLabelSerie();
+				
+				$chartname = md5(serialize($DataSet));
+				$imagehashes[] = $chartname;
+				
+				if(!file_exists($chartname))
+				{
+					$Test = new pChart(700,280);
+					$Test->setFontProperties("graph/Fonts/tahoma.ttf",8);
+					$Test->setGraphArea(50,30,680,200);
+					$Test->drawFilledRoundedRectangle(7,7,693,273,5,240,240,240);
+					$Test->drawRoundedRectangle(5,5,695,275,5,230,230,230);
+					$Test->drawGraphArea(255,255,255,TRUE);
+					$Test->drawScale($DataSet->GetData(),$DataSet->GetDataDescription(),SCALE_NORMAL,150,150,150,TRUE,45,2);
+					$Test->drawGrid(4,TRUE,230,230,230,50);
+					
+					// Draw the 0 line
+					$Test->setFontProperties("graph/Fonts/tahoma.ttf",6);
+					$Test->drawTreshold(0,143,55,72,TRUE,TRUE);
+					
+					// Draw the cubic curve graph
+					$Test->drawFilledCubicCurve($DataSet->GetData(),$DataSet->GetDataDescription(),.1,50);
+					
+					// Finish the graph
+					$Test->setFontProperties("graph/Fonts/tahoma.ttf",10);
+					$Test->drawTitle(50,22, $q['series'],50,50,50,585);
+					$Test->Render("render/" . $chartname);
+				}   
+			}
+		}		
+		
+		return $imagehashes;
+		
 	}
 	
 }
