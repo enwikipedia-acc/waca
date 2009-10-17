dfd59d193b49044d767e86f39f6dae039a8c0512
Lourie Pieterse <louriepieterse@yahoo.com>
2009-10-17 17:27:20 +0000

    Improved coding and added comments.
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@2680 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- includes/internalInterface.php
+++ includes/internalInterface.php
@@ -33,24 +33,43 @@ class internalInterface {
 		*/
 		global $tsSQLlink;
 		
+		// Assigns the calculated amount of users to the new array.
 		$howma = $this->gethowma();
+		
+		// Removes the howmany variable, as we only want the users online.
 		unset ($howma['howmany']);
 		
+		// Get the user id for each of the users in the array.
 		foreach ($howma as &$oluser) {
+			// Sanitizes the username and get their record.
 			$oluser = sanitize($oluser);
 			$query = "SELECT * FROM acc_user WHERE user_name = '$oluser';";
 			$result = mysql_query($query, $tsSQLlink);
-			if (!$result)
+			
+			// Display an error message if the query didnt work.
+			if (!$result) {
 				sqlerror("Query failed: $query ERROR: " . mysql_error() . " f190","Database query error.");
+			}
+			
+			// Uses the users row to gets its ID.
 			$row = mysql_fetch_assoc($result);
 			$uid = $row['user_id'];
-				$oluser = stripslashes($oluser);
-				$oluser = "<a href=\"users.php?viewuser=$uid\">$oluser</a>";
+			
+			// Generates a link containing the username and the user ID.
+			// Adds this link to the original array.
+			$oluser = stripslashes($oluser);
+			$oluser = "<a href=\"users.php?viewuser=$uid\">$oluser</a>";
 		}
 		
-		unset($oluser);
+		// Destroys the oluser variable.
+		// unset($oluser);
+		
+		// Adds all the items of the array into a string.
+		// The string would contain links, as the link where added by the for loop.
 		$out = "";
 		$out = implode(", ", $howma);
+
+		// Strips all the whitespaces from the string.
 		$out = ltrim(rtrim($out));
 		return ($out);
 	}
@@ -61,22 +80,36 @@ class internalInterface {
 		*/
 		global $tsSQLlink;
 		
-		$last5min = time() - 300; // Get the users active as of the last 5 mins
-		$last5mins = date("Y-m-d H:i:s", $last5min); // TODO: This produces a PHP Strict Standards error message. See next line
-		//Strict Standards: date() [function.date]: It is not safe to rely on the system's timezone settings. Please use the date.timezone setting, the TZ environment variable or the date_default_timezone_set() function. In case you used any of those methods and you are still getting this warning, you most likely misspelled the timezone identifier.
+		// Get the users active as of the last 5 mins, or 300 seconds.
+		$last5min = time() - 300;
 		
+		// TODO: This produces a PHP Strict Standards error message.
+		// Strict Standards: date() [function.date]: It is not safe to rely on the system's timezone settings.
+		// Please use the date.timezone setting, the TZ environment variable or the date_default_timezone_set() function.
+		// In case you used any of those methods and you are still getting this warning, you most likely misspelled the timezone identifier.
+		$last5mins = date("Y-m-d H:i:s", $last5min); 		
+		
+		// Runs a query to get all the users that was active the last 5 minutes.
 		$query = "SELECT user_name FROM acc_user WHERE user_lastactive > '$last5mins';";
 		$result = mysql_query($query, $tsSQLlink);
 		
+		// Display an error message if the query didnt work.
 		if (!$result) {
 			sqlerror("Query failed: $query ERROR: " . mysql_error(),"Database query error.");
 		}
 		
+		// Creates new array.
 		$whoactive = array ();
+		
+		// Add each item from the SQL query into the array.
 		while (list($user_name) = mysql_fetch_row($result)) {
 			array_push($whoactive, $user_name);
 		}
+		
+		// Counts the amount of elements in the array.
 		$howma = count($whoactive);
+		
+		// Adds the amount of elements to the array.
 		$whoactive['howmany'] = $howma;
 		return ($whoactive);
 	}
