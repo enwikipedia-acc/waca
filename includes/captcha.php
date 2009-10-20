8fb8e376d2c6086bf6d1bb4b56afd842f812ef02
Lourie Pieterse <louriepieterse@yahoo.com>
2009-10-20 14:34:04 +0000

    Added new headers to all of the files. Also added index.php files to some directories.
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@2705 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- includes/captcha.php
+++ includes/captcha.php
@@ -1,27 +1,16 @@
 <?php
-
-/**************************************************************
-** English Wikipedia Account Request Interface               **
-** Wikipedia Account Request Graphic Design by               **
-** Charles Melbye is licensed under a Creative               **
-** Commons Attribution-Noncommercial-Share Alike             **
-** 3.0 United States License. All other code                 **
-** released under Public Domain by the ACC                   **
-** Development Team.                                         **
-**             Developers:                                   **
-**  SQL ( http://en.wikipedia.org/User:SQL )                 **
-**  Cobi ( http://en.wikipedia.org/User:Cobi )               **
-** Cmelbye ( http://en.wikipedia.org/User:cmelbye )          **
-**FastLizard4 ( http://en.wikipedia.org/User:FastLizard4 )   **
-**Stwalkerster ( http://en.wikipedia.org/User:Stwalkerster ) **
-**Soxred93 ( http://en.wikipedia.org/User:Soxred93)          **
-**Alexfusco5 ( http://en.wikipedia.org/User:Alexfusco5)      **
-**OverlordQ ( http://en.wikipedia.org/wiki/User:OverlordQ )  **
-**Prodego    ( http://en.wikipedia.org/wiki/User:Prodego )   **
-**FunPika    ( http://en.wikipedia.org/wiki/User:FunPika )   **
-**PRom3th3an ( http://en.wikipedia.org/wiki/User:Promethean )**
-**Chris G ( http://en.wikipedia.org/wiki/User:Chris_G )      **
-**************************************************************/
+/**************************************************************************
+**********      English Wikipedia Account Request Interface      **********
+***************************************************************************
+** Wikipedia Account Request Graphic Design by Charles Melbye,           **
+** which is licensed under a Creative Commons                            **
+** Attribution-Noncommercial-Share Alike 3.0 United States License.      **
+**                                                                       **
+** All other code are released under the Public Domain                   **
+** by the ACC Development Team.                                          **
+**                                                                       **
+** See CREDITS for the list of developers.                               **
+***************************************************************************/
 
 global $ACC;
 global $tsurl;
