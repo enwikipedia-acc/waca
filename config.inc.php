11f0a0dc708fe4828b98b9552b8363dc92bdc154
Lourie Pieterse <louriepieterse@yahoo.com>
2009-12-13 17:08:14 +0000

    Improved overall layout and added comments.
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@2778 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- config.inc.php
+++ config.inc.php
@@ -19,90 +19,106 @@
 ** IT IS VERY IMPORTANT THAT THAT FILE IS CALLED config.local.inc.php    **
 ***************************************************************************/
 
-//name for the tool	
+// Name of this instance of the tool.
+// This name would be used by the bot as reference point.	
 $whichami = 'Live';
 
-//main database location and access details
-$toolserver_username = '';
-$toolserver_password = '';
+// Main database location and access details.
+$toolserver_username = "";
+$toolserver_password = "";
 $toolserver_host = "";
 $toolserver_database = "";
 
-$wikiurl = "en.wikipedia.org"; //Does nothing yet, intended for further localization
-$tsurl = "http://stable.toolserver.org/acc"; 
-$filepath = "/projects/acc/www/"; // root pathname of the local installation of the tool.
-$varfilepath = "/projects/acc/"; // location outside web directory to place temporary files
+// Does nothing yet, intended for further localization.
+$wikiurl = "en.wikipedia.org";
 
-//set up cookies and session information
+// URL of the current copy of the tool.
+$tsurl = "http://stable.toolserver.org/acc";
+
+// Root pathname of the local installation of the tool.
+$filepath = "/projects/acc/www/"; 
+
+// Location outside web directory to place temporary files.
+$varfilepath = "/projects/acc/"; 
+
+// Set up cookies and session information.
 $cookiepath = '/acc/';
 $sessionname = 'ACC';
 
-//a few options
-$enableRenames = 1; // enable account renaming
-$enableEmailConfirm = 1; // enable request email confirmation
-$enableReserving = 1; // enable request reservations
-$enableSQLError = 0; // enable display of SQL errors
-$enableDnsblChecks = 1; // enable DNS blacklist checks
-$showGraphs = 1; // show graphs on statistics pages
+// The general interface configuration.
+$enableRenames = 1; 		// Enable interface account renaming.
+$enableEmailConfirm = 1; 	// Enable request email confirmation.
+$enableReserving = 1; 		// Enable request reservations.
+$enableSQLError = 0; 		// Enable the display of SQL errors.
+$enableDnsblChecks = 1; 	// Enable DNS blacklist checks.
+$showGraphs = 1; 			// Show graphs on statistics pages.
 
-$dontUseDb = 0; // disable the tool completely
-$dontUseWikiDb = 0; // disable the wiki database
-$dontUseDbReason = ""; // if disabling the tool, please enter a reason here to be displayed internally.
-$dontUseDbCulprit = ""; // "     "      "    "     "      "   your name, or the person who broke the tool's name.
+$dontUseDb = 0; 			// Disable the tool completely.
+$dontUseWikiDb = 0; 		// Disable access to the Wiki database.
+$dontUseDbReason = ""; 		// Reason for disabeling the tool.
+$dontUseDbCulprit = ""; 	// Your name, or the person who broke the tool.
 	
-//antispoof configuration
+// The antispoof configuration.
 $antispoof_equivset = "equivset.php";
 $antispoof_host = "sql-s1";
 $antispoof_db = "enwiki_p";
 $antispoof_table = "spoofuser";
 
-// double reserving checks.
-// possible values:
-//    ignore: ignores the fact that some users have reserved two requests.
-//    inform: reserves the second request, and alerts the user that they have several requests reserved
-//    warn: asks the user if they are sure they wish to reserve the second request
-//    deny: prevents the user from reserving a second request.
-
-// There are many valid reasons to double reserve.
-
+// Double reserving configuration.
+//    ignore: Ignores the fact that some users have reserved two requests.
+//    inform: Reserves the second request, but alerts the user.
+//    warn: Asks the user if they are sure they wish to reserve the second request.
+//    deny: Prevents the user from reserving a second request.
 $allowDoubleReserving = "warn";
 
+// Enable the use of Captcha for interface registration.
 $useCaptcha = false;
 
-$ircBotNickServPassword = ""; // password for ACCBot's nickserv account
-$ircBotCommunicationKey = ""; // Key used to communicate with ACCBot
-$ircBotNetworkHost = "wolfe.freenode.net";
-$ircBotNetworkPort = 6667;
-$ircBotChannel = "#wikipedia-en-accounts";
-$ircBotNickname = "ACCBot";
-$ircBotCommandTrigger = '!';
-$ircBotUdpServer = '';
-$ircBotUdpPort = '';
+// The IRC bot configuration.
+$ircBotNickServPassword = ""; 				// Password for ACCBot's Nickserv account.
+$ircBotCommunicationKey = ""; 				// Key used to communicate with the ACCBot.
+$ircBotNetworkHost = "wolfe.freenode.net"; 	// The host to use for connecting.
+$ircBotNetworkPort = 6667;					// The port on the particular host.
+$ircBotChannel = "#wikipedia-en-accounts";	// The channel in which the discussions are.
+$ircBotNickname = "ACCBot";					// The nickname of the ACCBot.
+$ircBotCommandTrigger = '!';				// The ACCBot's command trigger.
+$ircBotUdpServer = '';						// The UDP server for connecting.
+$ircBotUdpPort = '';						// The port on the particular server.
 	
-// By default, reserve to a specific user. Adapted from livehack by st - use the userid, zero for unreserved.
+// Reserve requests to a specific user by default.
+// Adapted from livehack by st - use the userid, zero for unreserved.
 $defaultReserver = 0;
 
-// number of days that are given for a requestor to confirm their email address
+// Number of days that are given for a requestor to confirm their email address.
 $emailConfirmationExpiryDays = 2;
 
-// Should we show the 'You last logged in from' line at the bottom of the page
+// Enable last login statistics.
 $enableLastLogin = true;
 
-// perform a newbie check on tool registration
-$onRegistrationNewbieCheck = true;
-$onRegistrationNewbieCheckEditCount = 20;
-$onRegistrationNewbieCheckAge = 5184000;
+// Parameters for performing a newbie check on tool registration.
+$onRegistrationNewbieCheck = true;			// Enable the newbie checking.
+$onRegistrationNewbieCheckEditCount = 20;	// Minimum amount of edits on Wikipedia.
+$onRegistrationNewbieCheckAge = 5184000;	// Account age on Wikipedia in seconds.
 
-// should we use PATH_INFO for request parameters to prettify urls?
+// Enable the use of PATH_INFO for request parameters to prettify URLs.
 $usePathInfo = false;
 
-////// Don't add any new config options below this line, as they will not be changable by the local config file.
-require_once('config.local.inc.php');
-	
-	
-$ACC = 1; //Keep included files from being executed
+/**************************************************************************
+**********                   IMPORTANT NOTICE                    **********
+***************************************************************************
+**     DON'T ADD ANY NEW CONFIGURATION OPTIONS BELOW THIS LINE!!!        **
+**     THEY WILL NOT BE CHANGABLE BY THE LOCAL CONFIGURATION FILE.       **
+***************************************************************************/
 
+// Retriving the local configuration file.
+require_once('config.local.inc.php');
+
+// //Keep the included files from being executed.
+$ACC = 1;
+
+// Retrieving the blacklists.
 require_once ($filepath.'blacklist.php');
 
-ini_set( 'session.cookie_path', $cookiepath );
-ini_set( 'session.name', $sessionname );
+// Sets the values of the cookie configuration options.
+ini_set('session.cookie_path', $cookiepath);
+ini_set('session.name', $sessionname);
