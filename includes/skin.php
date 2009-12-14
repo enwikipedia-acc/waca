78f083b169576d00ef9cdc5d7f5005da90e7af79
Lourie Pieterse <louriepieterse@yahoo.com>
2009-12-14 05:33:44 +0000

    Created new method for displaying messages.
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@2779 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- includes/skin.php
+++ includes/skin.php
@@ -18,16 +18,17 @@ if ($ACC != "1") {
 } //Re-route, if you're a web client.
 
 class skin {
-
+	
 	/**
-	 * Prints the interface header to the screen.
+	 * Prints a specific interface message to the screen.
+	 * @param $msgID The ID of the message to print to the screen.
 	 */
-	public function displayheader() {
+	public function displayMessage($msgID) {
 		// Get DB object from index file.
 		global $tsSQL;
 		
 		// Formulates and executes SQL query to return the required message.
-		$result = $tsSQL->query("SELECT * FROM acc_emails WHERE mail_id = '8';");
+		$result = $tsSQL->query("SELECT * FROM acc_emails WHERE mail_id = '$msgID';");
 		
 		// Display an error message if the query fails.
 		if (!$result) {
@@ -41,24 +42,19 @@ class skin {
 	}
 	
 	/**
+	 * Prints the interface header to the screen.
+	 */
+	public function displayheader() {
+		// Displayes the interface header.
+		$this->displayMessage(8);
+	}
+	
+	/**
 	 * Prints the interface footer to the screen.
 	 */
 	public function displayfooter() {
-		// Get DB object from index file.
-		global $tsSQL;
-		
-		// Formulates and executes SQL query to return the required message.
-		$result = $tsSQL->query("SELECT * FROM acc_emails WHERE mail_id = '22';");
-		
-		// Display an error message if the query fails.
-		if (!$result) {
-			// TODO: Nice error message
-			die("ERROR: No result returned.");
-		}
-		
-		// Assigns the required row to a variable and print it to the screen.
-		$row = mysql_fetch_assoc($result);
-		echo $row['mail_text'];
+		// Displayes the interface header.
+		$this->displayMessage(22);
 	}
 	
 	/**
