fc089dd95769ba6f0613201f9d77cb550a9bd2c9
Lourie Pieterse <louriepieterse@yahoo.com>
2009-12-16 14:58:40 +0000

    Made use of the config file.
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@2792 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- backup.php
+++ backup.php
@@ -16,25 +16,18 @@ if (isset($_SERVER['REQUEST_METHOD'])) {
     die();
 } //Web clients die.
 
+// Get all the classes.
+require_once 'config.inc.php';
+
 //ini_set('display_errors', '1');
 
 echo "Initialising backup script\n";
-
-//Config
-$basefile = "backup";
-$dir = "/home/project/a/c/c/acc/backups";
-$monthdir = $dir . "/monthly";
-$dumper = "/usr/bin/mysqldump --defaults-file=~/.my.cnf p_acc_live"; //add params here if they are needed.
-$gzip = "/bin/gzip"; //add params here too if needed.
-$tar = "/bin/tar -cvf";
-
-echo "Loaded configuration\n";
 
 $arg = $argv['1'];
 if( $arg == "--monthly" ) {
 	echo "running monthly backups.\n";
 	$dateModifier = date( "FY" );
-	$cmdLine = "$tar $monthdir/mBackup-$dateModifier.tar $dir/*.sql.gz; rm $dir/*.sql.gz";
+	$cmdLine = "$BUtar $BUmonthdir/mBackup-$dateModifier.tar $BUdir/*.sql.gz; rm $BUdir/*.sql.gz";
 	echo "running command $cmdLine\n";
 	shell_exec( $cmdLine );
 	die( "done." );
@@ -42,9 +35,8 @@ if( $arg == "--monthly" ) {
 
 echo "running nightly backups\n";
 $dateModifier = date( "y-m-d" );
-$cmdLine = "$dumper > $dir/$basefile$dateModifier.sql; $gzip $dir/$basefile$dateModifier.sql";
+$cmdLine = "$BUdumper > $BUdir/$BUbasefile$dateModifier.sql; $BUgzip $BUdir/$BUbasefile$dateModifier.sql";
 echo "running command $cmdLine\n";
 shell_exec( $cmdLine );
 echo "done.";
 ?>
-
