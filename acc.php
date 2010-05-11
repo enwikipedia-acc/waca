enwikipedia-acc/waca

commit a4d11788d455d54ed8cf5ec0d5a8df8b2449e29a

fix two xss holes, plus one sql injection
git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@2884 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

acc.php
@@ -1302,7 +1302,7 @@ elseif ($action == "done" && $_GET['id'] != "") {
 	$towhom = $row2['pend_email'];
 	if ($gem != "0" and $gem != "custom") {
 		sendemail($gem, $towhom);
-		$query = "UPDATE acc_pend SET pend_emailsent = '1' WHERE pend_id = '" . $_GET['id'] . "';";
+		$query = "UPDATE acc_pend SET pend_emailsent = '1' WHERE pend_id = '" . $gid . "';";
 		$result = mysql_query($query, $tsSQLlink);
 	}
 	upcsum($_GET['id']);
@@ -1330,7 +1330,7 @@ elseif ($action == "logs") {
 		$filteruserl = " value=\"".$_GET['user']."\"";
 		$filteruser = $_GET['user'];
 	} else { $filteruserl = ""; $filteruser = "";}
-	
+	$filteruserl = xss($filteruserl);
 	
 	
 	echo '<h2>Logs</h2>

functions.php
@@ -138,6 +138,10 @@ function sanitize($what) {
 	return ($what);
 }
 
+function xss ($string) {
+	return htmlentities($string,ENT_QUOTES);
+}
+
 function upcsum($id) {
 	/*
 	* Updates the entries checksum (on each load of that entry, to prevent dupes)
@@ -584,13 +588,13 @@ function showlogin($action=null, $params=null) {
 	
 	// Would perform clause for any action except logout.
     if (($action) && ($action != "logout")) {    	
-		$html .= "&amp;newaction=" . $action;
+		$html .= "&amp;newaction=" . xss($action);
     	
 		// Create an array of all the values in the $GET variable.
 		// The variable supplied as the parameter would be used.
 		foreach ($params as $param => $value) { 
     		if ($param != '' && $param != "action") {
-    			$html .= "&amp;$param=".$value;
+    			$html .= "&amp;".xss($param)."=".xss($value);
     		}
     	}
     }

commit f5ec148b9316248765ce0ca38fb0cb215a8f67ff

Try and fix a bug which I suspect was caused by r2884. Double quotes get added to the Filter by username: box every time you submit a filter request. If you submit 2 in a row without changing the username box it becomes unable to find any logs.
git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@2902 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

acc.php
@@ -1328,11 +1328,10 @@ elseif ($action == "logout") {
 }
 elseif ($action == "logs") {
 	if(isset($_GET['user'])){
-		$filteruserl = " value=\"".$_GET['user']."\"";
 		$filteruser = $_GET['user'];
+		$filteruserl = xss($filteruser); 
+		$filteruserl = " value=\"".$filteruserl."\"";
 	} else { $filteruserl = ""; $filteruser = "";}
-	$filteruserl = xss($filteruserl);
-	
 	
 	echo '<h2>Logs</h2>
 	<form action="acc.php" method="get">