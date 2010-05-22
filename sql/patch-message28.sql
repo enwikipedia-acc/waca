25bf31633c21a576648456ea742f76201e218655
FunPika <funpikawiki@gmail.com>
2010-05-22 19:22:10 +0000

    yay I messed up an SQL comment....
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@2943 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- sql/patch-message28.sql
+++ sql/patch-message28.sql
@@ -1,3 +1,3 @@
---Insert message 28 "Declined - Sul Taken" into message table
+-- Insert message 28 "Declined - Sul Taken" into message table
 
 INSERT INTO acc_emails (mail_id, mail_text, mail_count, mail_desc, mail_type) VALUES(28,'random default text',0,'Declined - SUL Taken','Interface');
