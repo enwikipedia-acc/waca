08913f4f919aba4c49848fdbbf3f3796607b82ac
FunPika <funpikawiki@gmail.com>
2010-05-30 16:13:58 +0000

    Why is the userid showing as the original reserver instead of their actual name?
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@2985 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- acc.php
+++ acc.php
@@ -1561,11 +1561,12 @@ elseif ($action == "breakreserve") {
 					if (!$result)
 						Die("Error unreserving request.");
 					$now = date("Y-m-d H-i-s");
-					$query = "INSERT INTO acc_log (log_pend, log_user, log_action, log_cmt, log_time) VALUES ('$request', '".sanitise($_SESSION['user'])."', 'BreakReserve', $session->getUsernameFromUid($reservedBy), '$now');";
+					$reservename = $session->getUsernameFromUid($reservedBy);
+					$query = "INSERT INTO acc_log (log_pend, log_user, log_action, log_cmt, log_time) VALUES ('$request', '".sanitise($_SESSION['user'])."', 'BreakReserve', $reservename, '$now');";
 					$result = mysql_query($query, $tsSQLlink);
 					if (!$result)
 						Die("Query failed: $query ERROR: " . mysql_error());
-					$accbotSend->send("Reservation on Request $request broken by " . $session->getUsernameFromUid($_SESSION['userID']));
+					$accbotSend->send("Reservation on Request $request broken by" . $session->getUsernameFromUid($_SESSION['userID']));
 					echo defaultpage();
 				}
 				else
