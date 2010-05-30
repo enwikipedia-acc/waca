9a1e5e3e51ae09138c0ce1cfc38b3038fe273659
FunPika <funpikawiki@gmail.com>
2010-05-30 20:14:37 +0000

    Don't show the original reserver in the logs when it is force broken, at least for now.
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@2990 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- acc.php
+++ acc.php
@@ -1578,8 +1578,7 @@ elseif ($action == "breakreserve") {
 					if (!$result)
 						Die("Error unreserving request.");
 					$now = date("Y-m-d H-i-s");
-					$reservename = "$session->getUsernameFromUid($reservedBy)";
-					$query = "INSERT INTO acc_log (log_pend, log_user, log_action, log_cmt, log_time) VALUES ('$request', '".sanitise($_SESSION['user'])."', 'BreakReserve', '$reservename', '$now');";
+					$query = "INSERT INTO acc_log (log_pend, log_user, log_action, log_time) VALUES ('$request', '".sanitise($_SESSION['user'])."', 'BreakReserve', '$now');";
 					$result = mysql_query($query, $tsSQLlink);
 					if (!$result)
 						Die("Query failed: $query ERROR: " . mysql_error());
