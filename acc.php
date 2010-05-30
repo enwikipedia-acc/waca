enwikipedia-acc/waca

commit 1c49aefa2b843fe6b155094f342ae5470e984e45

Try to make it so that people other than the reserver can see the info for a request by adding a hashed parameter. The reserving user should be able to find out what the hash is and then give it to others.
git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@2976 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

acc.php
@@ -1321,7 +1321,13 @@ elseif ($action == "zoom") {
 		$skin->displayIfooter();
 		die();
 	}
-	echo zoomPage($_GET['id']);
+	if (isset($_GET['hash'])) {
+		$urlhash = $_GET['hash'];
+	}
+	else {
+		$urlhash = "";
+	}
+	echo zoomPage($_GET['id'],$urlhash);
 	$skin->displayIfooter();
 	die();
 }

functions.php
@@ -215,7 +215,7 @@ function sendemail($messageno, $target, $id) {
 	mail($target, "RE: [ACC #$id] English Wikipedia Account Request", $mailtxt, $headers);
 }
 
-function listrequests($type, $hideip) {
+function listrequests($type, $hideip, $correcthash) {
 	/*
 	 * List requests, at Zoom, and, on the main page
 	 */
@@ -289,7 +289,7 @@ function listrequests($type, $hideip) {
 		sqlerror("Query failed: $query ERROR: " . mysql_error(),"Database query error.");
 		$row4 = mysql_fetch_assoc($result4);
 
-		if ($hideip == FALSE || $session->hasright($_SESSION['user'], 'Admin') || $session->isCheckuser($_SESSION['user']) ) {
+		if ($hideip == FALSE || $correcthash == TRUE || $session->hasright($_SESSION['user'], 'Admin') || $session->isCheckuser($_SESSION['user']) ) {
 			// Email.
 			$out .= '[ </small></td>';
 			$out .= '<td><small><a class="request-src" href="mailto:' . $row['pend_email'] . '">' . $row['pend_email'] . '</a>';
@@ -311,7 +311,7 @@ function listrequests($type, $hideip) {
 		}
 
 
-		if ($hideip == FALSE || $session->hasright($_SESSION['user'], 'Admin') || $session->isCheckuser($_SESSION['user']) ) {
+		if ($hideip == FALSE ||  $correcthash == TRUE || $session->hasright($_SESSION['user'], 'Admin') || $session->isCheckuser($_SESSION['user']) ) {
 			// IP UT:
 			$out .= '</span></small></td><td><small> | </small></td><td><small><a class="request-src" name="ip-link" href="'.$wikipediaurl.'wiki/User_talk:' . $row['pend_ip'] . '" target="_blank">';
 			$out .= $row['pend_ip'] . '</a> ';
@@ -753,7 +753,7 @@ function isOnWhitelist($user)
 	return false;
 }
 
-function zoomPage($id)
+function zoomPage($id,$urlhash)
 {
 	global $tsSQLlink, $session, $skin, $enableReserving;
 
@@ -810,19 +810,33 @@ function zoomPage($id)
 	} else {
 		$hideinfo = TRUE;
 	}
-	$requesttable = listrequests($thisid, $hideinfo);
+	if ($row['pend_status'] == "Closed") {
+		$hash = md5($thisid. $thisemail . $thisip . microtime()); //If the request is closed, change the hash based on microseconds similar to the checksums. 
+	} else {
+		$hash = md5($thisid . $thisemail . $thisip);
+	}
+	if ($hash == $urlhash) {
+		$correcthash = TRUE;
+	}
+	else {
+		$correcthash = FALSE;
+	}
+	$requesttable = listrequests($thisid, $hideinfo, $correcthash);
 	$out .= $requesttable;
 
 	//Escape injections.
 	$out .= "<br /><strong>Requester Comment</strong>: " . $row['pend_cmt'] . "<br />\n";
 
-	global $enableReserving;
+	global $enableReserving, $tsurl;
 	if( $enableReserving )
 	{
 		$reservingUser = isReserved($thisid);
 		if( $reservingUser != 0 )
 		{
 			$out .= "<h3>This request is currently being handled by " . $session->getUsernameFromUid($reservingUser) ."</h3>";
+		}
+		if ( $reservingUser == $_SESSION['userID']) {
+			$out .= '<p><b>URL to allow other users to see IP/Email:</b> <a href="acc.php?action=zoom&amp;id=' . $thisid . '&amp;hash=' . $hash . '">' . $tsurl . '/acc.php?action=zoom&id=' . $thisid . '&hash=' . $hash . '</a></p>';
 		}
 	}
 

commit ff0a3cc55fabfd519e467b2a330c35c3a8431101

Fixing bug introduced in r2976-r2679
git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@2982 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

acc.php
@@ -1601,14 +1601,22 @@ elseif ($action == "breakreserve") {
 }
 
 elseif ($action == "comment") {
+	if (isset($_GET['hash'])) {
+		$urlhash = sanitise($_GET['hash']);
+	} else {
+	    $urlhash = "";
+	}
+       
     if( isset($_GET['id']) ) {
         $id = $_GET['id'];
-        echo "<h2>Comment on request <a href='acc.php?action=zoom&amp;id=$id'>#$id</a></h2>";
+        echo "<h2>Comment on request <a href='acc.php?action=zoom&amp;id=$id&amp;hash=$urlhash'>#$id</a></h2>
+              <form action='acc.php?action=comment-add&amp;hash=$urlhash' method='post'>";
     } else {
         $id = "";
-         echo "<h2>Comment on a request</h2>";
+        echo "<h2>Comment on a request</h2>
+              <form action='acc.php?action=comment-add' method='post'>";
     }
-    echo "<form action='acc.php?action=comment-add' method='post'>
+    echo "
     Request ID: <input type='text' name='id' value='$id' /> <br />
     Comments:   <input type='text' name='comment' size='75' /> <br />
     Visibility: <select name='visibility'><option>user</option><option>admin</option></select>
@@ -1627,13 +1635,20 @@ elseif ($action == "comment-add") {
         $visibility = sanitise($_POST['visibility']);
         $now = date("Y-m-d H-i-s");
         
+        if (isset($_GET['hash'])) {
+		$urlhash = sanitise($_GET['hash']);
+	    }
+	    else {
+		$urlhash = "";
+	    }
+        
         // the mysql field name is actually cmt_visability. yes, it's a typo, but I cba to fix it
 		$query = "INSERT INTO acc_cmt (cmt_time, cmt_user, cmt_comment, cmt_visability, pend_id) VALUES ('$now', '$user', '$comment', '$visibility', '$id');";
 		$result = mysql_query($query, $tsSQLlink);
 		if (!$result) {
             Die("Query failed: $query ERROR: " . mysql_error()); }
         echo " Comment added Successfully! <br />
-        <a href='acc.php?action=zoom&id=$id'>Return to request #$id</a>";
+        <a href='acc.php?action=zoom&amp;id=$id&amp;hash=$urlhash'>Return to request #$id</a>";
         $botcomment_pvt =  ($visibility == "admin") ? "private " : "";
         $botcomment = $user . " posted a " . $botcomment_pvt . "comment on request " . $id;
         if($visibility != 'admin')
@@ -1664,7 +1679,13 @@ elseif ($action == "comment-quick") {
         }
         $botcomment = $user . " posted a comment on request " . $id . ': ' . $comment;
         $accbotSend->send($botcomment);
-        echo zoomPage($id);
+        if (isset($_GET['hash'])) {
+		$urlhash = $_GET['hash'];
+	    }
+	    else {
+		$urlhash = "";
+	    }
+        echo zoomPage($id,$urlhash);
         $skin->displayIfooter();
 		die();
     }