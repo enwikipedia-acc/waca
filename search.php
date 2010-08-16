4f61818d1ec72f46d11dd5c011e73c71e677fa06
FunPika <funpikawiki@gmail.com>
2010-08-16 22:55:14 +0000

    for some reason now it seems like ip2long is not converting the IPs correctly....
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@3144 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- search.php
+++ search.php
@@ -127,9 +127,10 @@ if( isset($_GET['term'])) {
 		}
 		
 		if ($cidr != '32') {
+			$endrange = $term + pow(2, (32-$cidr)) - 1;
 			$termlong = ip2long($term);
-			$endrange = $termlong + pow(2, (32-$cidr)) - 1;
-			echo $term . 'to' . long2ip($endrange);
+			$endrange = ip2long($endrange);
+			echo ip2long($_GET['term']);
 			$query = "SELECT pend_id,pend_ip,pend_name,pend_date,pend_status FROM acc_pend WHERE inet_aton('pend_ip') between '$termlong' and '$endrange';";
 			echo $query;
 		}
