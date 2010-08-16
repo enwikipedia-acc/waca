7b552c2d35b7929b03cb977ba5ffaabfb4c36010
FunPika <funpikawiki@gmail.com>
2010-08-16 22:35:20 +0000

    Curse confusing function naming...AGAIN.
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@3141 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- search.php
+++ search.php
@@ -127,7 +127,7 @@ if( isset($_GET['term'])) {
 		}
 		
 		if ($cidr != '32') {
-			$termlong = iptolong($term);
+			$termlong = ip2long($term);
 			$endrange = $termlong + pow(2, (32-$cidr));
 			$query = "SELECT pend_id,pend_ip,pend_name,pend_date,pend_status FROM acc_pend WHERE inet_aton('pend_ip') between '$termlong' and '$endrange';";
 		}
