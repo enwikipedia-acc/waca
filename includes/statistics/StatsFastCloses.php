ce2a218a7236d8be566d72e587e8e0142a21bc76
Simon Walker <simon@stwalkerster.co.uk>
2010-08-20 20:18:07 +0000

    revert r3181 , fix unnecessary, was caused by a database patch being applied to live, but not sand
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@3182 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- includes/statistics/StatsFastCloses.php
+++ includes/statistics/StatsFastCloses.php
@@ -18,9 +18,37 @@ class StatsFastCloses extends StatisticsPage
 {
 	function execute()
 	{
+		$query = <<<QUERY
+SELECT
+  Closed.log_pend AS Request,
+  Closed.log_user AS User,
+  TIMEDIFF(Closed.log_time, Reserved.log_time) AS "Time Taken",
+  mail_desc AS "Close Type",
+  Closed.log_time AS "Date"
+FROM acc_log Closed
+INNER JOIN acc_log Reserved 
+  ON Closed.log_pend = Reserved.log_pend
+INNER JOIN closes c
+  ON c.`CONCAT("Closed ",mail_id)` = Closed.log_action
+WHERE
+  Closed.log_action != "Closed 4"
+  AND
+  Closed.log_action LIKE "Closed%"
+  AND
+  Reserved.log_action = "Reserved"
+  AND
+  TIMEDIFF(Closed.log_time, Reserved.log_time) < "00:00:30"
+  AND
+  Closed.log_user = Reserved.log_user
+  AND
+  TIMEDIFF(Closed.log_time, Reserved.log_time) > "00:00:00"
+ORDER BY 
+  TIMEDIFF(Closed.log_time, Reserved.log_time) ASC
+;
+QUERY;
 		global $tsurl;
 		$qb = new QueryBrowser();
-		return $qb->executeQueryToTable('SELECT Closed.log_pend AS Request, Closed.log_user AS User, TIMEDIFF(Closed.log_time, Reserved.log_time) AS "Time Taken", mail_desc AS "Close Type", Closed.log_time AS "Date" FROM acc_log Closed INNER JOIN acc_log Reserved ON Closed.log_pend = Reserved.log_pend INNER JOIN closes c ON c.`CONCAT("Closed ",mail_id)` = Closed.log_action WHERE Closed.log_action != "Closed 4" AND Closed.log_action LIKE "Closed%" AND Reserved.log_action = "Reserved" AND TIMEDIFF(Closed.log_time, Reserved.log_time) < "00:00:30" AND Closed.log_user = Reserved.log_user AND TIMEDIFF(Closed.log_time, Reserved.log_time) > "00:00:00" ORDER BY TIMEDIFF(Closed.log_time, Reserved.log_time) ASC;'); 
+		return $qb->executeQueryToTable($query); 
 	}
 	function getPageName()
 	{
