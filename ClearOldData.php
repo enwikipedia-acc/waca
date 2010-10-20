05c0e095a9039c9564b5ce117b521d4d51e82331
EdoDodo <dodo@toolserver.org>
2010-10-20 05:38:06 +0000

    Start work on task #ACC-155: Creating a script that will replace sensitive data with generic data after three months, as stwalkerster suggested.
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@3290 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

---
+++ ClearOldData.php
@@ -0,0 +1,11 @@
+<?php
+ini_set('display_errors', 1);
+require_once 'config.inc.php';
+
+mysql_pconnect($toolserver_host, $toolserver_username, $toolserver_password);
+@ mysql_select_db($toolserver_database) or sqlerror(mysql_error(), "Error selecting database.");
+mysql_query("UPDATE acc_pend SET pend_ip = '127.0.0.1', pend_email = 'acc@toolserver.org' WHERE pend_date < DATE_SUB(curdate(), interval 3 MONTH);");
+mysql_close();
+
+echo "Deletion complete.";
+?>
