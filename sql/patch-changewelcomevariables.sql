c62b1f12a93e462b360482be70d0f1205ec0621c
EdoDodo <dodo@toolserver.org>
2010-10-20 15:01:34 +0000

    Delete PHP script added in r3312 since I realized there's a far simpler way of doing it using just MySQL script from r3312.
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@3314 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- sql/patch-changewelcomevariables.sql
+++ sql/patch-changewelcomevariables.sql
@@ -1,3 +1,4 @@
-ALTER TABLE `acc_user` ALTER COLUMN `user_welcome_sig` SET DEFAULT '';
-ALTER TABLE `acc_user` ALTER COLUMN `user_welcome_templateid` SET DEFAULT '0';
-ALTER TABLE `acc_user` DROP COLUMN `user_welcome` SET DEFAULT '1';
+ALTER TABLE `acc_user` ALTER COLUMN `user_welcome_templateid` SET DEFAULT '0';"
+ALTER TABLE `acc_user` ALTER COLUMN `user_welcome_sig` SET DEFAULT '';"
+UPDATE `acc_user` SET `user_welcome_templateid` = '0' WHERE `user_welcome` < '1';"
+ALTER TABLE `acc_user` DROP COLUMN `user_welcome`;"
