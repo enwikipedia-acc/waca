5d6e409962478304e9a84e255eacdd94a875a68d
EdoDodo <dodo@toolserver.org>
2010-10-26 13:10:30 +0000

    Fix mistake that occurred because I forgot to replace user_welcome here when I depreceated it. Search confirms that this si the last remaining instance of it.
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@3341 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- accbot.php
+++ accbot.php
@@ -419,7 +419,7 @@
 
 			irc( 'PRIVMSG ' . $parsed['to'] . ' :' . $username . ' (' . $user['user_level'] . ') was last active '
 				. ( ( $user['user_lastactive'] == '0000-00-00 00:00:00' ) ? 'unknown' : $user['user_lastactive'] )
-				. '. He/she currently has automatic welcoming of users ' . ( ( $user['user_welcome'] == 1 ) ? 'enabled' : 'disabled' )
+				. '. He/she currently has automatic welcoming of users ' . ( ( $user['user_welcome_templateid'] == 0 ) ? 'disabled' : 'enabled' )
 				. '. His/her onwiki username is [[User:' . $user['user_onwikiname'] . ']].' );
 
 		} else {
