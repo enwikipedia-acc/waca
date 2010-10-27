7b149f29535754786c90dc40e75be534aa282c28
EdoDodo <dodo@toolserver.org>
2010-10-27 18:26:17 +0000

    Tweak RecreateTrustedIP.php to minimize the time that the table is empty.
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@3353 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- RecreateTrustedIPs.php
+++ RecreateTrustedIPs.php
@@ -8,21 +8,20 @@ require_once 'config.inc.php';
 $htmlfile = file_get_contents('http://www.wikimedia.org/trusted-xff.html');
 $matches = array();
 $machfound = preg_match_all('(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)', $htmlfile, $matches);
-$insertquery = 'INSERT INTO `acc_trustedips` (`trustedips_ipaddr`) VALUES ';
+$sqlquery = 'SET TRANSACTION ISOLATION LEVEL SERIALIZABLE; START TRANSACTION; TRUNCATE TABLE `acc_trustedips`; INSERT INTO `acc_trustedips` (`trustedips_ipaddr`) VALUES ';
 if ($matchfound) {
 	foreach ($matches as $match) {
 		$ip = $match[0];
-		$insertquery .= "('$ip'), ";
+		$sqlquery .= "('$ip'), ";
 	}
 } else {
 	die('ERROR: No IPs found on trusted XFF page.');
 }
-$insertquery = substr($insertquery, 0, -2) . ';';
+$sqlquery = substr($sqlquery, 0, -2) . '; COMMIT;';
 
 mysql_connect($toolserver_host, $toolserver_username, $toolserver_password);
 @ mysql_select_db($toolserver_database) or die(mysql_error());
-mysql_query('TRUNCATE TABLE `acc_trustedips`;');
-mysql_query($insertquery);
+mysql_query($sqlquery);
 mysql_close();
 
 echo "The trusted IPs table has been recreated.";
