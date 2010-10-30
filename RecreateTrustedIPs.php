189ca36bef686e952da217792fc5e15a94e14ac0
Simon Walker <simon@stwalkerster.co.uk>
2010-10-30 18:00:21 +0000

    fix loop
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@3372 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- RecreateTrustedIPs.php
+++ RecreateTrustedIPs.php
@@ -9,9 +9,10 @@ $htmlfile = file_get_contents('http://www.wikimedia.org/trusted-xff.html');
 $matchfound = preg_match_all('/(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/', $htmlfile, $matches, PREG_SET_ORDER);
 if (!$matchfound)
 	die('ERROR: No IPs found on trusted XFF page.');
+
 $sqlquery = 'INSERT INTO `acc_trustedips` (`trustedips_ipaddr`) VALUES ';
-foreach ($matches[0] as $match) {
-	$ip = $match;
+foreach ($matches as $match) {
+	$ip = $match[0];
 	$sqlquery .= "('$ip'), ";
 }
 $sqlquery = substr($sqlquery, 0, -2) . ';';
