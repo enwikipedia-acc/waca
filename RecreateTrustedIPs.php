9862a20334565d5cb7effb422816692914658d41
Simon Walker <simon@stwalkerster.co.uk>
2010-10-30 16:28:50 +0000

    add rollback capability to transaction
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@3368 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- RecreateTrustedIPs.php
+++ RecreateTrustedIPs.php
@@ -1,6 +1,6 @@
 <?php
 if (isset($_SERVER['REQUEST_METHOD'])) {
-    die();
+	die();
 } // Web clients die.
 ini_set('display_errors', 1);
 require_once 'config.inc.php';
@@ -8,7 +8,7 @@ require_once 'config.inc.php';
 $htmlfile = file_get_contents('http://www.wikimedia.org/trusted-xff.html');
 $matches = array();
 $machfound = preg_match_all('/(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/', $htmlfile, $matches);
-$sqlquery = 'SET TRANSACTION ISOLATION LEVEL SERIALIZABLE; START TRANSACTION; TRUNCATE TABLE `acc_trustedips`; INSERT INTO `acc_trustedips` (`trustedips_ipaddr`) VALUES ';
+$sqlquery = 'INSERT INTO `acc_trustedips` (`trustedips_ipaddr`) VALUES ';
 if ($matchfound) {
 	foreach ($matches as $match) {
 		$ip = $match[0];
@@ -17,12 +17,35 @@ if ($matchfound) {
 } else {
 	die('ERROR: No IPs found on trusted XFF page.');
 }
-$sqlquery = substr($sqlquery, 0, -2) . '; COMMIT;';
+$sqlquery = substr($sqlquery, 0, -2) . ';';
 
 mysql_connect($toolserver_host, $toolserver_username, $toolserver_password);
 @ mysql_select_db($toolserver_database) or die(mysql_error());
-mysql_query($sqlquery);
+
+mysql_query("SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;");
+
+if(mysql_query("START TRANSACTION;"))
+{
+	$success = true;
+	$success = mysql_query("TRUNCATE TABLE `acc_trustedips`;") && $success;
+	$success = mysql_query($sqlquery) && $success;
+
+
+	if($success)
+	{
+		mysql_query("COMMIT;");
+		echo "The trusted IPs table has been recreated.\n";
+	}
+	else
+	{
+		mysql_query("ROLLBACK;");
+		echo "Error in transaction.\n";
+	}
+}
+else
+	echo "Error starting transaction.\n";
+	
 mysql_close();
 
-echo "The trusted IPs table has been recreated.";
+
 ?>
