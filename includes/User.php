f11ca348802de4cfc56abae5c48e87b441d8435a
Simon Walker <simon@stwalkerster.co.uk>
2011-02-14 22:59:27 +0000

    generate a few more functions, these should just need implementing now
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@3468 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- includes/User.php
+++ includes/User.php
@@ -1,36 +1,75 @@
 <?php
 
+/**
+ * User class, describes everything that can be done with a user.
+ * @author stwalkerster
+ *
+ */
 class User
 {
-	static function getCurrentUser()
+	/**
+	 * Gets the current user logged in from the session
+	 * @return User the currently logged in user
+	 */
+	public static function getCurrentUser()
 	{
-		
-	}	
-	
-	static function getById($id)
-	{
-		
+
 	}
-	
-	static function getByName()
+
+	/**
+	 * Gets the user with the specified ID
+	 * @param integer $id The ID of the user
+	 * @return User the user with the specifed ID
+	 */
+	public static function getById($id)
 	{
-		
+
 	}
-	
-	private static function encryptPassword($password)
+
+	/**
+	 * Gets the user with the specified username
+	 * @param string $id The username of the user
+	 * @return User the user with the specifed username
+	 */
+	public static function getByName($name)
+	{
+
+	}
+
+
+	/**
+	 * Encrypts a password for storing in the database or authentication
+	 * @param string $password The plaintext password
+	 * @param integer $version The version of the encryption algorithm to use
+	 * @return string the encrypted password string
+	 */
+	private function encryptPassword($password, $version=1)
 	{
 		return md5($password);
 	}
-	
-	private $uIsNew = 0;
-	private $uPassword = "";
-	
-	function __construct($username, $password, $wikiname, $email, $usesecure)
+
+	public function __construct($username, $password, $wikiname, $email, $usesecure)
 	{
 		$this->uIsNew = 1;
 	}
-	
-	function save()
+
+	private $uIsNew = 0;
+	private $uId = 0;
+	private $uName = "";
+	private $uEmail = "";
+	private $uPassword = "";
+	private $uAccessLevel = "";
+	private $uOnwikiName = "";
+	private $uWelcomeSignature = "";
+	private $uLastActive = "";
+	private $uLastIp = "";
+	private $uForceLogout = "";
+	private $uSecureServer = "";
+	private $uIsCheckuser = "";
+	private $uIsIdentified = "";
+	private $uWelcomeTemplateId = "";
+
+	public function save()
 	{
 		if($this->isNew)
 		{
@@ -42,15 +81,66 @@ class User
 		}
 	}
 
-	function authenticate($password)
+	public function authenticate($password)
 	{
 		return
-			$this->uPassword == User::encryptPassword($password) ?
-			true : false;
+		$this->uPassword == $this->encryptPassword($password) ?
+		true : false;
 	}
 
-	function setPassword($newPassword)
+	public function setPassword($newPassword)
 	{
-		$this->uPassword = User::encryptPassword($newPassword);
+		$this->uPassword = $this->encryptPassword($newPassword);
 	}
+
+	public function getName() { return $uName;}
+
+	public function getEmail() { return $uEmail;}
+	public function setEmail($newEmail){$uEmail = $newEmail;}
+
+	public function getAccessLevel() { return $uAccessLevel;}
+
+	public function getOnwikiName() { return $uOnwikiName;}
+	public function setOnwikiName($newOnwikiName){$uOnwikiName = $newOnwikiName;}
+
+	public function getWelcomeSignature() { return $uWelcomeSignature;}
+	public function setWelcomeSignature($newWelcomeSignature){$uWelcomeSignature = $newWelcomeSignature;}
+
+	public function getLastActive() { return $uLastActive;}
+
+	public function getLastIp() { return $uLastIp;}
+
+	public function getForceLogout() { return $uForceLogout;}
+
+	public function getSecureServer() { return $uSecureServer;}
+	public function setSecureServer($newSecureServer){$uSecureServer = $newSecureServer;}
+
+	public function getIsCheckuser() { return $uIsCheckuser;}
+
+	public function getIsIdentified() { return $uIsIdentified;}
+
+	public function getWelcomeTemplateId() { return $uWelcomeTemplateId;}
+	public function setWelcomeTemplateId($newWelcomeTemplateId){$uWelcomeTemplateId = $newWelcomeTemplateId;}
+
+
+
+	//---------
+
+
+	public function rename($newName){$uName = $newName;}
+	public function promote(){}
+	public function demote($reason){}
+	public function decline($reason){}
+	public function approve(){}
+	public function suspend($reason){}
+	
+	
+	/**
+	 * Sets the last access time for the user
+	 */
+	public function touch(){
+		$uLastIp = $_SERVER['REMOTE_ADDR'];
+		$uLastActive = date(DATE_ATOM);
+	}
+
 }
