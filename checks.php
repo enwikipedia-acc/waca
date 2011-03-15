b92141b41d10a0bc40af0c2e6687c9e653ef3d57
Simon Walker <simon@stwalkerster.co.uk>
2011-03-15 05:02:04 +0000

    general cleanup, svn:eol-style=native (fixing r3471 )
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@3496 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- checks.php
+++ checks.php
@@ -1,16 +1,16 @@
 <?php
 /**************************************************************************
-**********      English Wikipedia Account Request Interface      **********
-***************************************************************************
-** Wikipedia Account Request Graphic Design by Charles Melbye,           **
-** which is licensed under a Creative Commons                            **
-** Attribution-Noncommercial-Share Alike 3.0 United States License.      **
-**                                                                       **
-** All other code are released under the Public Domain                   **
-** by the ACC Development Team.                                          **
-**                                                                       **
-** See CREDITS for the list of developers.                               **
-***************************************************************************/
+ **********      English Wikipedia Account Request Interface      **********
+ ***************************************************************************
+ ** Wikipedia Account Request Graphic Design by Charles Melbye,           **
+ ** which is licensed under a Creative Commons                            **
+ ** Attribution-Noncommercial-Share Alike 3.0 United States License.      **
+ **                                                                       **
+ ** All other code are released under the Public Domain                   **
+ ** by the ACC Development Team.                                          **
+ **                                                                       **
+ ** See CREDITS for the list of developers.                               **
+ ***************************************************************************/
 
 // Get all the classes.
 require_once 'config.inc.php';
@@ -62,4 +62,3 @@ echo "<br/><p>ACC is kindly hosted by the Wikimedia Toolserver. Our code resposi
 
 // Display the footer of the interface.
 $skin->displayPfooter();
-?>
