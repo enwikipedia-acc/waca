af8573005f72146e25d4881bdc2b7fe62c9f1405
Simon Walker <simon@stwalkerster.co.uk>
2011-04-08 22:46:39 +0000

    tweak r3511 :
    oldid => diff
    
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@3530 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- includes/statistics/StatsUsers.php
+++ includes/statistics/StatsUsers.php
@@ -115,7 +115,7 @@ class StatsUsers extends StatisticsPage
 		$out.= "<li>User Level: " . $row['user_level'] . "</li>\n";
 		$out.= "<li>User On-wiki name: <a href=\"http://$wikiurl/wiki/User:" . $row['user_onwikiname'] . "\">" . $row['user_onwikiname'] . "</a>  |  <a href=\"http://$wikiurl/wiki/User talk:" . $row['user_onwikiname'] . "\">talk page</a> </li>\n";
 		if($row['user_confirmationdiff']!=0){
-		$out .= "<li><a href=\"http://$wikiurl/w/index.php?oldid=".$row['user_confirmationdiff']."\">Confirmation diff</a></li>";
+		$out .= "<li><a href=\"http://$wikiurl/w/index.php?diff=".$row['user_confirmationdiff']."\">Confirmation diff</a></li>";
 		}
 		// Display the date and time of the user's last activity on the interface;
 		// If the user has not used the interface, display message
