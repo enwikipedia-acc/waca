b531bdd3f60713b24258f77dadced82d2c9a5eb1
Thehelpfulone <thehelpfulonewiki@gmail.com>
2011-06-12 23:02:15 +0000

    add myself
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@3594 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- accbot.php
+++ accbot.php
@@ -102,6 +102,7 @@ ini_set('display_errors',1);
 		'*!*@wikipedia/Manishearth' 				=> 'developer',
 		'*!*@Wikipedia/MacMed'						=> 'developer',
 		'*!*@Wikipedia/Logan'						=> 'developer',
+		'*!*@wikimedia/Thehelpfulone'				=> 'developer',
 		
 		'Helpmebot!~hmb6@wikimedia/bot/helpmebot'	=> 'bot',
                 'Helpmebot!helpmebot@wikimedia/bot/helpmebot'       => 'bot',
