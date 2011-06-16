b87185ddbe7389eaa46c505e4eab8a32ad96b639
EdoDodo <dodo@toolserver.org>
2011-06-16 12:49:15 +0000

    Move pager creation code to a function to avoid code repetition.
    git-svn-id: https://waca.svn.sourceforge.net/svnroot/waca/trunk/acc@3606 41b7b2fa-e25d-4c8f-a56d-f93ce3f2ce94

--- LogClass.php
+++ LogClass.php
@@ -19,6 +19,40 @@ class LogPage
 	var $filterRequest = "";
 	var $showPager = true;
 	
+	private function createPager($offset, $limit, $logListCount) {
+		$pager = '';
+		if($offset != 0)
+		{
+			$backOffset = ($offset < $limit) ? 0 : $offset - $limit;
+			$earliestLink = $this->swapUrlParams($limit, 0);
+			$urlParams = $this->swapUrlParams($limit, $backOffset);
+			$pager.= '<a href="?'.$earliestLink.'>Earliest</a> | <a href="?'.$urlParams.'">Previous '.$limit.'</a> | ';
+		}
+
+		if($logListCount == $limit)
+		{
+			$forwardOffset = $offset + $limit;
+			$earliestLink = $this->swapUrlParams($limit, $forwardOffest - $limit);
+			$urlParams = $this->swapUrlParams($limit, $forwardOffset);
+			$pager.= '<a href="?'.$urlParams.'">Next '.$limit.'</a> | <a href="?'.$earliestLink.'>Latest</a>';
+		}
+		elseif ($offset != 0) {
+			$pager = substr($pager, 0, -3);
+		}
+
+		$pager .= "<br /> Set limit: ";
+		$potentialLimits = array(20, 50, 100, 250, 500);
+		foreach ($potentialLimits as $potentialLimit) {
+			if ($potentialLimit != $limit && ($logListCount == $limit || $potentialLimit < $limit)) {
+				$urlParams = $this->swapUrlParams($potentialLimit, $offset);
+				$pager .= "<a href='?$urlParams'>$potentialLimit</a>";
+			} else {
+				$pager .= $potentialLimit;
+			}
+			$pager .= " | ";
+		}
+		$pager = substr($pager, 0, -3);
+	}
 	
 	private function getLog($offset = 0, $limit = 100)
 	{
@@ -289,59 +323,11 @@ class LogPage
 		}
 		else
 		{
-			if($this->showPager == true){
-				if($offset != 0)
-				{
-					$backOffset = ($offset < $limit) ? 0 : $offset - $limit;
-					$earliestLink = $this->swapUrlParams($limit, 0);
-					$urlParams = $this->swapUrlParams($limit, $backOffset);
-					$out.= '<a href="?'.$earliestLink.'>Earliest</a> | <a href="?'.$urlParams.'">Previous '.$limit.'</a> | ';
-				}
-
-				if($logListCount == $limit)
-				{
-					$forwardOffset = $offset + $limit;
-					$earliestLink = $this->swapUrlParams($limit, $forwardOffest - $limit);
-					$urlParams = $this->swapUrlParams($limit, $forwardOffset);
-					$out.= '<a href="?'.$urlParams.'">Next '.$limit.'</a> | <a href="?'.$earliestLink.'>Latest</a>';
-				}
-				elseif ($offset != 0) {
-					$out = substr($out, 0, -3);
-				}
-				
-				$out .= "<br /> Set limit: ";
-				$potentialLimits = array(20, 50, 100, 250, 500);
-				foreach ($potentialLimits as $potentialLimit) {
-					if ($potentialLimit != $limit && ($logListCount == $limit || $potentialLimit < $limit)) {
-						$urlParams = $this->swapUrlParams($potentialLimit, $offset);
-						$out .= "<a href='?$urlParams'>$potentialLimit</a>";
-					} else {
-						$out .= $potentialLimit
-					}
-					$out .= " | ";
-				}
-				$out = substr($out, 0, -3);
-			}
-			
-			$out.= "<ul>$logList</ul>";	
-			
-			if($this->showPager == true){
-							if($offset != 0)
-				{
-					$backOffset = ($offset < $limit) ? 0 : $offset - $limit;
-
-					$urlParams = $this->swapUrlParams($limit, $backOffset);
-					
-					$out.= '<a href="?'.$urlParams.'">Previous '.$limit.'</a> - ';
-				}
-
-				if($logListCount == $limit)
-				{
-					$forwardOffset = $offset + $limit;
-					$urlParams = $this->swapUrlParams($limit, $forwardOffset);
-					$out.= '<a href="?'.$urlParams.'">Next '.$limit.'</a>';
-				}
-			}
+			if($this->showPager == true)
+				$pager = $this->createPager($offset, $limit, $logListCount);
+			else
+				$pager = '';
+			$out.= "$pager<ul>$logList</ul>$pager";	
 		}
 		
 		return $out;
