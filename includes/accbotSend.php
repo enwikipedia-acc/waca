854fe738a9dbff54db84beaa4042137486534104
acc project <acc@toolserver.org>
2012-03-04 04:38:42 +0000

    hack notifications to use helpmebot.org.uk's mysql server

--- includes/accbotSend.php
+++ includes/accbotSend.php
@@ -20,17 +20,18 @@ if ($ACC != "1") {
 // accbot class
 class accbotSend {
 	public function send($message) {
-		global $whichami,$toolserver_notification_database;
+		global $whichami,$toolserver_username, $toolserver_password;
 		$message = html_entity_decode($message,ENT_COMPAT,'UTF-8'); // If a message going to the bot was for whatever reason sent through sanitze() earlier, reverse it. 
 		$message = stripslashes($message);
 		$blacklist = array("DCC", "CCTP", "PRIVMSG");
 		$message = str_replace($blacklist, "(IRC Blacklist)", $message); //Lets stop DCC etc
 
 		$msg = chr(2)."[$whichami]".chr(2).": $message";
-		
-		$database = new database("toolserver");
 
-		$database->query("insert into p_acc_notifications.notification values (null,null,1,'".$database->escape($msg)."');");
+		$dblink = mysql_connect("dbmaster.helpmebot.org.uk", $toolserver_username, $toolserver_password);
+		mysql_select_db("acc_notifications", $dblink);
+		mysql_query("insert into notification values (null,null,1,'".mysql_real_escape_string($msg,$dblink)."');", $dblink);
+		mysql_close($dblink);
 		
 		return;
 	}
