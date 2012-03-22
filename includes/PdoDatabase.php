3b56a0c741240c7ba26ced95934f193ae2ff2b8c
Simon Walker <github@stwalkerster.co.uk>
2012-03-22 22:27:20 +0000

    PdoDatabase class

---
+++ includes/PdoDatabase.php
@@ -0,0 +1,41 @@
+<?php
+
+class PdoDatabase extends PDO
+{
+	protected $hasActiveTransaction = false;
+
+	/**
+	 * Determines if this connection has a transaction in progress or not
+	 * @return true if there is a transaction in progress.
+	 */
+	public function hasActiveTransaction()
+	{
+		return $this->hasActiveTransaction;
+	}
+
+	public function beginTransaction () 
+	{
+		if ( $this->hasActiveTransaction ) 
+		{
+			return false;
+		} 
+		else 
+		{
+			$this->exec("SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;");
+			$this->hasActiveTransaction = parent::beginTransaction();
+			return $this->hasActiveTransaction;
+		}
+	}
+
+	public function commit () 
+	{
+		parent::commit();
+		$this->hasActiveTransaction = false;
+	}
+
+	public function rollBack () 
+	{
+		parent::rollback();
+		$this->hasActiveTransaction = false;
+	}
+}
