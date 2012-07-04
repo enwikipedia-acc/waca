efe88c1715588459039a80c4eb72a31a4f2cfe6d
Simon Walker <github@stwalkerster.co.uk>
2012-07-05 00:31:41 +0100

    remove deploy from API

--- api.php
+++ api.php
@@ -34,42 +34,12 @@ switch($_GET['action'])
 	case "stats":
 		actionStats();
 		break;
-	case "deploy":
-		actionDeploy();
-		break;
 	default:
 		$doc_api->setAttribute("actions", "count, status, stats");
 		break;
 }
 
 echo $document->saveXml();
-
-function actionDeploy()
-{
-	global $apiDeployPassword;
-	
-	$revision = isset( $_GET['r'] ) ? $_GET['r'] : '';
-	if( $revision == '' ) {
-		echo("Please specify a revision");
-		die;
-	}
-	
-	$key = isset( $_GET['k'] ) ? $_GET['k'] : '';
-	if( $key == '' ) {
-		echo("Please specify a key");
-		die;		
-	}
-	
-	
-	
-	if( md5( md5($revision) . $apiDeployPassword ) != $key ) {
-		echo("Invalid key.");
-		die;
-	}
-	
-	passthru( './deploy.sh ' . $revision  );
-	die;
-}
 
 function actionStatus()
 {
--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-git remote update origin
-
-git checkout $1
