33e2dbbe79bfcbfa431a019d67a74630a68eda20
Simon Walker <github@stwalkerster.co.uk>
2012-07-06 13:16:57 +0100

    update SGE jobs

--- sge/sge-backup.sh
+++ sge/sge-backup.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-#$ -l h_rt=0:30:00
+#$ -l h_rt=1:00:00
+#$ -l sql=1
+#$ -l virtual_free=500M
+#$ -l user_slot=10
 #$ -j y
 #$ -o $HOME/sge.backup.out
 #$ -m a
+
+
+### Resource limits:
+###  60 minute hard runtime (est)
+###  1 DB slot for sql
+###  500M virtual memory (est)
+###  10 user slots (Large operation on entire database / locking on backups directory)
 
 /usr/bin/php /home/project/a/c/c/acc/public_html/backup.php
--- sge/sge-ircbot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#$ -j y
-#$ -o $HOME/sge.ircbot.out
-#$ -r y
-#$ -m bae
-
-cd /home/project/a/c/c/acc/public_html/
-php accbot.php
-
