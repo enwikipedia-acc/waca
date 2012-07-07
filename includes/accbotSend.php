e809d0f1c3184a2e39329cb9588ac95f93a08f3a
Simon Walker <github@stwalkerster.co.uk>
2012-07-08 00:19:17 +0100

    add missing global

--- includes/accbotSend.php
+++ includes/accbotSend.php
@@ -20,7 +20,7 @@ if ($ACC != "1") {
 // accbot class
 class accbotSend {
 	public function send($message) {
-		global $whichami,$toolserver_username, $toolserver_password;
+		global $whichami,$toolserver_username, $toolserver_password, $ircBotNotificationType;
 		$message = html_entity_decode($message,ENT_COMPAT,'UTF-8'); // If a message going to the bot was for whatever reason sent through sanitze() earlier, reverse it. 
 		$message = stripslashes($message);
 		$blacklist = array("DCC", "CCTP", "PRIVMSG");
