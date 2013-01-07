179618c833717d3b93e4cc1380de84b8901914bb
Simon Walker <github@stwalkerster.co.uk>
2013-01-07 16:58:37 +0000

    rq left comment message

--- includes/request.php
+++ includes/request.php
@@ -249,7 +249,15 @@ class accRequest {
 						$what = "<Account Creator Needed!>";
 					}
 					$comments = html_entity_decode(stripslashes($row['pend_cmt']));
-						$accbot->send("\00314[[\00303acc:\00307$pid\00314]]\0034 N\00310 \00302$tsurl/acc.php?action=zoom&id=$pid\003 \0035*\003 \00303$user\003 \0035*\00310 $what\003");
+					
+					$ircmessage = "\00314[[\00303acc:\00307$pid\00314]]\0034 N\00310 \00302$tsurl/acc.php?action=zoom&id=$pid\003 \0035*\003 \00303$user\003 \0035*\00310 $what\003"
+					
+					if(mb_strlen($comments) > 0)
+					{
+						$ircmessage .= " <Requestor Left Comment>";
+					}
+					$accbot->send($ircmessage);
+					
 				} elseif( $row['pend_mailconfirm'] == "Confirmed" ) {
 					echo "Your e-mail address has already been confirmed!\n";
 				} else {
