c7ffe32666280cfc1d733be93889ba535fc38722
Simon Walker <github@stwalkerster.co.uk>
2013-01-08 03:34:09 +0000

    change query

--- includes/statistics/StatsIdUsers.php
+++ includes/statistics/StatsIdUsers.php
@@ -36,43 +36,15 @@ class StatsIdUsers extends StatisticsPage
 	
 	function getUserList()
 	{
-		global $tsSQL;
-		$out = "";
-		$result = $tsSQL->query("SELECT * FROM acc_user WHERE user_identified=1 ORDER BY user_level, user_name;");
-		if (!$result)
-		{
-			return "No users found.";
-		}
-		// Build list of users
-		$lastlevel = NULL;
-		while ($row = mysql_fetch_assoc($result)) // Return the result of the database query as an associative array; then , for each row returned...
-		{
-			if ($row['user_level'] != $lastlevel)
-			{
-				// If the user level has changed, we are on the next group of users (ie, if it was admin and is now user, then we must start a new ordered list for users
-				if ($lastlevel == NULL)
-				{
-					// Initial list
-					$out.= "\n<h3>" . $row['user_level'] . "</h3>\n<ul>\n"; // Build header, start unordered list
-				}
-				else
-				{
-					// Any additional lists
-					$out.= "</ul>\n<h3>" . $row['user_level'] . "</h3>\n<ul>\n"; // End previous unordered list, build header, start new unordered list
-				}
-			}
-		
-			$out.= "<li><a href=\"?page=Users&amp;user=" . $row['user_id'] . "\">"; // Start list item, link to user page
-			$uid = array ($row['user_name'], $row['user_onwikiname'], $row['user_id']); // Build an array of the user's name, onwiki name, and ID to compare with users in devlist
-			// Write the users name
-			$out.= $row['user_name'];
-			$out.= "</a></li>\n"; // End the list item
+		$query = "select user_name, user_level, user_checkuser from acc_user where user_identified = 1 order by user_name;";
+	
+		global $tsurl;
+		$qb = new QueryBrowser();
+		$qb->rowFetchMode = MYSQL_NUM;
+		$r = $qb->executeQueryToTable($query); 
+		echo mysql_error();
 
-			$lastlevel = $row['user_level']; // Set lastlevel to the level of this user so we can see if we need to start a new list
-		}
-		$out.= "</ul>\n<br />\n";
-		
-		return $out;
+		return $r;
 	}
 	
 	function requiresWikiDatabase()
