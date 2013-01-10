613fbc137387045029329827b20d7b93bc203109
Simon Walker <github@stwalkerster.co.uk>
2013-01-10 12:43:09 +0000

    fix

--- functions.php
+++ functions.php
@@ -895,7 +895,7 @@ function zoomPage($id,$urlhash)
 			$posc4 .= $oS . '" target="_blank">SUL</a> ';
 			
 			// Open the SUL of the conflicting users.
-			$posc4 = '(<a href="http://toolserver.org/~quentinv57/tools/sulinfo.php?showinactivity=1&showblocks=1&username=Stwa1kerster';
+			$posc4 .= '(<a href="http://toolserver.org/~quentinv57/tools/sulinfo.php?showinactivity=1&showblocks=1&username=Stwa1kerster';
 			$posc4 .= $oS . '" target="_blank">SUL-ib</a> | ';
 
 			$posc4 .= '<a href="http://toolserver.org/~hersfold/newfakeSULutil.php?username=';
