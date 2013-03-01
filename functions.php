enwikipedia-acc/waca

commit 798020bde9486318cfe05a5be220d42b6c3c0db6

Fix "Other requets from IP" bug on requests w/ XFF
Appears to have been introduced with the new XFF system.  This
particular bug affects any request with XFF headers that are trusted
further than one level down.

Another bug exists when all IPs in the XFF headers as well as the
origin are trusted proxies.  Not fixed in this commit.

functions.php
@@ -1074,7 +1074,11 @@ function zoomPage($id,$urlhash)
 
 	$out .= "<h2>Other requests from $ipmsg:</h2>\n";
 	if ($thisip != '127.0.0.1') {
-		$query = "SELECT * FROM acc_pend WHERE pend_ip = '$thisip' AND pend_id != '$thisid' AND pend_mailconfirm = 'Confirmed';";
+		if($proxies)
+			$whereClause = "pend_proxyip LIKE '%{$thisip}%'";
+		else
+			$whereClause = "pend_ip = '$thisip'";
+		$query = "SELECT * FROM acc_pend WHERE {$whereClause} AND pend_id != '$thisid' AND pend_mailconfirm = 'Confirmed';";
 		$result = mysql_query($query, $tsSQLlink);
 		if (!$result)
 		Die("Query failed: $query ERROR: " . mysql_error());

commit 1a1bc81baa5f74b845db4a7a94f55fcd7f486bc8

Fix bug: old-XFF rqs no show in "other requests"
Fixes bug where requests made when the old XFF processing system was in
use would not appear on requests made after the new XFF processing
system was made active.

This resolves issue #8, fixing a bug I introduced in
798020bde9486318cfe05a5be220d42b6c3c0db6.  Feel free to trout me at your
earliest convenience.

functions.php
@@ -1074,11 +1074,7 @@ function zoomPage($id,$urlhash)
 
 	$out .= "<h2>Other requests from $ipmsg:</h2>\n";
 	if ($thisip != '127.0.0.1') {
-		if($proxies)
-			$whereClause = "pend_proxyip LIKE '%{$thisip}%'";
-		else
-			$whereClause = "pend_ip = '$thisip'";
-		$query = "SELECT * FROM acc_pend WHERE {$whereClause} AND pend_id != '$thisid' AND pend_mailconfirm = 'Confirmed';";
+		$query = "SELECT * FROM acc_pend WHERE (pend_proxyip LIKE '%{$thisip}%' OR pend_ip = '$thisip') AND pend_id != '$thisid' AND pend_mailconfirm = 'Confirmed';";
 		$result = mysql_query($query, $tsSQLlink);
 		if (!$result)
 		Die("Query failed: $query ERROR: " . mysql_error());