f93d3c4bde00ae257269bf9f72e89a6ed206d191
Simon Walker <github@stwalkerster.co.uk>
2013-04-23 05:18:29 +0100

    enforce minimum password version
    ( issue #9 and #10 )

--- includes/authutils.php
+++ includes/authutils.php
@@ -20,12 +20,17 @@ class authutils {
      * Test the specified data against the specified credentials
      */
     public static function testCredentials( $password, $credentials ) {
+        global $minimumPasswordVersion;
+    
         if( substr( $credentials, 0, 1 ) == ":" ) {
             // new style, but what version?
             $data = explode( ':', substr( $credentials, 1 ) );
             
             // call the encryptVersion function for the version that this password actually is.
             // syntax: :VERSION:SALT:HASH
+            
+            // check the version is one of the allowed ones:
+            if( $minimumPasswordVersion > $data[ 0 ] ) return false;
             
             // re-encrypt the new password
             $newcrypt = call_user_func( array( "authutils", "encryptVersion" . $data[ 0 ] ), $password, $data[ 1 ] );
@@ -34,8 +39,16 @@ class authutils {
             return ( $newcrypt == $credentials );
             
         } else { // old style, eew.
-            // trigger_error("Old user password in use, please change your password.", E_USER_WARNING);
-            return self::encryptVersion0( $password, "" ) == $credentials;
+        
+            // not allowed this version of password
+            if( $minimumPasswordVersion > 0 ) return false;
+        
+            // various different ways of escaping this have been done in the past.
+            // we have to test all to make sure it's gonna work, reducing security.
+            return ( self::encryptVersion0( $password, "" ) == $credentials
+                || self::encryptVersion0( sanitize( $password ), "" ) == $credentials
+                || self::encryptVersion0( mysql_escape_string( $password ), "" ) == $credentials
+                );
         }
     }
     
