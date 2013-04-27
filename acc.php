163e896344533bd90f16bdc00498eae9e50b57e6
Steven Donovan <stevend811@comcast.net>
2013-04-27 15:16:58 -0400

    Add ability for users to edit their own Email addresses, fixes #24.

--- acc.php
+++ acc.php
@@ -1177,7 +1177,6 @@ elseif ($action == "defer" && $_GET['id'] != "" && $_GET['sum'] != "") {
 elseif ($action == "welcomeperf" || $action == "prefs") { //Welcomeperf is deprecated, but to avoid conflicts, include it still.
 	if (isset ($_POST['sig'])) {
 		$sig = sanitize($_POST['sig']);
-		$template = sanitize($_POST['template']);
 		$sid = sanitize($_SESSION['user']);
 		if( isset( $_POST['secureenable'] ) ) {
 			$secureon = 1;
@@ -1189,7 +1188,21 @@ elseif ($action == "welcomeperf" || $action == "prefs") { //Welcomeperf is depre
 		} else {
 			$abortprefOld= 0;
 		}
-		$query = "UPDATE acc_user SET user_welcome_sig = '$sig', user_secure = '$secureon', user_abortpref = '$abortprefOld' WHERE user_name = '$sid'";
+		if( isset( $_POST['email'] ) ) {
+			$mailisvalid = preg_match('/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.(ac|ad|ae|aero|af|ag|ai|al|am|an|ao|aq|ar|arpa|as|asia|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|biz|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cat|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|com|coop|cr|cu|cv|cx|cy|cz|de|dj|dk|dm|do|dz|ec|edu|ee|eg|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gov|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|info|int|io|iq|ir|is|it|je|jm|jo|jobs|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mil|mk|ml|mm|mn|mo|mobi|mp|mq|mr|ms|mt|mu|museum|mv|mw|mx|my|mz|na|name|nc|ne|net|nf|ng|ni|nl|no|np|nr|nu|nz|om|org|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|pro|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|st|su|sv|sy|sz|tc|td|tel|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|travel|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)$/i', $_POST['email']);
+			if ($mailisvalid == 0) {
+				$skin->displayRequestMsg("ERROR: Invalid E-mail address.<br />");
+			}
+			else {
+				$newemail = sanitize($_POST['email']);
+			}
+		}
+		if (isset($newemail)) {
+		$query = "UPDATE acc_user SET user_welcome_sig = '$sig', user_secure = '$secureon', user_abortpref = '$abortprefOld', user_email = '$newemail' WHERE user_name = '$sid'";
+		}
+		else {
+			$query = "UPDATE acc_user SET user_welcome_sig = '$sig', user_secure = '$secureon', user_abortpref = '$abortprefOld' WHERE user_name = '$sid'";
+		}
 		$result = mysql_query($query, $tsSQLlink);
 		if (!$result)
 			sqlerror("Query failed: $query ERROR: " . mysql_error());
@@ -1211,6 +1224,7 @@ elseif ($action == "welcomeperf" || $action == "prefs") { //Welcomeperf is depre
 			$abortpref= "";
 		}
 	}
+	$useremail = " value=\"" . $row['user_email'] . "\"";
 	echo '<table>';
     echo '<tr><th>Table of Contents</th></tr>';
     echo '<tr><td><a href="#1">General settings</a></td></tr>';
@@ -1222,7 +1236,8 @@ elseif ($action == "welcomeperf" || $action == "prefs") { //Welcomeperf is depre
     echo 'Your signature (wikicode).<input type="text" name="sig" size ="40"'. $sig.'/>';
     echo '<i>This would be the same as ~~~ on-wiki. No date, please.</i><br /><br />';
 	//Preference used in functions.php:
-    echo '<input type="checkbox" name="abortpref"'.$abortpref.'/> Don\'t ask to double check before closing requests (requires Javascript)<br /><br />';	    
+    echo '<input type="checkbox" name="abortpref"'.$abortpref.'/> Don\'t ask to double check before closing requests (requires Javascript)<br /><br />';
+    echo 'Your Email address.<input type="text" name="email" size ="40"'. $useremail .'/>';
     // TODO: clean up into nicer code, rather than coming out of php
 	echo <<<HTML
     <input type="submit"/><input type="reset"/>
