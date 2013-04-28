3cd4841fea9530008cd516f65d4b0e6175608203
Manishearth <manishsmail@gmail.com>
2013-04-28 18:43:04 +0530

    Showing up in the wrong places

--- includes/request.php
+++ includes/request.php
@@ -712,7 +712,7 @@ class accRequest {
 		// Checks whether the username is already in use on Wikipedia.
 		$userexist = file_get_contents("http://en.wikipedia.org/w/api.php?action=query&list=users&ususers=" . urlencode($_POST['name']) . "&format=php");
 		$ue = unserialize($userexist);
-		if (!isset ($ue['query']['users']['0']['missing'])) {
+		if (!isset ($ue['query']['users']['0']['missing'])&&isset ($ue['query']['users']['0']['userid'])) {
 			$message = $messages->getMessage(10);
 			$skin->displayRequestMsg("<!-- m:10 -->$message<br />\n");
 			$fail = 1;
