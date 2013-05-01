4e8530f941191526e34f7411cdaba8dd1ff98bbf
Simon Walker <github@stwalkerster.co.uk>
2013-05-01 03:39:20 +0100

    password conversion stats page (cherry picked from commit 3320925324e2def81aa5fc354b2c110a0601fea7)

---
+++ includes/statistics/StatsPasswordConversion.php
@@ -0,0 +1,59 @@
+<?php
+/**************************************************************************
+ **********      English Wikipedia Account Request Interface      **********
+ ***************************************************************************
+ ** Wikipedia Account Request Graphic Design by Charles Melbye,           **
+ ** which is licensed under a Creative Commons                            **
+ ** Attribution-Noncommercial-Share Alike 3.0 United States License.      **
+ **                                                                       **
+ ** All other code are released under the Public Domain                   **
+ ** by the ACC Development Team.                                          **
+ **                                                                       **
+ ** See CREDITS for the list of developers.                               **
+ ***************************************************************************/
+
+class StatsPasswordConversion extends StatisticsPage
+{
+	function execute()
+	{
+		$query = <<<sql
+SELECT "0" AS "Version", "Active" AS "Type", COUNT(*) AS "Count" FROM acc_user WHERE user_pass NOT LIKE ":%" AND (user_level = "User" OR user_level = "Admin")
+UNION
+SELECT "0", "Inactive", COUNT(*) FROM acc_user WHERE user_pass NOT LIKE ":%" AND NOT (user_level = "User" OR user_level = "Admin")
+UNION
+SELECT SUBSTRING(user_pass FROM 2 FOR 1), "Active", COUNT(*) FROM acc_user WHERE user_pass LIKE ":%" AND (user_level = "User" OR user_level = "Admin") GROUP BY Version
+UNION
+SELECT SUBSTRING(user_pass FROM 2 FOR 1), "Inactive", COUNT(*) FROM acc_user WHERE user_pass LIKE ":%" AND NOT (user_level = "User" OR user_level = "Admin") GROUP BY Version
+ORDER BY Version ASC, Type ASC
+;
+sql;
+        
+		global $tsurl;
+		$qb = new QueryBrowser();
+		$qb->rowFetchMode = MYSQL_NUM;
+		$r = $qb->executeQueryToTable($query); 
+		echo mysql_error();
+
+		return $r;
+	}
+	
+	function getPageTitle()
+	{
+		return "Password conversion status";
+	}
+	
+	function getPageName()
+	{
+		return "PasswordConversion";
+	}
+	
+	function isProtected()
+	{
+		return true;
+	}
+	
+	function requiresWikiDatabase()
+	{
+		return false;
+	}
+}
