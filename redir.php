e8c2730fb5b3b9767f8a2b32b0cb63c5ae5a7bcc
FunPika <stevend811@comcast.net>
2013-06-01 15:10:05 -0300

    Replace the edit counter link with new one leading to Tool Labs.
    At the moment though queries on that tool are extremely slow, only changing this link because the old link is 404ing.

--- redir.php
+++ redir.php
@@ -1,7 +1,7 @@
 <?php
 
 $toolList = array(
-	'tparis-pcount' => 'http://toolserver.org/~tparis/pcount/index.php?lang=en&wiki=wikipedia&name=%DATA%',
+	'tparis-pcount' => 'http://tools.wmflabs.org/xtools/pcount/index.php?lang=en&wiki=wikipedia&name=%DATA%',
 	'luxo-contributions' => '//toolserver.org/~luxo/contributions/contributions.php?lang=en&blocks=true&user=%DATA%',
 	'oq-whois' => '//toolserver.org/~overlordq/cgi-bin/whois.cgi?lookup=%DATA%',
 	'ipinfodb-locator' => 'http://www.ipinfodb.com/ip_locator.php?ip=%DATA%',
