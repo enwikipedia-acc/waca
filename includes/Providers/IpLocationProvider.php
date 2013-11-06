4b7c978a24d4ca212b07b21b088a82d15ec5e1dc
Simon Walker <github@stwalkerster.co.uk>
2013-11-06 14:46:30 +0000

    Location provider (issue #73)

---
+++ includes/Providers/IpLocationProvider.php
@@ -0,0 +1,76 @@
+<?php
+
+if ($ACC != "1") {
+	header("Location: $tsurl/");
+	die();
+} //Re-route, if you're a web client.
+
+interface IIpLocationProvider
+{
+    public function getIpLocation($address);   
+}
+
+class IpLocationProvider implements IIpLocationProvider
+{
+    private $apikey;
+    private $database;
+    
+    public function __construct(PdoDatabase $database, string $apikey)
+    {
+        $this->database = $database;
+        $this->apikey = $apikey;
+    }
+    
+    public function getIpLocation($address)
+    {
+        // lets look in our database first.
+        $location = GeoLocation::getByAddress($address);
+        
+        if($location != null)
+        {
+            // touch cache timer
+            $location->save();
+            
+            return $location->getData();   
+        }
+        
+        // OK, it's not there, let's do an IP2Location lookup.
+        $result = $this->getResult($address);
+        
+        if($result != null)
+        {
+            $location = new GeoLocation();
+            $location->setDatabase($this->database);
+            $location->setAddress($address);
+            $location->setData($result);
+            $location->save();
+            
+            return $result;
+        }
+    }
+    
+    // adapted from http://www.ipinfodb.com/ip_location_api.php
+	private function getResult($ip)
+    {
+		if(filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6))
+        {
+			$xml = @file_get_contents('http://api.ipinfodb.com/v3/ip-city/?key=' . $this->apikey . '&ip=' . $ip . '&format=xml');
+
+			if(get_magic_quotes_runtime())
+            {
+				$xml = stripslashes($xml);
+			}
+
+			$response = @new SimpleXMLElement($xml);
+
+			foreach($response as $field=>$value)
+            {
+				$result[(string)$field] = (string)$value;
+			}
+
+			return $result;
+		}
+        
+		return null;
+	}
+}
