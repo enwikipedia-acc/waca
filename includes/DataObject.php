e29710096dead2cdbe8bb170d34794547e519447
Simon Walker <github@stwalkerster.co.uk>
2013-11-06 02:31:55 +0000

    merge more dbobject changes across from stwalkerster/siteframework

--- includes/DataObject.php
+++ includes/DataObject.php
@@ -9,17 +9,43 @@ if ($ACC != "1") {
  * DataObject is the base class for all the database access classes. Each "DataObject" holds one record from the database, and
  * provides functions to allow loading from and saving to the database.
  *
+ * Note: This requires the database tables to be named the same as the classes, and the database tables must have an "id" column.
+ * Simple views can be used as a way of aliasing to allow for a transition period.
+ * 
  * @author Simon Walker
  */
 abstract class DataObject
 {
     protected $isNew = true;
     
+    protected $dbObject;
+    
+    public function setDatabase($db)
+    {
+        $this->dbObject = $db;
+    }
+    
 	/**
      * Retrieves a data object by it's row ID.
      * @param $id
      */
-	public abstract static function getById($id);
+	public static function getById($id, $database) {
+		$statement = $database->prepare("SELECT * FROM `" . strtolower( get_called_class() ) . "` WHERE id = :id LIMIT 1;");
+		$statement->bindParam(":id", $id);
+
+		$statement->execute();
+
+		$resultObject = $statement->fetchObject( get_called_class() );
+
+		if($resultObject != false)
+		{
+			$resultObject->isNew = false;
+		}
+
+        $resultObject->setDatabase($database); 
+       
+		return $resultObject;
+	}
 
 	/**
      * Saves a data object to the database, either updating or inserting a record.
@@ -29,10 +55,21 @@ abstract class DataObject
 	/**
 	 * Retrieves the ID attribute
 	 */
-	public abstract function getId();
+	public function getId()
+	{
+		return $this->id;
+	}
 
 	/**
 	 * Deletes the object from the database
 	 */
-	public abstract function delete();
+	public function delete() 
+    {
+		$statement = $this->dbObject->prepare("DELETE FROM `" . strtolower( get_called_class() ) . "` WHERE id = :id LIMIT 1;");
+		$statement->bindParam(":id", $this->id);
+		$statement->execute();
+
+		$this->id=0;
+		$this->isNew = true;
+    }
 }
