f25a5de3bb0d6cc20dae4720968a401d2cbbff06
Simon Walker <github@stwalkerster.co.uk>
2013-11-06 02:32:12 +0000

    GeoLocation database objects (issue #73)

---
+++ includes/DataObjects/GeoLocation.php
@@ -0,0 +1,69 @@
+<?php
+
+if ($ACC != "1") {
+	header("Location: $tsurl/");
+	die();
+} //Re-route, if you're a web client.
+
+class GeoLocation extends DataObject
+{
+    private $address;
+    private $data;
+    private $creation;
+    
+    public function save()
+    {
+		if($this->isNew)
+		{ // insert
+			$statement = $this->dbObject->prepare("INSERT INTO `geolocation` (address, data) VALUES (:address, :data);");
+			$statement->bindParam(":address", $this->address);
+			$statement->bindParam(":data", $this->data);
+			if($statement->execute())
+			{
+				$this->isNew = false;
+				$this->id = $this->dbObject->lastInsertId();
+			}
+			else
+			{
+				throw new Exception($statement->errorInfo());
+			}
+		}
+		else
+		{ // update
+			$statement = $this->dbObject->prepare("UPDATE `geolocation` SET address = :address, data = :data WHERE id = :id LIMIT 1;");
+			$statement->bindParam(":address", $this->address);
+			$statement->bindParam(":id", $this->id);
+			$statement->bindParam(":data", $this->data);
+            
+			if(!$statement->execute())
+			{
+				throw new Exception($statement->errorInfo());
+			}
+		} 
+    }
+    
+    public function getAddress()
+    {
+        return $this->address;   
+    }
+    
+    public function setAddress($address)
+    {
+        $this->address = $address;
+    }
+    
+    public function getData()
+    {
+        return unserialize($this->data);
+    }
+    
+    public function setData($data)
+    {
+        $this->data = serialize($data);
+    }
+    
+    public function getCreation()
+    {
+        return $this->creation;   
+    }
+}
