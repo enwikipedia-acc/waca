9bb40c7fa53651b95808c4d7ee043d4ae2a03eb2
Simon Walker <github@stwalkerster.co.uk>
2013-12-20 23:53:21 +0000

    view for requests

---
+++ sql/pdo-views/patch-request.sql
@@ -0,0 +1,15 @@
+﻿CREATE OR REPLACE VIEW request AS
+SELECT `pend_id` as id,
+    `pend_email` as email,
+    `pend_ip` as ip,
+    `pend_name` as name,
+    `pend_cmt` as comment,
+    `pend_status` as status,
+    `pend_date` as date,
+    `pend_checksum` as checksum,
+    `pend_emailsent` as emailsent,
+    `pend_mailconfirm` as emailconfirm,
+    `pend_reserved` as reserved,
+    `pend_useragent` as useragent,
+    `pend_proxyip` as forwardedip
+FROM `acc_pend`;
