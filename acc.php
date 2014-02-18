5a613065ed0bf889bc21262c2967e616381da97d
Simon Walker <github@stwalkerster.co.uk>
2014-02-18 18:02:39 +0000

    comment-quick to PDO

--- acc.php
+++ acc.php
@@ -1736,29 +1736,45 @@ elseif ($action == "comment-add")
     die();
 }
 
-elseif ($action == "comment-quick") {
-    if ((isset($_POST['id'])) && (isset($_POST['visibility'])) && ($_POST['id'] != "")) {
-        $id = $internalInterface->checkreqid($_POST['id']);
-        $user = sanitise($_SESSION['user']);
-        $comment = mysql_real_escape_string($_POST['comment']);
-        $visibility = sanitise($_POST['visibility']);
-        $now = date("Y-m-d H-i-s");
-	if($_POST['comment'] == ""){
-		header("Location: acc.php?action=zoom&id=".$id);
-		die();
-	}
-        $query = "INSERT INTO acc_cmt (cmt_time, cmt_user, cmt_comment, cmt_visability, pend_id) VALUES ('$now', '$user', '$comment', '$visibility', '$id');";
-        $result = mysql_query($query, $tsSQLlink);
-        if (!$result) {
-            sqlerror("Query failed: $query ERROR: " . mysql_error());
-        }
-        $botcomment = $user . " posted a comment on request " . $id;
-        $accbotSend->send($botcomment);
-
-	header("Location: acc.php?action=zoom&id=".$id);
-
-	die();
+elseif ($action == "comment-quick") 
+{
+    $request = Request::getById($_POST['id']);
+    if($request == false)
+    {
+        BootstrapSkin::displayAlertBox("Could not find request!", "alert-error", "Error", true, false);
+        BootstrapSkin::displayInternalFooter();
+        die();
     }
+    
+    if(!isset($_POST['comment']) || $_POST['comment'] == "")
+    {
+		header("Location: acc.php?action=zoom&id=" . $request->getId());
+        die(); 
+    }
+    
+    $visibility = 'user';
+    if( isset($_POST['visibility']) )
+    {
+        // sanity check
+        $visibility = $_POST['visibility'] == 'user' ? 'user' : 'admin';
+    }
+    
+    $comment = new Comment();
+    $comment->setDatabase(gGetDb());
+    
+    $comment->setRequest($request->getId());
+    $comment->setVisibility($visibility);
+    $comment->setUser(User::getCurrent()->getId());
+    $comment->setComment($_POST['comment']);
+    
+    $comment->save();
+    
+    $botcomment_pvt =  ($visibility == "admin") ? "private " : "";
+    $botcomment = User::getCurrent()->getUsername() . " posted a " . $botcomment_pvt . "comment on request " . $request->getId();
+
+    $accbotSend->send($botcomment);
+    
+    header("Location: acc.php?action=zoom&id=" . $request->getId());
 }
 
 elseif ($action == "changepassword") {
