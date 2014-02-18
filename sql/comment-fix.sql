bfdfd69ae8a4bae56cda5fbb7edadc3080c7cde0
Simon Walker <github@stwalkerster.co.uk>
2014-02-18 17:37:48 +0000

    change patch to use table instead of view

---
+++ sql/comment-fix.sql
@@ -0,0 +1,18 @@
+﻿DROP TABLE IF EXISTS comment;
+DROP VIEW IF EXISTS comment;
+
+CREATE TABLE comment AS
+SELECT cmt_id as id,
+    cmt_time as time,
+    id as user,
+    cmt_comment as comment,
+    cmt_visability as visibility,
+    pend_id as request
+FROM acc_cmt
+INNER JOIN user on username = cmt_user;
+
+ALTER TABLE comment 
+CHANGE COLUMN id id INT(11) NOT NULL AUTO_INCREMENT ,
+ADD PRIMARY KEY (id);
+
+ALTER TABLE acc_cmt COMMENT = '[DEPRECATED] Request comments' ;
--- sql/pdo-views/patch-comment.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE VIEW comment AS
-SELECT cmt_id as id,
-    cmt_time as time,
-    id as user,
-    cmt_comment as comment,
-    cmt_visability as visibility,
-    pend_id as request
-FROM acc_cmt
-INNER JOIN user on username = cmt_user;
