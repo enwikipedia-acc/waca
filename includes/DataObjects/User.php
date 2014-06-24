99d24c86c1fd59b23beac86620bd7eaccf16b326
Simon Walker <github@stwalkerster.co.uk>
2014-06-24 04:09:26 +0100

    code style

--- includes/DataObjects/User.php
+++ includes/DataObjects/User.php
@@ -68,38 +68,38 @@ class User extends DataObject
     
     public static function getByUsername($username, PdoDatabase $database)
     {
-        $statement = $database->prepare("SELECT * FROM `" . strtolower( get_called_class() ) . "` WHERE username = :id LIMIT 1;");
-		$statement->bindValue(":id", $username);
+        $statement = $database->prepare("SELECT * FROM user WHERE username = :id LIMIT 1;");
+        $statement->bindValue(":id", $username);
 
-		$statement->execute();
+        $statement->execute();
 
-		$resultObject = $statement->fetchObject( get_called_class() );
+        $resultObject = $statement->fetchObject( get_called_class() );
 
-		if($resultObject != false)
-		{
-			$resultObject->isNew = false;
+        if($resultObject != false)
+        {
+            $resultObject->isNew = false;
             $resultObject->setDatabase($database); 
-		}
+        }
 
-		return $resultObject;
+        return $resultObject;
     }
     
     public static function getByRequestToken($requestToken, PdoDatabase $database)
     {
-		$statement = $database->prepare("SELECT * FROM `" . strtolower( get_called_class() ) . "` WHERE oauthrequesttoken = :id LIMIT 1;");
-		$statement->bindValue(":id", $requestToken);
+        $statement = $database->prepare("SELECT * FROM user WHERE oauthrequesttoken = :id LIMIT 1;");
+        $statement->bindValue(":id", $requestToken);
 
-		$statement->execute();
+        $statement->execute();
 
-		$resultObject = $statement->fetchObject( get_called_class() );
+        $resultObject = $statement->fetchObject( get_called_class() );
 
-		if($resultObject != false)
-		{
-			$resultObject->isNew = false;
+        if($resultObject != false)
+        {
+            $resultObject->isNew = false;
             $resultObject->setDatabase($database); 
-		}
+        }
 
-		return $resultObject;
+        return $resultObject;
     }
     
     /**
@@ -107,7 +107,7 @@ class User extends DataObject
      */
     public static function getAllWithStatus($status, PdoDatabase $database)
     {
-        $statement = $database->prepare("SELECT * FROM `" . strtolower( get_called_class() ) . "` WHERE status = :status");
+        $statement = $database->prepare("SELECT * FROM user WHERE status = :status");
         $statement->execute( array( ":status" => $status ) );
         
         $resultObject = $statement->fetchAll( PDO::FETCH_CLASS, get_called_class() );
@@ -123,7 +123,7 @@ class User extends DataObject
     
     public static function getAllCheckusers(PdoDatabase $database)
     {
-        $statement = $database->prepare("SELECT * FROM `" . strtolower( get_called_class() ) . "` WHERE checkuser = 1;");
+        $statement = $database->prepare("SELECT * FROM user WHERE checkuser = 1;");
         $statement->execute();
         
         $resultObject = $statement->fetchAll( PDO::FETCH_CLASS, get_called_class() );
@@ -140,9 +140,18 @@ class User extends DataObject
     public static function getAllInactive(PdoDatabase $database)
     {   
         $date = new DateTime();
-		$date->modify("-45 days");
+        $date->modify("-45 days");
         
-        $statement = $database->prepare("SELECT * FROM `" . strtolower( get_called_class() ) . "` WHERE lastactive < :lastactivelimit and status != 'Suspended' and status != 'Declined' and status != 'New' ORDER BY lastactive     ASC;");
+        $statement = $database->prepare(<<<SQL
+            SELECT * 
+            FROM user 
+            WHERE lastactive < :lastactivelimit 
+                AND status != 'Suspended' 
+                AND status != 'Declined' 
+                AND status != 'New' 
+            ORDER BY lastactive ASC;
+SQL
+        );
         $statement->execute( array( ":lastactivelimit" => $date->format("Y-m-d H:i:s") ) );
         
         $resultObject = $statement->fetchAll( PDO::FETCH_CLASS, get_called_class() );
@@ -160,16 +169,21 @@ class User extends DataObject
     {
 		if($this->isNew)
 		{ // insert
-			$statement = $this->dbObject->prepare(
-                "INSERT INTO `user` (" . 
-                "username, email, password, status, onwikiname, welcome_sig, lastactive, forcelogout," . 
-                "checkuser, identified, welcome_template, abortpref, confirmationdiff, emailsig," . 
-                "oauthrequesttoken, oauthrequestsecret, oauthaccesstoken, oauthaccesssecret" .
-                ") VALUES (" . 
-                ":username, :email, :password, :status, :onwikiname, :welcome_sig, :lastactive, :forcelogout," . 
-                ":checkuser, :identified, :welcome_template, :abortpref, :confirmationdiff, :emailsig," . 
-                ":ort, :ors, :oat, :oas" .
-                ");");
+			$statement = $this->dbObject->prepare(<<<SQL
+                INSERT INTO `user` ( 
+                    username, email, password, status, onwikiname, welcome_sig, 
+                    lastactive, forcelogout, checkuser, identified, 
+                    welcome_template, abortpref, confirmationdiff, emailsig, 
+                    oauthrequesttoken, oauthrequestsecret, 
+                    oauthaccesstoken, oauthaccesssecret
+                ) VALUES (
+                    :username, :email, :password, :status, :onwikiname, :welcome_sig,
+                    :lastactive, :forcelogout, :checkuser, :identified, 
+                    :welcome_template, :abortpref, :confirmationdiff, :emailsig, 
+                    :ort, :ors, :oat, :oas
+                );
+SQL
+            );
 			$statement->bindValue(":username", $this->username);
 			$statement->bindValue(":email", $this->email);
 			$statement->bindValue(":password", $this->password);
@@ -192,7 +206,7 @@ class User extends DataObject
 			if($statement->execute())
 			{
 				$this->isNew = false;
-				$this->id = $this->dbObject->lastInsertId();
+				$this->id = (int)$this->dbObject->lastInsertId();
 			}
 			else
 			{
@@ -201,14 +215,21 @@ class User extends DataObject
 		}
 		else
 		{ // update
-			$statement = $this->dbObject->prepare("UPDATE `user` SET " . 
-                "username = :username, email = :email, password = :password, status = :status, " .
-                "onwikiname = :onwikiname, welcome_sig = :welcome_sig, lastactive = :lastactive, " .
-                "forcelogout = :forcelogout, checkuser = :checkuser, identified = :identified, " .
-                "welcome_template = :welcome_template, abortpref = :abortpref, confirmationdiff = :confirmationdiff, " .
-                "emailsig = :emailsig, " .
-                "oauthrequesttoken = :ort, oauthrequestsecret = :ors, oauthaccesstoken = :oat, oauthaccesssecret = :oas " .
-                "WHERE id = :id LIMIT 1;");
+			$statement = $this->dbObject->prepare(<<<SQL
+                UPDATE `user` SET 
+                    username = :username, email = :email, 
+                    password = :password, status = :status,
+                    onwikiname = :onwikiname, welcome_sig = :welcome_sig, 
+                    lastactive = :lastactive, forcelogout = :forcelogout, 
+                    checkuser = :checkuser, identified = :identified,
+                    welcome_template = :welcome_template, abortpref = :abortpref, 
+                    confirmationdiff = :confirmationdiff, emailsig = :emailsig, 
+                    oauthrequesttoken = :ort, oauthrequestsecret = :ors, 
+                    oauthaccesstoken = :oat, oauthaccesssecret = :oas 
+                WHERE id = :id 
+                LIMIT 1;
+SQL
+            );
 			$statement->bindValue(":id", $this->id);
 			$statement->bindValue(":username", $this->username);
 			$statement->bindValue(":email", $this->email);
@@ -255,36 +276,40 @@ class User extends DataObject
     
     public function touchLastLogin()
     {
-        $this
-            ->dbObject
-            ->prepare("UPDATE user SET lastactive = CURRENT_TIMESTAMP() WHERE id = :id;")
-            ->execute(array(":id" => $this->id));
+        $query = "UPDATE user SET lastactive = CURRENT_TIMESTAMP() WHERE id = :id;";
+        $this->dbObject->prepare($query)->execute(array(":id" => $this->id));
     }
     
     #region properties
     
-    public function getUsername(){
+    public function getUsername()
+    {
         return $this->username;
     }
 
-    public function setUsername($username){
+    public function setUsername($username)
+    {
         $this->username = $username;
         $this->forcelogout = 1;
     }
 
-    public function getEmail(){
+    public function getEmail()
+    {
         return $this->email;
     }
 
-    public function setEmail($email){
+    public function setEmail($email)
+    {
         $this->email = $email;
     }
 
-    public function setPassword($password){
+    public function setPassword($password)
+    {
         $this->password = AuthUtility::encryptPassword($password);
     }
 
-    public function getStatus(){
+    public function getStatus()
+    {
         return $this->status;
     }
 
@@ -313,128 +338,148 @@ class User extends DataObject
         return $this->onwikiname;
     }
 
-    public function setOnWikiName($onwikiname){
+    public function setOnWikiName($onwikiname)
+    {
         $this->onwikiname = $onwikiname;
     }
 
-    public function getWelcomeSig(){
+    public function getWelcomeSig()
+    {
         return $this->welcome_sig;
     }
 
-    public function setWelcomeSig($welcome_sig){
-        $this->welcome_sig = $welcome_sig;
+    public function setWelcomeSig($welcomesig)
+    {
+        $this->welcome_sig = $welcomesig;
     }
 
-    public function getLastActive(){
+    public function getLastActive()
+    {
         return $this->lastactive;
     }
 
-    public function setLastActive($lastactive){
+    public function setLastActive($lastactive)
+    {
         $this->lastactive = $lastactive;
     }
 
-    public function getForcelogout(){
+    public function getForcelogout()
+    {
         return $this->forcelogout;
     }
 
-    public function setForcelogout($forcelogout){
+    public function setForcelogout($forcelogout)
+    {
         $this->forcelogout = $forcelogout ? 1 : 0;
     }
     
-    public function getSecure(){
+    public function getSecure()
+    {
         return true;
     }
 
-    public function getCheckuser(){
+    public function getCheckuser()
+    {
         return $this->checkuser;
     }
 
-    public function setCheckuser($checkuser){
+    public function setCheckuser($checkuser)
+    {
         $this->checkuser = $checkuser;
     }
 
-    public function getIdentified(){
+    public function getIdentified()
+    {
         return $this->identified;
     }
 
-    public function setIdentified($identified){
+    public function setIdentified($identified)
+    {
         $this->identified = $identified;
     }
 
-    public function getWelcomeTemplate(){
+    public function getWelcomeTemplate()
+    {
         return $this->welcome_template;
     }
 
-    public function setWelcomeTemplate($welcome_template){
-        $this->welcome_template = $welcome_template;
+    public function setWelcomeTemplate($welcometemplate)
+    {
+        $this->welcome_template = $welcometemplate;
     }
 
-    public function getAbortPref(){
+    public function getAbortPref()
+    {
         return $this->abortpref;
     }
 
-    public function setAbortPref($abortpref){
+    public function setAbortPref($abortpref)
+    {
         $this->abortpref = $abortpref;
     }
 
-    public function getConfirmationDiff(){
+    public function getConfirmationDiff()
+    {
         return $this->confirmationdiff;
     }
 
-    public function setConfirmationDiff($confirmationdiff){
+    public function setConfirmationDiff($confirmationdiff)
+    {
         $this->confirmationdiff = $confirmationdiff;
     }
 
     /**
      * @return string
      */
-    public function getEmailSig(){
+    public function getEmailSig()
+    {
         return $this->emailsig;
     }
 
-    public function setEmailSig($emailsig){
+    public function setEmailSig($emailsig)
+    {
         $this->emailsig = $emailsig;
     }
     
     public function getOAuthRequestToken()
-	{
-		return $this->oauthrequesttoken;
-	}
+    {
+        return $this->oauthrequesttoken;
+    }
 
 	public function setOAuthRequestToken($oauthrequesttoken)
-	{
-		$this->oauthrequesttoken = $oauthrequesttoken;
-	}
-
-	public function getOAuthRequestSecret()
-	{
-		return $this->oauthrequestsecret;
-	}
-
-	public function setOAuthRequestSecret($oauthrequestsecret)
-	{
-		$this->oauthrequestsecret = $oauthrequestsecret;
-	}
-
-	public function getOAuthAccessToken()
     {
-		return $this->oauthaccesstoken;
-	}
+        $this->oauthrequesttoken = $oauthrequesttoken;
+    }
 
-	public function setOAuthAccessToken($oauthaccesstoken)
-	{
-		$this->oauthaccesstoken = $oauthaccesstoken;
-	}
+    public function getOAuthRequestSecret()
+    {
+        return $this->oauthrequestsecret;
+    }
 
-	public function getOAuthAccessSecret()
-	{
-		return $this->oauthaccesssecret;
-	}
+    public function setOAuthRequestSecret($oauthrequestsecret)
+    {
+        $this->oauthrequestsecret = $oauthrequestsecret;
+    }
 
-	public function setOAuthAccessSecret($oauthaccesssecret)
-	{
-		$this->oauthaccesssecret = $oauthaccesssecret;
-	}
+    public function getOAuthAccessToken()
+    {
+        return $this->oauthaccesstoken;
+    }
+
+    public function setOAuthAccessToken($oauthaccesstoken)
+    {
+        $this->oauthaccesstoken = $oauthaccesstoken;
+    }
+
+    public function getOAuthAccessSecret()
+    {
+        return $this->oauthaccesssecret;
+    }
+
+    public function setOAuthAccessSecret($oauthaccesssecret)
+    {
+        $this->oauthaccesssecret = $oauthaccesssecret;
+    }
 
     #endregion
     
@@ -566,14 +611,14 @@ class User extends DataObject
         {
             $this->identityCache = null;
             $this->oauthidentitycache = null;
-            $this->dbObject->
-                prepare( "UPDATE user SET oauthidentitycache = null WHERE id = :id;" )->
-                execute( array( ":id" => $this->id ) );
+            $clearCacheQuery = "UPDATE user SET oauthidentitycache = null WHERE id = :id;";
+            $this->dbObject->prepare($clearCacheQuery)->execute( array( ":id" => $this->id ) );
             
             return null;   
         }
         
-        global $oauthConsumerToken, $oauthSecretToken, $oauthBaseUrl, $oauthBaseUrlInternal, $oauthMediaWikiCanonicalServer;
+        global $oauthConsumerToken, $oauthSecretToken, $oauthBaseUrl;
+        global $oauthBaseUrlInternal, $oauthMediaWikiCanonicalServer;
 
         if($this->oauthidentitycache == null)
         {
@@ -637,7 +682,15 @@ class User extends DataObject
 
     public function getApprovalDate()
     {
-        $query = $this->dbObject->prepare("SELECT log_time FROM acc_log WHERE log_pend = :userid AND log_action = 'Approved' ORDER BY log_id DESC LIMIT 1;");
+        $query = $this->dbObject->prepare(<<<SQL
+            SELECT log_time 
+            FROM acc_log 
+            WHERE log_pend = :userid 
+                AND log_action = 'Approved' 
+            ORDER BY log_id DESC 
+            LIMIT 1;
+SQL
+        );
         $query->execute( array( ":userid" => $this->id ) );
         
         $data = DateTime::createFromFormat( "Y-m-d H:i:s", $query->fetchColumn() );
