6ec8807db5a03cced09d1a9cf24f1b818f339496
Simon Walker <github@stwalkerster.co.uk>
2014-11-21 19:55:08 +0000

    Adding reload test to db test script

--- sql/test_db.sh
+++ sql/test_db.sh
@@ -26,5 +26,22 @@ for f in `ls patches/patch*.sql`; do
 	mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_SCHEMA < $f
 done
 
+echo "Dumping schema to file..."
+mysqldump -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_SCHEMA > schema.sql
 
+echo "Dropping database from server..."
+mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD -e "DROP DATABASE IF EXISTS $MYSQL_SCHEMA;"
 
+echo "Creating database..."
+mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD -e "CREATE DATABASE $MYSQL_SCHEMA;"
+
+echo "Reloading database from file..."
+mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_SCHEMA < schema.sql
+
+echo "Dumping schema to file..."
+mysqldump -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_SCHEMA > schema2.sql
+
+echo "Comparing dumps..."
+diff -q schema.sql schema2.sql
+
+echo "Done."
