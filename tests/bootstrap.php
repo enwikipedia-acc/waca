9688a3f8f9fa2b0e6cbc6d67762177bced4b8413
Simon Walker <github@stwalkerster.co.uk>
2014-12-21 05:58:37 +0000

    create local config file for unit tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,10 @@
 <?php
 //This file is used by phpunit
 
+if(!file_exists(__DIR__ . '/../config.local.inc.php'))
+{
+    touch(__DIR__ . '/../config.local.inc.php'); 
+}
+
 // Load the config file for the autoloader.
 require_once __DIR__ . '/../config.inc.php';
