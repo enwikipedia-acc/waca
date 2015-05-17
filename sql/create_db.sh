8566cc79d8567570d0ed3bb07cab416ea603a12d
FunPika <funpikawiki@gmail.com>
2015-05-16 20:33:53 -0400

    Fix if/else statements in create_db.sh.

--- sql/create_db.sh
+++ sql/create_db.sh
@@ -5,25 +5,29 @@
 set -e
 
 # Default to values given in variables used by TravisCI if no args are given
-if [ $# -ge 1] then
+if [ $# -ge 1 ]; then
 	$SQL_SERVER = $1
 else
 	$SQL_SERVER = $DB_SERV
-	
-if [ $# -ge 2] then
+fi
+
+if [ $# -ge 2 ]; then
 	$SQL_DBNAME = $2
 else
 	$SQL_DBNAME = $DB_NAME
+fi
 
-if [ $# -ge 3] then
+if [ $# -ge 3 ]; then
 	$SQL_USERNAME = $3
 else
 	$SQL_USERNAME = $DB_USER
-	
-if [ $# -ge 4] then
+fi
+
+if [ $# -ge 4 ]; then
 	$SQL_PASSWORD = $4
 else
 	$SQL_PASSWORD = $DB_PASS
+fi
 
 echo "Creating database..."
 mysql -h $SQL_SERVER -u $SQL_USERNAME -p$SQL_PASSWORD -e "CREATE DATABASE $SQL_DBNAME;"
