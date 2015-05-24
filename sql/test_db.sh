5487a713375dc25fa68b75e31134a897ef863fcf
FunPika <funpikawiki@gmail.com>
2015-05-23 20:57:39 -0400

    Merge create_db.sh into test_db.sh.

--- sql/test_db.sh
+++ sql/test_db.sh
@@ -1,53 +1,96 @@
 #!/bin/bash
+# Usage: ./test_db.sh <createonly> <server> <dbname> <user> <pass>
+# Arguments other than the first are optional
+# Set <createonly> to 1 if you do not need to test the build
+# Set <createonly> to 0 if you do need to test the build
 
 set -e
 
+if [ $# -lt 1 ]; then
+	echo "Usage: ./test_db.sh <createonly> <server> <dbname> <user> <pass>\n"
+	echo "Only the first parameter is required. It should be set to a 1 if\n"
+	echo "you are only creating a database, or 0 if you are testing the build.\n"
+	exit 1
+fi
 
-echo "Testing database build."
+if [ $1 -eq 0 ]; then
+	echo "Testing database build."
+else
+	echo "Creating database."
+fi
+
+# Default to values given in variables used by TravisCI if no args are given
+if [ $# -ge 2 ]; then
+	SQL_SERVER=$2
+else
+	SQL_SERVER=$MYSQL_HOST
+fi
+
+if [ $# -ge 3 ]; then
+	SQL_DBNAME=$3
+else
+	SQL_DBNAME=$MYSQL_SCHEMA
+fi
+
+if [ $# -ge 4 ]; then
+	SQL_USERNAME=$4
+else
+	SQL_USERNAME=$MYSQL_USER
+fi
+
+if [ $# -ge 5 ]; then
+	SQL_PASSWORD=-p$5
+elif [ -n "$MYSQL_PASSWORD" ]; then
+	SQL_PASSWORD=-p$MYSQL_PASSWORD
+else
+	SQL_PASSWORD=
+fi
 
 echo "Dropping old database..."
-mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD -e "DROP DATABASE IF EXISTS $MYSQL_SCHEMA;"
+mysql -h $SQL_SERVER -u $SQL_USERNAME $SQL_PASSWORD -e "DROP DATABASE IF EXISTS $SQL_DBNAME;"
 
 echo "Creating database..."
-mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD -e "CREATE DATABASE $MYSQL_SCHEMA;"
+mysql -h $SQL_SERVER -u $SQL_USERNAME $SQL_PASSWORD -e "CREATE DATABASE $SQL_DBNAME;"
 
 echo "Loading initial schema..."
-mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_SCHEMA < db-structure.sql
+mysql -h $SQL_SERVER -u $SQL_USERNAME $SQL_PASSWORD $SQL_DBNAME < db-structure.sql
 
 echo "Loading initial seed data..."
 for f in `ls seed/*_data.sql`; do
 	echo "  * $f"
-	mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_SCHEMA < $f
+	mysql -h $SQL_SERVER -u $SQL_USERNAME $SQL_PASSWORD $SQL_DBNAME < $f
 done
 
 echo "Applying patches..."
 for f in `ls patches/patch*.sql`; do
 	echo "  * $f"
-	mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_SCHEMA < $f
+	mysql -h $SQL_SERVER -u $SQL_USERNAME $SQL_PASSWORD $SQL_DBNAME < $f
 done
 
-echo "Dumping schema to file..."
-mysqldump --compact -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_SCHEMA > schema.sql
+if [ $1 -eq 0 ]; then
+	echo "Dumping schema to file..."
+	mysqldump --compact -h $SQL_SERVER -u $SQL_USERNAME $SQL_PASSWORD $SQL_DBNAME > schema.sql
 
-echo "Dropping database from server..."
-mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD -e "DROP DATABASE IF EXISTS $MYSQL_SCHEMA;"
+	echo "Dropping database from server..."
+	mysql -h $SQL_SERVER -u $SQL_USERNAME $SQL_PASSWORD -e "DROP DATABASE IF EXISTS $SQL_DBNAME;"
 
-echo "Creating database..."
-mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD -e "CREATE DATABASE $MYSQL_SCHEMA;"
+	echo "Creating database..."
+	mysql -h $SQL_SERVER -u $SQL_USERNAME $SQL_PASSWORD -e "CREATE DATABASE $SQL_DBNAME;"
 
-echo "Reloading database from file..."
-mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_SCHEMA < schema.sql
+	echo "Reloading database from file..."
+	mysql -h $SQL_SERVER -u $SQL_USERNAME $SQL_PASSWORD $SQL_DBNAME < schema.sql
 
-echo "Dumping schema to file..."
-mysqldump --compact -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_SCHEMA > schema2.sql
+	echo "Dumping schema to file..."
+	mysqldump --compact -h $SQL_SERVER -u $SQL_USERNAME $SQL_PASSWORD $SQL_DBNAME > schema2.sql
 
-echo "Comparing dumps..."
-diff -q schema.sql schema2.sql
+	echo "Comparing dumps..."
+	diff -q schema.sql schema2.sql
 
-echo "Rewriting definer..."
-cat schema.sql | sed "s/!50013 DEFINER=/DISABLED: 50013 DEFINER=/" > database.sql
+	echo "Rewriting definer..."
+	cat schema.sql | sed "s/!50013 DEFINER=/DISABLED: 50013 DEFINER=/" > database.sql
 
-echo "Removing unneeded files..."
-rm schema.sql schema2.sql
+	echo "Removing unneeded files..."
+	rm schema.sql schema2.sql
+fi
 
 echo "Done."
