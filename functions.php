e9fe0e6bb8584619432175b29a1d34f41ae8bbdb
Simon Walker <github@stwalkerster.co.uk>
2016-01-09 20:45:55 +0000

    test more than just XFP header

--- functions.php
+++ functions.php
@@ -45,6 +45,37 @@ function sendemail($messageno, $target, $id)
 }
 
 /**
+ * Returns a value indicating whether the current request was issued over HTTPSs
+ * @return bool true if HTTPS
+ */
+function isHttps() {
+	if (isset($_SERVER['HTTP_X_FORWARDED_PROTO'])) {
+		if($_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+			// Client <=> Proxy is encrypted
+			return true;
+		}
+		else {
+			// Proxy <=> Server link is encrypted, but not Client <=> Proxy.
+			return false;
+		}
+	}
+
+	if (isset($_SERVER['HTTPS'])) {
+		if($_SERVER['HTTPS'] === 'off') {
+			// ISAPI on IIS breaks the spec. :(
+			return false;
+		}
+
+		if($_SERVER['HTTPS'] !== '') {
+			// Set to a non-empty value
+			return true;
+		}
+	}
+
+	return false;
+}
+
+/**
  * Show the login page
  */
 function showlogin()
@@ -68,10 +99,12 @@ function showlogin()
 
 	global $strictTransportSecurityExpiry;
 	if ($strictTransportSecurityExpiry !== false) {
-		if ($_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+		if (isHttps()) {
+			// Client can clearly use HTTPS, so let's enforce it for all connections.
 			header("Strict-Transport-Security: max-age=15768000");
 		}
 		else {
+			// This is the login form, not the request form. We need protection here.
 			$path = 'https://' . $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
 			header("Location: " . $path);
 		}
