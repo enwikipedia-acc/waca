527bfb76d42134a25ef3c841b8b0b44f340d04f0
Simon Walker <github@stwalkerster.co.uk>
2016-03-26 14:24:27 +0000

    Update copyright headers everywhere

--- redir.php
+++ redir.php
@@ -1,4 +1,10 @@
 <?php
+/******************************************************************************
+ * Wikipedia Account Creation Assistance tool                                 *
+ *                                                                            *
+ * All code in this file is released into the public domain by the ACC        *
+ * Development Team. Please see team.json for a list of contributors.         *
+ ******************************************************************************/
 
 $toolList = array(
 	'tparis-pcount' => '//tools.wmflabs.org/supercount/index.php?user=%DATA%&project=en.wikipedia',
--- tests/includes/Validation/RequestValidationHelperTest.php
+++ tests/includes/Validation/RequestValidationHelperTest.php
@@ -1,4 +1,10 @@
 <?php
+/******************************************************************************
+ * Wikipedia Account Creation Assistance tool                                 *
+ *                                                                            *
+ * All code in this file is released into the public domain by the ACC        *
+ * Development Team. Please see team.json for a list of contributors.         *
+ ******************************************************************************/
 
 namespace Waca\Tests\Validation;
 
