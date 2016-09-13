6f58d6c74ec620f7ccc0f4204b92125e22fa7c0b
Matthew Bowker <matthewrbowker@me.com>
2016-09-13 14:02:28 -0600

    Beginning of Web Start test

---
+++ tests/includes/WebStartTest.php
@@ -0,0 +1,40 @@
+<?php
+/******************************************************************************
+ * Wikipedia Account Creation Assistance tool                                 *
+ *                                                                            *
+ * All code in this file is released into the public domain by the ACC        *
+ * Development Team. Please see team.json for a list of contributors.         *
+ ******************************************************************************/
+
+namespace Waca\Tests;
+
+use Waca\SiteConfiguration;
+use Waca\WebStart;
+use Waca\Router\RequestRouter;
+
+class WebStartTest extends \PHPUnit_Framework_TestCase
+{
+	private $sc;
+	private $ir;
+	private $ws;
+
+	public function setUp() {
+		$this->sc = new SiteConfiguration();
+		$this->ir = new RequestRouter();
+		$this->ws = new WebStart($this->sc, $this->ir);
+	}
+
+	public function tearDown() {
+		unset($this->ws);
+		unset($this->ir);
+		unset($this->sc);
+	}
+
+	public function testCreatedProperly() {
+		$this->assertInstanceOf('Waca\SiteConfiguration', $this->sc);
+	}
+
+	public function testRun() {
+		$this->markTestSkipped("Not implemented yet.");
+	}
+}
