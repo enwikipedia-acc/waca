610f47448ce04547a678670830ea88640f9391c2
Simon Walker <github@stwalkerster.co.uk>
2016-09-22 22:12:23 +0200

    Formatting, and don't run the ID verifier test if contacting meta throws an exception - this will mark the test as ignored when offline.
    T248

--- tests/includes/ApplicationBaseTest.php
+++ tests/includes/ApplicationBaseTest.php
@@ -8,41 +8,48 @@
 
 namespace Waca\Tests;
 
-use \Waca\ApplicationBase;
+use PHPUnit_Framework_MockObject_MockObject;
+use PHPUnit_Framework_TestCase;
+use Waca\ApplicationBase;
+use Waca\SiteConfiguration;
 
-class ApplicationBaseTest extends \PHPUnit_Framework_TestCase
+class ApplicationBaseTest extends PHPUnit_Framework_TestCase
 {
 	private $si;
-    private $ab;
+	/** @var PHPUnit_Framework_MockObject_MockObject */
+	private $ab;
 
-	public function setUp() {
-		$this->si = new \Waca\SiteConfiguration();
+	public function setUp()
+	{
+		$this->si = new SiteConfiguration();
 
 		$this->ab = $this->getMockForAbstractClass(ApplicationBase::class, [$this->si]);
 	}
 
-	public function testConstruct() {
+	public function testConstruct()
+	{
+		// -- stw 2016-09-22
+		$this->markTestIncomplete("Broken, not sure what this should be doing");
 
 		$this->ab->expects($this->any())
-			-> method("run")
-			-> will($this->returnValue(NULL));
-
+			->method("run")
+			->will($this->returnValue(null));
 	}
 
-
-	function testRun() {
+	function testRun()
+	{
 		$this->markTestIncomplete("Not fully implemented yet.");
 
 		$this->ab->expects($this->any())
-			-> method("run")
-			-> will($this->returnValue(NULL));
+			->method("run")
+			->will($this->returnValue(null));
 	}
 
-	function testGetConfiguration() {
+	function testGetConfiguration()
+	{
 		$this->markTestIncomplete("Not fully implemented yet.");
 		$this->ab->expects($this->any())
-			-> method("getConfiguration")
-			-> will($this->returnValue(NULL));
+			->method("getConfiguration")
+			->will($this->returnValue(null));
 	}
-
 }
