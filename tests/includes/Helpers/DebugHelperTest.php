6a9702918d1b8f7893054e5b1d2f7a0e3f3a6cfb
Matthew Bowker <matthewrbowker@me.com>
2016-11-09 14:57:26 -0700

    Disable DebugHelper test again, new mocking method

--- tests/includes/Helpers/DebugHelperTest.php
+++ tests/includes/Helpers/DebugHelperTest.php
@@ -15,69 +15,47 @@ use Waca\Helpers\DebugHelper;
 class DebugHelperTest extends PHPUnit_Framework_TestCase
 {
 	/** @var PHPUnit_Extensions_MockFunction */
-	private $btMock;
-	/** @var DebugHelper */
 	private $dbh;
 
 	public function setUp()
 	{
 		//$this->markTestSkipped("Appears to allocate too much memory, we may have a bug here.  Skipping for now.");
 
-		$this->dbh = new DebugHelper();
+		//$this->dbh = new DebugHelper();
 
-		$this->btMock = new PHPUnit_Extensions_MockFunction('get_debug_backtrace', $this->dbh);
+		//$this->btMock = new PHPUnit_Extensions_MockFunction('get_debug_backtrace', $this->dbh);
+
+		//$this->dbhMock = $this->getMockBuilder(DebugHelper::class)->getMock();
+		$this->dbh = $this->getMock(DebugHelper::class, array("get_debug_backtrace"));
+		$this->dbh->method('get_debug_backtrace')->willReturn(
+			array(
+				array(
+					"file"     => "/tmp/a.php",
+					"line"     => 10,
+					"function" => "a_test",
+					"args"     => array("friend"),
+				),
+				array(
+					"file"     => "/tmp/b.php",
+					"line"     => 2,
+					"function" => "include_once",
+					"args"     => array("/tmp/a.php"),
+				),
+			)
+		);
 	}
 
 	public function tearDown()
 	{
-		$this->btMock = null;
 		$this->dbh = null;
 	}
 
 	public function testGetBacktrace()
 	{
-
-		$mock = $this->getMock("debugHelper", array("get_Debug_backtrace"));
-		$mock->expects($this->once())->method("c")->will($this->returnValue(array(
-			array(
-				"file"     => "/tmp/a.php",
-				"line"     => 10,
-				"function" => "a_test",
-				"args"     => array("friend"),
-			),
-			array(
-				"file"     => "/tmp/b.php",
-				"line"     => 2,
-				"function" => "include_once",
-				"args"     => array("/tmp/a.php"),
-			),
-		)));
-		/*
-		$this->btMock->expects($this->any())
-			->will($this->returnValue(
-				array(
-					array(
-						"file"     => "/tmp/a.php",
-						"line"     => 10,
-						"function" => "a_test",
-						"args"     => array("friend"),
-					),
-					array(
-						"file"     => "/tmp/b.php",
-						"line"     => 2,
-						"function" => "include_once",
-						"args"     => array("/tmp/a.php"),
-					),
-				)
-			));
-*/
-		$this->assertContains("/tmp/a.php", $mock->getBacktrace());
-		$this->assertContains("/tmp/b.php", $mock->getBacktrace());
-		$this->assertContains("a_test", $mock->getBacktrace());
-		$this->assertContains("include_once", $mock->getBacktrace());
-		//$this->assertContains("/tmp/a.php", $this->dbh->getBacktrace());
-		//$this->assertContains("/tmp/b.php", $this->dbh->getBacktrace());
-		//$this->assertContains("a_test", $this->dbh->getBacktrace());
-		//$this->assertContains("include_once", $this->dbh->getBacktrace());
+		$this->markTestSkipped("MRB - Still working on this one.");
+		$this->assertContains("/tmp/a.php", $this->dbh->getBacktrace());
+		$this->assertContains("/tmp/b.php", $this->dbh->getBacktrace());
+		$this->assertContains("a_test", $this->dbh->getBacktrace());
+		$this->assertContains("include_once", $this->dbh->getBacktrace());
 	}
 }
