f35fcf3e81e2bb8adebd2bed986ac384c1aeb832
Simon Walker <github@stwalkerster.co.uk>
2017-02-18 21:50:54 +0000

    Let's make this not eat errors

--- .travis.phplint.sh
+++ .travis.phplint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo -e "\033[32mRunning lint test on all PHP files...\033[0m"
+echo -e "\033[32mCurrent directory is \033[37;1m`pwd`\033[0m"
+
 files=$(find . \
         \( -path ./vendor -o -path ./lib \) -prune -o \
         -type f -name \*.php \
@@ -10,15 +13,18 @@ files=$(find . \
 fail=0
 
 for fileName in ${files}; do
-    php -l ${fileName} | grep "Errors parsing "
-    if [ $? -eq 1 ]
+    php -l ${fileName}
+    if [ $? -ne 0 ]
     then
+        echo -e "\033[31mError detected in lint, marking build as \033[41;37mFAILED\033[m"
         fail=1
     fi
 done
 
 if [ ${fail} -eq 0 ]; then
+    echo -e "\033[32mGlobal lint check \033[1mPASSED\033[m"
     exit 0
 else
+    echo -e "\033[31mGlobal lint check \033[1mFAILED\033[m"
     exit 1
 fi
