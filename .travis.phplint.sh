cd303e4a92822a197c78bce5aeec96d14150e6fb
Simon Walker <github@stwalkerster.co.uk>
2017-02-18 21:16:56 +0000

    Fix phplint script
    == brief testing ==
    ```
    [21:15:45][stwalkerster@oddish:~/waca][mrb_newinternal-includes:344b68f|1|0|0]
    bash $ ./.travis.phplint.sh
    
    [21:15:53][stwalkerster@oddish:~/waca][mrb_newinternal-includes:344b68f|1|0|0]
    bash $ echo $?
    0
    
    [21:15:57][stwalkerster@oddish:~/waca][mrb_newinternal-includes:344b68f|1|0|0]
    bash $ nano index.php
    
    [21:16:03][stwalkerster@oddish:~/waca][mrb_newinternal-includes:344b68f|2|0|0]
    bash $ ./.travis.phplint.sh
    PHP Parse error:  syntax error, unexpected 'namespace' (T_NAMESPACE) in ./index.php on line 9
    Errors parsing ./index.php
    
    [21:16:10][stwalkerster@oddish:~/waca][mrb_newinternal-includes:344b68f|2|0|0]
    bash $ echo $?
    1
    ```

--- .travis.phplint.sh
+++ .travis.phplint.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
-#Author Addshore
 
-find ../ -type f -name \*.php -exec php -l {} \; | grep "Errors parsing ";
+files=$(find . \
+        \( -path ./vendor -o -path ./lib \) -prune -o \
+        -type f -name \*.php \
+        | grep -v '^./vendor$' \
+        | grep -v '^./lib$'
+)
 
-#Flip the exit code
-if [ $? -ne 0 ]
-then
-	exit 0
+fail=0
+
+for fileName in ${files}; do
+    php -l ${fileName} | grep "Errors parsing "
+    if [ $? -eq 1 ]
+    then
+        fail=1
+    fi
+done
+
+if [ ${fail} -eq 0 ]; then
+    exit 0
 else
-	exit 1
+    exit 1
 fi
