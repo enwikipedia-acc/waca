68bcf0d013168934bb516479d9462e296db3a5e1
Amanda/DeltaQuad <deltaquadwiki@gmail.com>
2020-04-08 16:48:15 -0400

    No further data for the array

--- redir.php
+++ redir.php
@@ -10,7 +10,7 @@ $toolList = array(
 	'google' => 'https://www.google.com/search?q=%DATA%',
 	'rangefinder' => 'https://tools.wmflabs.org/rangeblockfinder/?ip=%DATA%',
 	'ipcheck' => 'https://tools.wmflabs.org/ipcheck/index.php?ip=%DATA%',
-	'bgpview' => 'https://bgpview.io/ip/%DATA%',
+	'bgpview' => 'https://bgpview.io/ip/%DATA%'
 );
 
 if(!isset($_GET['tool'])
