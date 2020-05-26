37008f503303dac4d9f42949bb3828ff4b96e39e
Simon Walker <github@stwalkerster.co.uk>
2020-05-26 18:06:55 +0100

    Delete unused methods

--- includes/StringFunctions.php
+++ includes/StringFunctions.php
@@ -11,63 +11,6 @@ namespace Waca;
 class StringFunctions
 {
     /**
-     * Formats a string to be used as a username.
-     *
-     * @param $username
-     *
-     * @return string
-     */
-    public function formatAsUsername($username)
-    {
-        // trim whitespace from the ends
-        $uname = mb_ereg_replace("^[ \t]+|[ \t]+$", "", $username);
-
-        // convert first char to uppercase
-        $uname = $this->upperCaseFirst($uname);
-
-        // replace spaces with underscores
-        $uname = mb_ereg_replace("[ ]+", "_", $uname);
-
-        // trim underscores from the end
-        $uname = mb_ereg_replace("[_]+$", "", $uname);
-
-        return $uname;
-    }
-
-    /**
-     * Formats a string to be used as an email (specifically strips whitespace
-     * from the beginning/end of the Email, as well as immediately before/after
-     * the @ in the Email).
-     *
-     * @param $email
-     *
-     * @return string
-     */
-    public static function formatAsEmail($email)
-    {
-        // trim whitespace from the ends
-        $newemail = mb_ereg_replace("^[ \t]+|[ \t]+$", "", $email);
-
-        // trim whitespace from around the email address
-        $newemail = mb_ereg_replace("[ \t]+@", "@", $newemail);
-        $newemail = mb_ereg_replace("@[ \t]+", "@", $newemail);
-
-        return $newemail;
-    }
-
-    /**
-     * Returns true if a string is a multibyte string
-     *
-     * @param string $string
-     *
-     * @return bool
-     */
-    public function isMultibyte($string)
-    {
-        return strlen($string) !== mb_strlen($string);
-    }
-
-    /**
      * Make a string's first character uppercase
      *
      * @param string $string
--- tests/includes/StringFunctionsTest.php
+++ tests/includes/StringFunctionsTest.php
@@ -28,65 +28,6 @@ class StringFunctionsTest extends TestCase
         $this->e = null;
     }
 
-    public function testFormatAsUsername()
-    {
-        // Happy path
-        $this->assertEquals($this->e->formatAsUsername("this"), "This");
-        $this->assertEquals($this->e->formatAsUsername("1this"), "1this");
-        $this->assertEquals($this->e->formatAsUsername("This"), "This");
-        $this->assertEquals($this->e->formatAsUsername("This "), "This");
-        $this->assertEquals($this->e->formatAsUsername("This_"), "This");
-
-        // Sad Path
-        $this->assertNotEquals($this->e->formatAsUsername("This "), "This ");
-        $this->assertNotEquals($this->e->formatAsUsername("This_"), "This_");
-        $this->assertNotEquals($this->e->formatAsUsername("this"), "this");
-        $this->assertNotEquals($this->e->formatAsUsername("1this"), "1This");
-    }
-
-    public function testFormatAsEmail()
-    {
-        $this->assertEquals($this->e->formatAsEmail("this@example.com"), "this@example.com");
-        $this->assertEquals($this->e->formatAsEmail("1this12345@example.com"), "1this12345@example.com");
-        $this->assertEquals($this->e->formatAsEmail(" 1this12345@example.com"), "1this12345@example.com");
-        $this->assertEquals($this->e->formatAsEmail("1this12345@example.com "), "1this12345@example.com");
-        $this->assertEquals($this->e->formatAsEmail("1this12345 @example.com"), "1this12345@example.com");
-        $this->assertEquals($this->e->formatAsEmail("1this12345@ example.com"), "1this12345@example.com");
-
-        // Sad Path
-        $this->assertNotEquals($this->e->formatAsEmail(" 1this12345@example.com"), " 1this12345@example.com");
-        $this->assertNotEquals($this->e->formatAsEmail("1this12345@example.com "), "1this12345@example.com ");
-        $this->assertNotEquals($this->e->formatAsEmail("1this12345 @example.com"), "1this12345 @example.com");
-        $this->assertNotEquals($this->e->formatAsEmail("1this12345@ example.com"), "1this12345@ example.com");
-    }
-
-    public function testIsMultibyte()
-    {
-        $emptyString = '';
-        $this->assertFalse($this->e->isMultibyte($emptyString));
-
-        $nullString = null;
-        $this->assertFalse($this->e->isMultibyte($nullString));
-
-        $nbspString = html_entity_decode('&nbsp;');
-        $this->assertTrue($this->e->isMultibyte($nbspString));
-
-        $numberString = '12345';
-        $this->assertFalse($this->e->isMultibyte($numberString));
-
-        $asciiString = 'abcd';
-        $utf8String = 'àbcd';
-        $this->assertFalse($this->e->isMultibyte($asciiString));
-        $this->assertTrue($this->e->isMultibyte($utf8String));
-
-        $greekString = 'ΣΙΛΟΝ';
-        $tibetanString = '༆༇༂༖';
-        $chineseString = '专世丳儽';
-        $this->assertTrue($this->e->isMultibyte($greekString));
-        $this->assertTrue($this->e->isMultibyte($tibetanString));
-        $this->assertTrue($this->e->isMultibyte($chineseString));
-    }
-
     public function testUcFirst()
     {
         $this->assertEquals('Abc', $this->e->upperCaseFirst('abc'));
