4097dba8e0a70d59f5aaa49d105dd89da4afb3d5
Simon Walker <github@stwalkerster.co.uk>
2020-05-30 01:27:01 +0100

    Fix for when there's no config file present

--- .travis.scss.sh
+++ .travis.scss.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
 echo -e "\033[32mRunning SCSS rebuild...\033[0m"
-cd "$(dirname "$0")/maintenance" || { echo "Failed to find maintenance directory!"; exit 1; }
+cd "$(dirname "$0")" || { echo "Failed to find root directory!"; exit 1; }
 
-find ../resources/generated -type f -name '*.css' -delete
+echo "Constructing minimal configuration"
+echo "<?php" > config.local.inc.php
+echo "\$toolserver_host = '${MYSQL_HOST}';" >> config.local.inc.php
+echo "\$toolserver_username = '${MYSQL_USER}';" >> config.local.inc.php
+echo "\$toolserver_password = '${MYSQL_PASSWORD}';" >> config.local.inc.php
+echo "\$toolserver_database = '${MYSQL_SCHEMA}';" >> config.local.inc.php
+echo '$toolserver_notification_dbhost = $toolserver_host;' >> config.local.inc.php
+echo '$toolserver_notification_database = $toolserver_database;' >> config.local.inc.php
+echo '$notifications_username = $toolserver_username;' >> config.local.inc.php
+echo '$notifications_password = $toolserver_password;' >> config.local.inc.php
 
+
+echo "Cleaning up existing files for test"
+find resources/generated -type f -name '*.css' -delete
+
+cd maintenance || { echo "Failed to find maintenance directory!"; exit 1; }
 php RegenerateStylesheets.php
 
 cd ../resources/generated || { echo "Failed to find generated resources directory!" ; exit 1; }
@@ -23,3 +37,5 @@ fi
 
 
 
+
+}
