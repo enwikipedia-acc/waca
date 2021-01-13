60d29754419da61ea9b645e98c51614776646f03
Simon Walker <github@stwalkerster.co.uk>
2021-01-13 20:57:32 +0000

    Pull in popper.js via npm
    Fixes #276

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,14 +1,18 @@
 #!/bin/bash -ex
 
 cd /var/www/html
-
-chown -R www-data templates_c errorlog
+git config --global --replace-all safe.directory /var/www/html
+cp ~/.gitconfig /etc/gitconfig
 
 if [[ ! -f config.local.inc.php ]]; then
     cp docker/config.local.inc.php config.local.inc.php
 fi
 
-php /opt/composer.phar install
+php /opt/composer.phar install --no-progress
+npm install
+
+chown -R www-data templates_c errorlog
+
 php maintenance/RegenerateStylesheets.php
 
 exec apache2-foreground
