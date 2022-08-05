e7f34faae89e131e306764186dd432418be0f5b5
Matthew Bowker <matthewbowker.bugs@gmail.com>
2022-08-05 12:39:54 -0600

    Allow administrators to manually confirm requests
    #259

---
+++ includes/Pages/RequestAction/PageManuallyConfirm.php
@@ -0,0 +1,49 @@
+<?php
+/******************************************************************************
+ * Wikipedia Account Creation Assistance tool                                 *
+ *                                                                            *
+ * All code in this file is released into the public domain by the ACC        *
+ * Development Team. Please see team.json for a list of contributors.         *
+ ******************************************************************************/
+
+namespace Waca\Pages\RequestAction;
+
+use Waca\Exceptions\ApplicationLogicException;
+use Waca\Exceptions\OptimisticLockFailedException;
+use Waca\Helpers\Logger;
+
+class PageManuallyConfirm extends RequestActionBase
+{
+    /**
+     * This endpoint manually confirms a request, bypassing email confirmation.
+     *
+     * Only administrators are allowed to do this, for obvious reasons.
+     *
+     * @throws ApplicationLogicException|OptimisticLockFailedException
+     */
+    protected function main()
+    {
+        // This method throws an error if we don't post
+        $this->checkPosted();
+
+        // Retrieve the database.
+        $database = $this->getDatabase();
+
+        // Find the request
+        // This method throws exceptions if there is an error with the request.
+        $request = $this->getRequest($database);
+
+        // Mark the request as confirmed.
+        $request->setEmailConfirm("Confirmed");
+        $request->save();
+
+        // Log that the request was manually confirmed
+        Logger::manuallyConfirmRequest($database, $request);
+
+        // Notify the IRC channel
+        $this->getNotificationHelper()->requestReceived($request);
+
+        // Redirect back to the main request, now it should show the request.
+        $this->redirect('viewRequest', null, array('id' => $request->getId()));
+    }
+}
