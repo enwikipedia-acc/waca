01f572d3065f8f37b548043b214e2a90108a65b6
Andrew "FastLizard4" Adams <FastLizard4-Git-Commits@fastlizard4.org>
2022-08-08 13:06:53 -0700

    Various Docker improvements
    Various improvements to the Docker setup for local development:
    
    - Adds a default config.local.inc.php that will be added to the root dir
      if you don't already have one; should work out of the box for any
      Docker setup
    - Adds a RabbitMQ definitions file which creates a "main" exchange and
      queue (also specified by default in the Docker config.local.inc.php
      file) that receives all notifications sent by the tool. Uses a fanout
      exchange so that all notifications are delivered to the same "main"
      queue regardless of routing key.
    - Adds a mailsink service to the docker-compose environment and
      configures the application service to use it; this allows for easy
      local testing of email-based functionality.
    
    Secondarily:
    - Disable XDebug's `discover_client_host` setting as this was causing
      more issues than it fixed (and the hard-coded host of
      `host.docker.internal` should work in almost all environments
    - Update my entry in team.json
    
    Resolves #750.

---
+++ docker/config.local.inc.php
@@ -0,0 +1,25 @@
+<?php
+/******************************************************************************
+ * Wikipedia Account Creation Assistance tool                                 *
+ *                                                                            *
+ * All code in this file is released into the public domain by the ACC        *
+ * Development Team. Please see team.json for a list of contributors.         *
+ ******************************************************************************/
+
+// Sane defaults for Docker-based ACC development environments. When running `docker compose up`,
+// this file will be copied into the ACC root if a local config file does not already exist.
+
+$toolserver_username = "waca";
+$toolserver_password = "waca";
+$toolserver_host = "database";
+$toolserver_database = "waca";
+
+$whichami = 'docker_dev';
+
+// URL of the current copy of the tool.
+$baseurl = "http://localhost:8080";
+$cookiepath = '/';
+
+$amqpConfiguration = ['host' => 'msgbroker', 'port' => 5672, 'user' => 'guest', 'password' => 'guest', 'vhost' => '/', 'exchange' => 'main', 'tls' => false];
+
+$enableEmailConfirm = 1; // Emails will be sent to the mailsink docker-compose service
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,6 +4,10 @@ cd /var/www/html
 
 chown -R www-data templates_c errorlog
 
+if [[ ! -f config.local.inc.php ]]; then
+    cp docker/config.local.inc.php config.local.inc.php
+fi
+
 php /opt/composer.phar install
 php maintenance/RegenerateStylesheets.php
 
