357079f8e956a0d1be1aa203c2f7df8d9ede9e06
Simon Walker <github@stwalkerster.co.uk>
2023-04-18 20:42:11 +0100

    Whitespace fix

--- tests/includes/DataObjectTest.php
+++ tests/includes/DataObjectTest.php
@@ -12,39 +12,40 @@ use PHPUnit\Framework\TestCase;
 
 class DataObjectTest extends TestCase
 {
-	private $do;
-	private $dbh;
+    private $do;
+    private $dbh;
 
-	public function setUp() : void
+    public function setUp(): void
     {
-		$this->do = $this->getMockForAbstractClass("\Waca\DataObject");
+        $this->do = $this->getMockForAbstractClass("\Waca\DataObject");
 
-		$this->dbh = $this->getMockBuilder('\Waca\PdoDatabase')
-			->setMockClassName('PdoDatabase')
-			->disableOriginalConstructor()
-			->getMock();
+        $this->dbh = $this->getMockBuilder('\Waca\PdoDatabase')
+            ->setMockClassName('PdoDatabase')
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
 
-	}
+    public function testID()
+    {
+        $this->assertTrue($this->do->isNew());
+        $this->assertEquals($this->do->getID(), 0);
+    }
 
-	public function testID() {
-		$this->assertTrue($this->do->isNew());
-		$this->assertEquals($this->do->getID(), 0);
-	}
+    public function testUpdateVersion()
+    {
+        $this->assertEquals($this->do->getUpdateVersion(), 0);
 
-	public function testUpdateVersion()
-	{
-		$this->assertEquals($this->do->getUpdateVersion(), 0);
+        $this->assertEquals($this->do->setUpdateVersion(42), null);
 
-		$this->assertEquals($this->do->setUpdateVersion(42), null);
+        $this->assertEquals($this->do->getUpdateVersion(), 42);
+    }
 
-		$this->assertEquals($this->do->getUpdateVersion(), 42);
-	}
+    public function testDatabase()
+    {
+        $this->assertNull($this->do->getDatabase());
 
-	public function testDatabase() {
-		$this->assertNull($this->do->getDatabase());
+        $this->assertNull($this->do->setDatabase($this->dbh));
 
-		$this->assertNull($this->do->setDatabase($this->dbh));
-
-		$this->assertEquals($this->do->getDatabase(), $this->dbh);
-	}
+        $this->assertEquals($this->do->getDatabase(), $this->dbh);
+    }
 }
