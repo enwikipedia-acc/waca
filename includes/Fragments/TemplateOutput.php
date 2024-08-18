ddb3191ef4b6f8b260049ac644d3c4a8ebe17a19
Rich Smith <rsmithy@cluenet.org>
2024-08-18 14:17:24 +0100

    Update includes/Fragments/TemplateOutput.php
    Co-authored-by: Simon Walker <github@stwalkerster.co.uk>

--- includes/Fragments/TemplateOutput.php
+++ includes/Fragments/TemplateOutput.php
@@ -44,8 +44,7 @@ trait TemplateOutput
     {
         $this->smarty = new Smarty();
         $this->smarty->addPluginsDir($this->getSiteConfiguration()->getFilePath() . '/smarty-plugins');
-        $this->registerPlugin('modifier', 'count', 'count');
-        $this->registerPlugin('modifier', 'implode', 'implode');
+        $this->smarty->registerPlugin('modifier', 'implode', 'implode');
 
         $this->assign('currentUser', User::getCommunity());
         $this->assign('skin', 'auto');
