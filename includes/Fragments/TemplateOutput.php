201c798383b559c70f53d7555afb73a06ed048df
Simon Walker <github@stwalkerster.co.uk>
2024-08-30 18:57:48 +0100

    Smarty 5 compat: Replace implode with join

--- includes/Fragments/TemplateOutput.php
+++ includes/Fragments/TemplateOutput.php
@@ -44,7 +44,6 @@ trait TemplateOutput
     {
         $this->smarty = new Smarty();
         $this->smarty->addPluginsDir($this->getSiteConfiguration()->getFilePath() . '/smarty-plugins');
-        $this->smarty->registerPlugin('modifier', 'implode', 'implode');
 
         $this->assign('currentUser', User::getCommunity());
         $this->assign('skin', 'auto');
