31773279e30b4e00bee782043ded901404cef046
methecooldude <rsmithy@cluenet.org>
2025-05-09 22:55:47 +0100

    Update BanHelper.php

--- includes/Helpers/BanHelper.php
+++ includes/Helpers/BanHelper.php
@@ -200,8 +200,8 @@ SQL;
     ':email'     => $request->getEmail(),
     ':useragent' => $request->getUserAgent(),
     ':domain'    => $request->getDomain(),
-    ':ip4'       => $isIPv6 ? null : $trustedIp,
-    ':ip6'       => $isIPv6 ? $trustedIp : null,
+    ':ip4'       => $isIPv6 ? '' : $trustedIp,
+    ':ip6'       => $isIPv6 ? $trustedIp : '',
 ];
         die(print_r($params, true);
         $statement->execute($params);
